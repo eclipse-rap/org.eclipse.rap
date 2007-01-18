f28934c5eac2338ac8cb6c4ded0a507fcf948392
Frank Appel <fappel>
2007-01-18 13:46:37 +0000

    Initial Implementation of TableViewer

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/DemoTableViewPart.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/DemoTableViewPart.java
@@ -11,7 +11,11 @@
 
 package org.eclipse.rap.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.rap.jface.viewers.*;
 import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.graphics.Image;
 import org.eclipse.rap.rwt.layout.*;
 import org.eclipse.rap.rwt.widgets.*;
 import org.eclipse.rap.ui.part.ViewPart;
@@ -22,30 +26,68 @@ public class DemoTableViewPart extends ViewPart {
   private static final int ROWS = 40;
   private static final int COLUMNS = 10;
 
+  private class ViewContentProvider implements IStructuredContentProvider {
+    public Object[] getElements( Object inputElement ) {
+      List buffer = new ArrayList();
+      for( int i = 0; i < ROWS; i++ ) {
+        String[] row = new String[ COLUMNS ];
+        for( int j = 0; j < COLUMNS; j++ ) {
+          row[ j ] = "Item" + i + "-" + j ;
+        }
+        buffer.add( row );
+      }
+      Object[] result = new Object[ buffer.size() ];
+      buffer.toArray( result );
+      return result;
+    }
+    public void dispose() {
+    }
+    public void inputChanged( Viewer viewer, Object oldInput, Object newInput ) {
+    }
+  }
+  
+  private class ViewLabelProvider 
+    extends LabelProvider
+    implements ITableLabelProvider
+  {
+    public Image getColumnImage( Object element, int columnIndex ) {
+      return null;
+    }
+    public String getColumnText( Object element, int columnIndex ) {
+      String[] row = ( String[] )element;
+      String result = row[ columnIndex ];
+      return result;
+    }
+  }
+  
   public void createPartControl( final Composite parent ) {
     parent.setLayout( new FormLayout() );
-    final Table table = new Table( parent, RWT.NONE );
+    TableViewer viewer = new TableViewer( parent, RWT.NONE );
+    viewer.setContentProvider( new ViewContentProvider() );
+    viewer.setLabelProvider( new ViewLabelProvider() );
+    final Table table = viewer.getTable();
+    viewer.setColumnProperties( initColumnProperties( table ) );
+    viewer.setInput( this );
     FormData formData = new FormData();
     table.setLayoutData( formData );
     formData.top = new FormAttachment( 0, 5 );
     formData.left = new FormAttachment( 0, 5 );
     formData.right = new FormAttachment( 100, -5 );
     formData.bottom = new FormAttachment( 100, -5 );
+  }
+
+  private String[] initColumnProperties( final Table table ) {
+    String[] result = new String[ COLUMNS ];
     for( int i = 0; i < COLUMNS; i++ ) {
       TableColumn tableColumn = new TableColumn( table, RWT.NONE );
-      tableColumn.setText( "Column" + i );
+      result[ i ] = "Column" + i ;
+      tableColumn.setText( result[ i ] );
       if( i == 2 ) {
         tableColumn.setWidth( 190 );
       } else {
         tableColumn.setWidth( 70 );
       }
     }
-    for( int i = 0; i < ROWS; i++ ) {
-      TableItem tableItem = new TableItem( table, RWT.NONE );
-      for( int j = 0; j < COLUMNS; j++ ) {
-        tableItem.setText( j, "Item" + i + "-" + j );
-      }
-    }
-
+    return result;
   }
 }
