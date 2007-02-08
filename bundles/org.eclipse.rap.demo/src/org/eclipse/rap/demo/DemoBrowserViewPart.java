2cbce026c0b53358a7adb307e04699643b62bba2
Frank Appel <fappel>
2007-02-08 13:40:24 +0000

    done some minor cleanup

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/DemoBrowserViewPart.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/DemoBrowserViewPart.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2002-2006 Innoopract Informationssysteme GmbH.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Innoopract Informationssysteme GmbH - initial API and implementation
+ * Copyright (c) 2002-2006 Innoopract Informationssysteme GmbH. All rights
+ * reserved. This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors: Innoopract Informationssysteme GmbH - initial API and
+ * implementation
  ******************************************************************************/
-
 package org.eclipse.rap.demo;
 
 import org.eclipse.rap.demo.DemoTreeViewPart.TreeObject;
@@ -20,35 +17,34 @@ import org.eclipse.rap.rwt.widgets.Composite;
 import org.eclipse.rap.ui.*;
 import org.eclipse.rap.ui.part.ViewPart;
 
-
 public class DemoBrowserViewPart extends ViewPart {
-  Browser browser;
-  
-  public void createPartControl( final Composite parent ) {
-	browser = new Browser( parent, RWT.NONE );
-	browser.setUrl( "http://www.eclipse.org/birt/phoenix/examples/solution/TopSellingProducts.html" );
-	createSelectionListener();
-  }
-  
-  private void createSelectionListener() {
-	    IWorkbench workbench = PlatformUI.getWorkbench();
-	    IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-	    ISelectionService selectionService = window.getSelectionService();
-	    selectionService.addSelectionListener( new ISelectionListener() {
-	      public void selectionChanged( final IWorkbenchPart part, 
-	                                    final ISelection selection ) 
-	      {
-	        IStructuredSelection sselection = ( IStructuredSelection )selection;
-	        Object firstElement = sselection.getFirstElement();
-	        if( firstElement instanceof TreeObject ) {
-	          String location = ( ( TreeObject )firstElement ).getLocation();
-	          if( location != null ) {
-	            browser.setUrl( location );
-	          }
-	        } 
-	      } 
-	    } );
-	  }
 
-  
+  Browser browser;
+
+  public void createPartControl( final Composite parent ) {
+    browser = new Browser( parent, RWT.NONE );
+    browser.setUrl( "http://www.eclipse.org/birt/phoenix/examples/solution/TopSellingProducts.html" );
+    createSelectionListener();
+  }
+
+  private void createSelectionListener() {
+    IWorkbench workbench = PlatformUI.getWorkbench();
+    IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+    ISelectionService selectionService = window.getSelectionService();
+    selectionService.addSelectionListener( new ISelectionListener() {
+
+      public void selectionChanged( final IWorkbenchPart part,
+                                    final ISelection selection )
+      {
+        IStructuredSelection sselection = ( IStructuredSelection )selection;
+        Object firstElement = sselection.getFirstElement();
+        if( firstElement instanceof TreeObject ) {
+          String location = ( ( TreeObject )firstElement ).getLocation();
+          if( location != null ) {
+            browser.setUrl( location );
+          }
+        }
+      }
+    } );
+  }
 }
