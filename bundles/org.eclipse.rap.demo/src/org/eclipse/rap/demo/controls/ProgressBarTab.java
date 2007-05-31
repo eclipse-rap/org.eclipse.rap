ab59028d9df679fc48ab8b1b5485200c0178bb8a
Frank Appel <fappel>
2007-05-31 18:08:07 +0000

    minor code cleanup

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/controls/ProgressBarTab.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/controls/ProgressBarTab.java
@@ -28,23 +28,21 @@ public class ProgressBarTab extends ExampleTab {
 
   protected void createExampleControls( final Composite parent ) {
     int style = getStyle() == 0 ? SWT.HORIZONTAL : getStyle();
-    final ProgressBar progressBar = new ProgressBar( parent, style );
-    final Button button = new Button( parent, SWT.PUSH );
     
+    final ProgressBar progressBar = new ProgressBar( parent, style );
     progressBar.setLocation( 10, 10 );
     progressBar.setSize( 200, 22 );
     progressBar.setMaximum( 20 );
     
+    final Button button = new Button( parent, SWT.PUSH );
     button.setText( "Start Background Process" );
     button.addSelectionListener( new SelectionAdapter() {
       public void widgetSelected( final SelectionEvent evt ) {
         button.setEnabled( false );
+        // activate UI-callback mechanism
         UICallBackUtil.activateUICallBack( ProgressBarTab.class.getName() );
-        Runnable runnable = createRunnable( progressBar,
-                                            button,
-                                            progressBar.getMaximum(),
-                                            progressBar.getDisplay() );
-        Thread thread = new Thread( runnable );
+        // create and start background thread that updates the progress bar
+        Thread thread = new Thread( createRunnable( progressBar, button ) );
         thread.setDaemon( true );
         thread.start();
         
@@ -56,24 +54,28 @@ public class ProgressBarTab extends ExampleTab {
   }
 
   private Runnable createRunnable( final ProgressBar progressBar,
-                                   final Button button,
-                                   final int maximum,
-                                   final Display display )
+                                   final Button button )
   {
+    final int maximum = progressBar.getMaximum();
+    final Display display = progressBar.getDisplay();
     Runnable result = new Runnable() {
       public void run() {
         for( int i = 0; i <= maximum; i++ ) {
           final int selection = i;
           try {
+            // simulate some work
             Thread.sleep( 250 );
-          } catch( Throwable th ) {
+          } catch( final Throwable shouldNotHappen ) {
+            shouldNotHappen.printStackTrace();
           }
+          // perform process bar update
           display.syncExec( new Runnable() {
             public void run() {
               if( !progressBar.isDisposed() ) {
                 progressBar.setSelection( selection );
                 if( selection == maximum ) {
                   button.setEnabled( true );
+                  // deactivate UI-callback mechanism
                   String id = ProgressBarTab.class.getName();
                   UICallBackUtil.deactivateUICallBack( id );
                 }
