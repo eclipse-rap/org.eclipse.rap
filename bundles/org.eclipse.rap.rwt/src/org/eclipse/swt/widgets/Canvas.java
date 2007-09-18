e25c30ecc75e4d9df45d95a59ca9c2e05eba63dd
Ralf Sternberg <rsternberg@eclipsesource.com>
2007-09-18 20:56:58 +0000

    Added Javadoc for Canvas

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Canvas.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Canvas.java
@@ -4,16 +4,37 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
  ******************************************************************************/
 
 package org.eclipse.swt.widgets;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
+
 
 /**
- * TODO [fappel]: documentation
+ * This class serves as a base class for custom widgets.
+ * It does not yet provides any drawing capabilities.
+ * <!--Instances of this class provide a surface for drawing
+ * arbitrary graphics.-->
+ * <dl>
+ * <dt><b>Styles:</b></dt>
+ * <dd>(none)</dd>
+ * <dt><b>Events:</b></dt>
+ * <dd>(none)</dd>
+ * </dl>
+ * <p>
+ * This class may be subclassed by custom control implementors
+ * who are building controls that are <em>not</em> constructed
+ * from aggregates of other controls.<!-- That is, they are either
+ * painted using SWT graphics calls or are handled by native
+ * methods.-->
+ * </p>
+ *
+ * @see Composite
  */
 public class Canvas extends Composite {
 
@@ -22,6 +43,33 @@ public class Canvas extends Composite {
     super( parent );
   }
 
+  /**
+   * Constructs a new instance of this class given its parent
+   * and a style value describing its behavior and appearance.
+   * <p>
+   * The style value is either one of the style constants defined in
+   * class <code>SWT</code> which is applicable to instances of this
+   * class, or must be built by <em>bitwise OR</em>'ing together
+   * (that is, using the <code>int</code> "|" operator) two or more
+   * of those <code>SWT</code> style constants. The class description
+   * lists the style constants that are applicable to the class.
+   * Style bits are also inherited from superclasses.
+   * </p>
+   *
+   * @param parent a composite control which will be the parent of the new instance (cannot be null)
+   * @param style the style of control to construct
+   *
+   * @exception IllegalArgumentException <ul>
+   *    <li>ERROR_NULL_ARGUMENT - if the parent is null</li>
+   * </ul>
+   * @exception SWTException <ul>
+   *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the parent</li>
+   * </ul>
+   *
+   * @see SWT
+   * @see Widget#checkSubclass
+   * @see Widget#getStyle
+   */
   public Canvas( final Composite parent, final int style ) {
     super( parent, style );
   }
