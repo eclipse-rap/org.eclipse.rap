5d44fcaab1715e4e9e866d4d50bb9b816ef41e96
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2007-12-21 16:48:29 +0000

    Formatting

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/DisposedWidgets.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/DisposedWidgets.java
@@ -8,7 +8,6 @@
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
  ******************************************************************************/
-
 package org.eclipse.rwt.internal.lifecycle;
 
 import java.util.LinkedList;
