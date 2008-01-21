fcf147124663f766a30a8089a4ce10ff1a0ff7ce
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2008-01-21 11:36:43 +0000

    Removed useless code introduced with most recent commit (Code cleanup)

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/wizard/SurveyWizard.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/wizard/SurveyWizard.java
@@ -1,9 +1,5 @@
 package org.eclipse.rap.demo.wizard;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.Wizard;
 
 public class SurveyWizard extends Wizard {
@@ -14,7 +10,6 @@ public class SurveyWizard extends Wizard {
     addPage( new MoreInformationPage() );
     addPage( new ThanksPage() );
     setWindowTitle( "RAP Survey Wizard" );
-    setNeedsProgressMonitor( true );
   }
 
   /**
@@ -24,27 +19,6 @@ public class SurveyWizard extends Wizard {
    */
   public boolean performFinish() {
     // Dismiss the wizard
-    try {
-      getContainer().run( true, true, new IRunnableWithProgress() {
-        public void run( final IProgressMonitor monitor )
-          throws InvocationTargetException, InterruptedException
-        {
-          monitor.setTaskName( "taskName" );
-          monitor.beginTask( "Doing silly things...", 100 );
-          for( int i = 0; !monitor.isCanceled() && i < 100; i++ ) {
-            monitor.worked( 1 );
-            Thread.sleep( 100 );
-          }
-          monitor.done();
-        }
-      } );
-    } catch( InvocationTargetException e ) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch( InterruptedException e ) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
     return true;
   }
 }
