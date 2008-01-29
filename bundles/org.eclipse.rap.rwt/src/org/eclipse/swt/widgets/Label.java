833ca5b2aa29e1bd8d9d60bb159e65b851f294a7
Ralf Sternberg <rsternberg@eclipsesource.com>
2008-01-29 18:07:59 +0000

    Fixed bug 216879: extra space in Label#computeSize() https://bugs.eclipse.org/bugs/show_bug.cgi?id=216879

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Label.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Label.java
@@ -12,6 +12,7 @@
 package org.eclipse.swt.widgets;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.internal.graphics.TextSizeDetermination;
 
@@ -250,7 +251,7 @@ public class Label extends Control {
       }
       Point extent
         = TextSizeDetermination.textExtent( getFont(), text, wrapWidth );
-      width = extent.x + 8;
+      width = extent.x;
       height = extent.y + 2;
     }
     if( wHint != SWT.DEFAULT ) {
