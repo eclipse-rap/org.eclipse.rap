7391c1288850543feb446aee9431aaf7ada65983
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2008-02-19 17:40:37 +0000

    Fixed compile error

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/DemoTreeViewPart.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/DemoTreeViewPart.java
@@ -1,65 +1,67 @@
 /*******************************************************************************
- * Copyright (c) 2002-2006 Innoopract Informationssysteme GmbH.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Innoopract Informationssysteme GmbH - initial API and implementation
+ * Copyright (c) 2002-2006 Innoopract Informationssysteme GmbH. All rights
+ * reserved. This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors: Innoopract Informationssysteme GmbH - initial API and
+ * implementation
  ******************************************************************************/
-
 package org.eclipse.rap.demo;
 
 import java.util.ArrayList;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.part.ViewPart;
 
-
 public class DemoTreeViewPart extends ViewPart implements IDoubleClickListener {
 
   private TreeViewer viewer;
-
   private final class LeafStarLabelDecorator implements ILabelDecorator {
-    public String decorateText(String text, Object element) {
-        if(text.startsWith("Leaf")) {
-            return text + "*";
-        }
-        return text;
+
+    public String decorateText( final String text, final Object element ) {
+      if( text.startsWith( "Leaf" ) ) {
+        return text + "*";
+      }
+      return text;
     }
 
-    public void addListener(ILabelProviderListener listener) {
+    public void addListener( final ILabelProviderListener listener ) {
     }
 
     public void dispose() {
     }
 
-    public boolean isLabelProperty(Object element, String property) {
-        return false;
+    public boolean isLabelProperty( final Object element,
+                                    final String property )
+    {
+      return false;
     }
 
-    public void removeListener(ILabelProviderListener listener) {
+    public void removeListener( final ILabelProviderListener listener ) {
     }
-}
 
-class TreeObject {
+    public Image decorateImage( final Image image, final Object element ) {
+      return null;
+    }
+  }
+  class TreeObject {
 
-    private String name;
+    private final String name;
     private String location;
     private TreeParent parent;
 
-    public TreeObject( String name ) {
+    public TreeObject( final String name ) {
       this.name = name;
     }
 
-    public TreeObject( String name, String location ) {
-        this.name = name;
-        this.location = location;
-      }
+    public TreeObject( final String name, final String location ) {
+      this.name = name;
+      this.location = location;
+    }
 
     public String getName() {
       return name;
@@ -68,8 +70,8 @@ class TreeObject {
     public String getLocation() {
       return location;
     }
-    
-    public void setParent( TreeParent parent ) {
+
+    public void setParent( final TreeParent parent ) {
       this.parent = parent;
     }
 
@@ -81,73 +83,78 @@ class TreeObject {
       return getName();
     }
   }
-  
   class TreeParent extends TreeObject {
 
-    private ArrayList children;
+    private final ArrayList children;
 
-    public TreeParent( String name ) {
+    public TreeParent( final String name ) {
       super( name );
       children = new ArrayList();
     }
 
-    public void addChild( TreeObject child ) {
+    public void addChild( final TreeObject child ) {
       children.add( child );
       child.setParent( this );
     }
 
-    public void removeChild( TreeObject child ) {
+    public void removeChild( final TreeObject child ) {
       children.remove( child );
       child.setParent( null );
     }
 
     public TreeObject[] getChildren() {
-      return ( TreeObject[] )children.toArray( new TreeObject[ children.size() ] );
+      TreeObject[] result = new TreeObject[ children.size() ];
+      children.toArray( result );
+      return result;
     }
 
     public boolean hasChildren() {
       return children.size() > 0;
     }
   }
-  
   class TreeViewerContentProvider
     implements IStructuredContentProvider, ITreeContentProvider
   {
 
     private TreeParent invisibleRoot;
 
-    public void inputChanged( Viewer v, Object oldInput, Object newInput ) {
+    public void inputChanged( final Viewer v,
+                              final Object oldInput,
+                              final Object newInput )
+    {
     }
 
     public void dispose() {
     }
 
-    public Object[] getElements( Object parent ) {
+    public Object[] getElements( final Object parent ) {
       if( parent instanceof IViewPart ) {
-        if( invisibleRoot == null )
+        if( invisibleRoot == null ) {
           initialize();
+        }
         return getChildren( invisibleRoot );
       }
       return getChildren( parent );
     }
 
-    public Object getParent( Object child ) {
+    public Object getParent( final Object child ) {
       if( child instanceof TreeObject ) {
         return ( ( TreeObject )child ).getParent();
       }
       return null;
     }
 
-    public Object[] getChildren( Object parent ) {
+    public Object[] getChildren( final Object parent ) {
       if( parent instanceof TreeParent ) {
         return ( ( TreeParent )parent ).getChildren();
       }
       return new Object[ 0 ];
     }
 
-    public boolean hasChildren( Object parent ) {
-      if( parent instanceof TreeParent )
+    public boolean hasChildren( final Object parent ) {
+      if( parent instanceof TreeParent ) {
         return ( ( TreeParent )parent ).hasChildren();
+      }
       return false;
     }
 
@@ -156,9 +163,12 @@ class TreeObject {
      * code, you will connect to a real model and expose its hierarchy.
      */
     private void initialize() {
-      TreeObject to1 = new TreeObject( "EclipseCon location", "http://maps.google.com/maps?q=5001%20Great%20America%20Pkwy%20Santa%20Clara%20CA%2095054" );
-      TreeObject to2 = new TreeObject( "Eclipse Foundation", "http://maps.google.com/maps?q=Ottawa" );
-      TreeObject to3 = new TreeObject( "Innoopract Inc", "http://maps.google.com/maps?q=Portland" );
+      TreeObject to1 = new TreeObject( "EclipseCon location",
+                                       "http://maps.google.com/maps?q=5001%20Great%20America%20Pkwy%20Santa%20Clara%20CA%2095054" );
+      TreeObject to2 = new TreeObject( "Eclipse Foundation",
+                                       "http://maps.google.com/maps?q=Ottawa" );
+      TreeObject to3 = new TreeObject( "Innoopract Inc",
+                                       "http://maps.google.com/maps?q=Portland" );
       TreeParent p1 = new TreeParent( "Locate in browser view" );
       p1.addChild( to1 );
       p1.addChild( to2 );
@@ -175,27 +185,29 @@ class TreeObject {
       invisibleRoot.addChild( root );
     }
   }
-  
+
   public void createPartControl( final Composite parent ) {
     viewer = new TreeViewer( parent );
     viewer.setContentProvider( new TreeViewerContentProvider() );
-    viewer.setLabelProvider(new DecoratingLabelProvider(new LabelProvider(),
-                                                new LeafStarLabelDecorator()));
+    viewer.setLabelProvider( new DecoratingLabelProvider( new LabelProvider(),
+                                                          new LeafStarLabelDecorator() ) );
     viewer.setInput( this );
-    viewer.addDoubleClickListener(this);
+    viewer.addDoubleClickListener( this );
     getSite().setSelectionProvider( viewer );
   }
-  
+
   public void setFocus() {
     viewer.getTree().setFocus();
   }
 
-  public void doubleClick(DoubleClickEvent event) {
-      MessageDialog.openInformation(viewer.getTree().getShell(), "Treeviewer",
-              "You doubleclicked on " + event.getSelection().toString());
+  public void doubleClick( final DoubleClickEvent event ) {
+    MessageDialog.openInformation( viewer.getTree().getShell(),
+                                   "Treeviewer",
+                                   "You doubleclicked on "
+                                       + event.getSelection().toString() );
   }
 
   public TreeViewer getViewer() {
-      return viewer;
+    return viewer;
   }
 }
