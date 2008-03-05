6fdcab3c684fb23e5b558b30f7e4ef78320906b7
Ralf Sternberg <rsternberg@eclipsesource.com>
2008-03-05 16:00:20 +0000

    Removed unused import

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Label.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Label.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002-2007 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002-2008 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,6 @@
 package org.eclipse.swt.widgets;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.SWTException;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.internal.graphics.TextSizeDetermination;
 
