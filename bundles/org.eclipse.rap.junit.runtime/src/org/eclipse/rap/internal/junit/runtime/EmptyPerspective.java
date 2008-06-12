fb890d3ed2407580860e7d1ca17f925408ed3b4e
Ralf Sternberg <rsternberg@eclipsesource.com>
2008-06-12 19:11:33 +0000

    Adjusted copyright headers for junit and junit.runtime - bug 214860: Adjust copyright headers https://bugs.eclipse.org/bugs/show_bug.cgi?id=214860

--- bundles/org.eclipse.rap.junit.runtime/src/org/eclipse/rap/internal/junit/runtime/EmptyPerspective.java
+++ bundles/org.eclipse.rap.junit.runtime/src/org/eclipse/rap/internal/junit/runtime/EmptyPerspective.java
@@ -1,4 +1,14 @@
-// Created on 02.03.2008
+/*******************************************************************************
+ * Copyright (c) 2008 Innoopract Informationssysteme GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Innoopract Informationssysteme GmbH - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.rap.internal.junit.runtime;
 
 import org.eclipse.ui.IPageLayout;
