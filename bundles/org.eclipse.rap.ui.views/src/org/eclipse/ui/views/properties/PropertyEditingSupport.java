bac3c344b2187e469a86078fb929264362acbca3
Ralf Sternberg <rsternberg@eclipsesource.com>
2008-06-12 15:24:25 +0000

    Adjusted copyright header for ui, ui.forms, and ui.views - bug 214860: Adjust copyright headers https://bugs.eclipse.org/bugs/show_bug.cgi?id=214860

--- bundles/org.eclipse.rap.ui.views/src/org/eclipse/ui/views/properties/PropertyEditingSupport.java
+++ bundles/org.eclipse.rap.ui.views/src/org/eclipse/ui/views/properties/PropertyEditingSupport.java
@@ -1,14 +1,14 @@
-/**
+/*******************************************************************************
  * Copyright (c) 2008 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Ed Merks, IBM - Initial API and implementation (bug 220843)
- *
- */
+ *     Ed Merks, IBM - Initial API and implementation (bug 220843)
+ ******************************************************************************/
+
 package org.eclipse.ui.views.properties;
 
 import org.eclipse.jface.viewers.*;
