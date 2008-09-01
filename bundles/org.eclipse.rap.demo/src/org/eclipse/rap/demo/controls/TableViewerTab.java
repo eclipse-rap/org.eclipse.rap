a1a5f846e1189f7018932c09e8caf14333b648d0
Ralf Sternberg <rsternberg@eclipsesource.com>
2008-09-01 22:48:37 +0000

    Revoke unintentionally commited change in controls demo

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/controls/TableViewerTab.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/controls/TableViewerTab.java
@@ -253,14 +253,7 @@ public class TableViewerTab extends ExampleTab {
     viewer.addFilter( viewerFilter );
     viewer.addSelectionChangedListener( new ISelectionChangedListener() {
       public void selectionChanged( final SelectionChangedEvent event ) {
-        TableItem item = ( ( Table )viewer.getControl() ).getItem( 0 );
-        String addText = ", " + item;
-        if( item != null ) {
-          addText = ", " + item.getText();
-        } else {
-          addText = "";
-        }
-        lblSelection.setText( "Selection: " + event.getSelection() + addText  );
+        lblSelection.setText( "Selection: " + event.getSelection() );
         lblSelection.getParent().layout( new Control[] { lblSelection } );
       }
     } );
