00efde1533834500cef0b2f6b275dfec450b6de6
Ralf Sternberg <rsternberg@eclipsesource.com>
2008-10-09 08:42:10 +0000

    cleaned up duplicated tests - bug 245834: [Theming] Implement generic CSS theming engine https://bugs.eclipse.org/bugs/show_bug.cgi?id=245834

--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/theme/css/CssFileReader_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/theme/css/CssFileReader_Test.java
@@ -15,7 +15,6 @@ import java.io.*;
 
 import junit.framework.TestCase;
 
-import org.eclipse.rwt.internal.theme.css.StyleSheet.SelectorWrapper;
 import org.w3c.css.sac.CSSException;
 
 
@@ -24,18 +23,6 @@ public class CssFileReader_Test extends TestCase {
   private static final String PACKAGE = "resources/theme/";
 
   private static final String TEST_SYNTAX_CSS = "TestSyntax.css";
-
-  private static final String TEST_SELECTORS_CSS = "TestSelectors.css";
-
-  private static final int ALL_RULE = 0;
-  private static final int ELEMENT_RULE = 1;
-  private static final int CLASS_RULE = 2;
-  private static final int PSEUDO_CLASS_RULE = 3;
-  private static final int ATTRIBUTE_RULE = 4;
-  private static final int ATTRIBUTE_VALUE_RULE = 5;
-  private static final int ONE_OF_ATTRIBUTE_RULE = 6;
-  private static final int COMBINED_ATTRIBUTE_RULE = 7;
-  private static final int SELECTOR_LIST_RULE = 8;
 
   public void testParseSac() throws Exception {
     ClassLoader classLoader = CssFileReader_Test.class.getClassLoader();
@@ -56,160 +43,6 @@ public class CssFileReader_Test extends TestCase {
     assertTrue( containsProblem( problems, "import rules not supported" ) );
     assertTrue( containsProblem( problems, "page rules not supported" ) );
     inStream.close();
-  }
-
-  public void testCascadeMatchAll() throws Exception {
-    StyleSheet styleSheet = getStyleSheet( TEST_SELECTORS_CSS );
-    StyleRule[] styleRules = styleSheet.getStyleRules();
-    StylableElement text = new StylableElement( "Text" );
-    text.setAttribute( "style", "SIMPLE" );
-    SelectorWrapper[] rules = styleSheet.getMatchingStyleRules( text );
-    assertNotNull( rules );
-    assertEquals( 2, rules.length );
-    assertEquals( styleRules[ ALL_RULE ].getProperties(), rules[ 0 ].propertyMap );
-    assertEquals( styleRules[ ATTRIBUTE_VALUE_RULE ].getProperties(), rules[ 1 ].propertyMap );
-  }
-
-  public void testMatchAll() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ ALL_RULE ];
-    StylableElement label = new StylableElement( "Label" );
-    assertTrue( matchingRule.matches( label ) );
-    StylableElement tree = new StylableElement( "Tree" );
-    assertTrue( matchingRule.matches( tree ) );
-    StylableElement list = new StylableElement( "NONE" );
-    assertTrue( matchingRule.matches( list ) );
-  }
-
-  public void testMatchElement() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ ELEMENT_RULE ];
-    StylableElement label = new StylableElement( "Label" );
-    assertTrue( matchingRule.matches( label ) );
-    StylableElement button = new StylableElement( "Button" );
-    assertFalse( matchingRule.matches( button ) );
-  }
-
-  public void testMatchClass() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ CLASS_RULE ];
-    StylableElement button = new StylableElement( "Button" );
-    assertFalse( matchingRule.matches( button ) );
-    button.setClass( "special" );
-    assertTrue( matchingRule.matches( button ) );
-    StylableElement label = new StylableElement( "Label" );
-    assertFalse( matchingRule.matches( label ) );
-    label.setClass( "special" );
-    assertFalse( matchingRule.matches( label ) );
-  }
-
-  public void testMatchPseudoClass() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ PSEUDO_CLASS_RULE ];
-    StylableElement list = new StylableElement( "List" );
-    assertFalse( matchingRule.matches( list ) );
-    list.setPseudoClass( "selected" );
-    assertTrue( matchingRule.matches( list ) );
-    StylableElement button = new StylableElement( "Button" );
-    assertFalse( matchingRule.matches( button ) );
-    button.setPseudoClass( "selected" );
-    assertFalse( matchingRule.matches( button ) );
-  }
-
-  public void testMatchAttribute() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ ATTRIBUTE_RULE ];
-    StylableElement text = new StylableElement( "Text" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "SIMPLE", "" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "SIMPLE", "x" );
-    assertTrue( matchingRule.matches( text ) );
-  }
-
-  public void testMatchAttributeValue() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ ATTRIBUTE_VALUE_RULE ];
-    StylableElement text = new StylableElement( "Text" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "SIMPLE" );
-    assertTrue( matchingRule.matches( text ) );
-    text.setAttribute( "style", "SIMPLE BORDER" );
-    assertFalse( matchingRule.matches( text ) );
-  }
-
-  public void testMatchOneOfAttribute() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ ONE_OF_ATTRIBUTE_RULE ];
-    StylableElement text = new StylableElement( "Text" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "x" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "BORDER", "x" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "BORDER" );
-    assertTrue( matchingRule.matches( text ) );
-    text.setAttribute( "style", "SIMPLE BORDER" );
-    assertTrue( matchingRule.matches( text ) );
-  }
-
-  public void testCombinedAttributes() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ COMBINED_ATTRIBUTE_RULE ];
-    StylableElement text = new StylableElement( "Text" );
-    text.setClass( "special" );
-    text.setAttribute( "style", "SIMPLE" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "BORDER" );
-    assertFalse( matchingRule.matches( text ) );
-    text.setAttribute( "style", "SIMPLE BORDER" );
-    assertTrue( matchingRule.matches( text ) );
-    text.setAttribute( "style", "BORDER SIMPLE" );
-    assertTrue( matchingRule.matches( text ) );
-    text.resetClass( "special" );
-    assertFalse( matchingRule.matches( text ) );
-  }
-
-  public void testSelectorList() throws Exception {
-    StyleRule[] rules = getStyleSheet( TEST_SELECTORS_CSS ).getStyleRules();
-    ElementMatcher matchingRule = rules[ SELECTOR_LIST_RULE ];
-    StylableElement tree = new StylableElement( "Tree" );
-    assertFalse( matchingRule.matches( tree ) );
-    StylableElement label = new StylableElement( "Label" );
-    assertTrue( matchingRule.matches( label ) );
-    StylableElement button = new StylableElement( "Button" );
-    assertFalse( matchingRule.matches( button ) );
-    button.setClass( "special" );
-    assertTrue( matchingRule.matches( button ) );
-    StylableElement list = new StylableElement( "List" );
-    assertFalse( matchingRule.matches( list ) );
-    list.setPseudoClass( "selected" );
-    assertTrue( matchingRule.matches( list ) );
-  }
-
-  public void testNamespaces() {
-//    Doesn't work with Batik parser
-//    StyleRule[] rules = getStyleSheet( "TestNamespaces.css" ).getStyleRules();
-//    assertEquals( 3, rules.length );
-  }
-
-  private StyleSheet getStyleSheet( final String fileName )
-    throws CSSException, IOException
-  {
-    StyleSheet result;
-    ClassLoader classLoader = CssFileReader_Test.class.getClassLoader();
-    InputStream inStream = classLoader.getResourceAsStream( PACKAGE + fileName );
-    try {
-      CssFileReader reader = new CssFileReader();
-      result = reader.parse( inStream, fileName );
-    } finally {
-      inStream.close();
-    }
-    return result;
   }
 
   private boolean containsProblem( final Object[] array, final String part ) {
