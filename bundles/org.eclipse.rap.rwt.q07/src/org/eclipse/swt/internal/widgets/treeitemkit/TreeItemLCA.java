0a73f0bb3ebc97c77efde1f408bc661dfd60f24c
Ivan Furnadjiev <ivan@eclipsesource.com>
2009-03-11 15:06:07 +0000

    Fixed - bug 267740: NPE in TreeItemLCA https://bugs.eclipse.org/bugs/show_bug.cgi?id=267740

--- bundles/org.eclipse.rap.rwt.q07/src/org/eclipse/swt/internal/widgets/treeitemkit/TreeItemLCA.java
+++ bundles/org.eclipse.rap.rwt.q07/src/org/eclipse/swt/internal/widgets/treeitemkit/TreeItemLCA.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2008 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2009 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -351,18 +351,23 @@ public final class TreeItemLCA extends AbstractWidgetLCA {
 
   private static String toCss( final Font font ) {
     StringBuffer result = new StringBuffer();
-    FontData fontData = font.getFontData()[ 0 ];
-    if( ( fontData.getStyle() & SWT.ITALIC ) != 0 ) {
-      result.append( "italic " );
+    if( font != null ) {
+      FontData fontData = font.getFontData()[ 0 ];
+      if( ( fontData.getStyle() & SWT.ITALIC ) != 0 ) {
+        result.append( "italic " );
+      }
+      if( ( fontData.getStyle() & SWT.BOLD ) != 0 ) {
+        result.append( "bold " );
+      }
+      result.append( fontData.getHeight() );
+      result.append( "px " );
+      // TODO [rh] preliminary: low budget font-name-escaping
+      String escapedName = fontData.getName().replaceAll( "\"", "" );
+      result.append( escapedName );
+    } else {
+      // TODO [if] Revise when properly implemented in TreeItem.js
+      result.append( "" );
     }
-    if( ( fontData.getStyle() & SWT.BOLD ) != 0 ) {
-      result.append( "bold " );
-    }
-    result.append( fontData.getHeight() );
-    result.append( "px " );
-    // TODO [rh] preliminary: low budget font-name-escaping
-    String escapedName = fontData.getName().replaceAll( "\"", "" );
-    result.append( escapedName );
     return result.toString();
   }
 
