04c8ea7010f9b7bf0b7e64d44fb3185d7ac53b60
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2009-04-15 09:35:42 +0000

    Organize imports

--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/widgets/Table_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/widgets/Table_Test.java
@@ -18,7 +18,6 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.internal.graphics.TextSizeDetermination;
 import org.eclipse.swt.internal.widgets.ITableAdapter;
 import org.eclipse.swt.internal.widgets.ItemHolder;
 import org.eclipse.swt.layout.FillLayout;
