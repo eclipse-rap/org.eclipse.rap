7c17c6d173f613c17932a0d8a139e97e55a5320b
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2009-04-20 09:47:07 +0000

    Changed switchThread to use same object for sync, notify and wait

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/UIThread.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/UIThread.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2008, 2009 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,9 +42,10 @@ final class UIThread
   }
 
   public void switchThread() throws InterruptedException {
-    synchronized( getLock() ) {
-      notifyAll();
-      wait();
+    Object lock = getLock();
+    synchronized( lock ) {
+      lock.notifyAll();
+      lock.wait();
     }
   }
 
