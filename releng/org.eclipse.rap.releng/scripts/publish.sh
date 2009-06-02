c60a27b5e64a30c3d6fb951658507e30b5f62d9a
Ralf Sternberg <rsternberg@eclipsesource.com>
2009-06-02 14:06:11 +0000

    publish script eventually working :-)

--- releng/org.eclipse.rap.releng/scripts/publish.sh
+++ releng/org.eclipse.rap.releng/scripts/publish.sh
@@ -1,257 +1,337 @@
 #!/bin/bash
 #
-# This script is used to publish a new build to the existing update site / repository.
-#
-# * Run this script like this:
-#
-# ./publish.sh -a rap-tooling-1.2.0-M-qualifier.zip -u "1.2/update" -r /opt/Eclipse-N-Builds/M7/
+# This script is used to publish a new RAP build to the download server.
 
-clear
+# http://wiki.eclipse.org/JarProcessor_Options
+# http://java.sun.com/j2se/1.5.0/docs/api/java/util/jar/Pack200.Packer.html
+# http://wiki.eclipse.org/Pack200
+
 # Initialize variables that represent command line args with default values
-UPDATE_SITE_PATH="1.2/update"
-REPOSITORY_ARCHIVE=
-RUNTIME_DIR=
+INPUT_ARCHIVE=
+ZIP_DOWNLOAD_PATH=
+UPDATE_SITE_PATH=
+
+JAVA_HOME=/home/ralf/tools/jvm/jdk1.5.0_19
+ECLIPSE_HOME=/home/ralf/eclipse/eclipse-SDK-3.5RC3/eclipse
+
 DOWNLOAD_LOCATION=/home/data/httpd/download.eclipse.org/rt/rap
 SIGNING_LOCATION=/opt/public/download-staging.priv/rt/rap
-TARGET=all
 
-# print usage info
 function printUsage() {
-  echo "Usage: $0 [options] [target]"
-  echo "  -u, --update-site-path    path to remote update site to merge with" 
-  echo "  -a, --tooling-archive     zip file with rap tooling"
-  echo "  -r, --runtime             path to runtime eclipse install"
-  echo
-  echo "Available targets:"
-  echo "  * toolingZip"
-  echo "  * toolingRepo"
-  echo "  * runtimeZip"
-  echo "  * runtimeRepo"
-  echo
-  echo "Example:"
-  echo "  $0 -a rap-tooling-1.2.0-M-qualifier.zip -u \"1.2/update\" -r /opt/Eclipse-N-Builds/M7/"
-  echo ""
+  cat <<EOT
+Usage: $0 [options]"
+  -a, --input-archive       input zip file
+  -u, --update-site-path    path to remote update site to merge with (relative)
+  -d, --download-location   path to zip download directory (relative)
+  -e, --eclipse-home        path to eclipse runtime installation
+  -j, --java-home           path to java home (overrides \$JAVA_HOME)
+
+Example:
+  $0 -a rap-tooling-1.2.0-M-qualifier.zip -u "1.2/update" -e /opt/Eclipse-N-Builds/M7/
+EOT
 }
 
 function parseArguments() {
-  while getopts u:a:r:h opt; do
-    case $opt in
-      a) REPOSITORY_ARCHIVE=$OPTARG ;;
-      u) UPDATE_SITE_PATH=$OPTARG ;;
-      r) RUNTIME_DIR=$OPTARG ;;
-      ?) printUsage; exit 2 ;;
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -a|--input-archive) shift; INPUT_ARCHIVE=$1 ;;
+      -u|--update-site-path) shift; UPDATE_SITE_PATH=$1 ;;
+      -d|--download-location) shift; ZIP_DOWNLOAD_PATH=$1 ;;
+      -e|--eclipse-home) shift; ECLIPSE_HOME=$1 ;;
+      -j|--java-home) shift; JAVA_HOME=$1 ;;
+      -h|--help) printUsage; exit 0 ;;
+      *) echo "invalid parameter: $1"; exit 1 ;;
     esac
+    shift
   done
-  shift `expr $OPTIND - 1`
-  TARGET=$1
-
-  if [ -z "$REPOSITORY_ARCHIVE" ]; then
-  	echo "Reposity missing"
+  if [ -z "$INPUT_ARCHIVE" ]; then
+    echo "Input zip archive missing (provide parameter -a)"
     printUsage
     exit 1
   fi
-  if [ -z "$RUNTIME_DIR" ]; then
-    echo "runtime missing"
+  INPUT_ARCHIVE_NAME=`basename "$INPUT_ARCHIVE"`
+  if echo "$INPUT_ARCHIVE" | grep -q " "; then
+    echo "Input zip file name must not contain spaces"
+    exit 1
+  fi
+  if [ -z "$JAVA_HOME" ]; then
+    echo "Java home missing, set JAVA_HOME or specify parameter -j"
     printUsage
     exit 1
   fi
-  ensureArchive $REPOSITORY_ARCHIVE
-  ensureRuntime $RUNTIME_DIR
-  ensureRemoteRepository $UPDATE_SITE_PATH
-  
-  # get absolute path and filename for archive
-  REPOSITORY_ARCHIVE=`readlink -f $REPOSITORY_ARCHIVE`
-  REPOSITORY_FILE_NAME=${REPOSITORY_ARCHIVE##*/}
-}
-
-# print failure notice and exit
-function fail() {
-  echo "Publishing failed."
-  exit 1
-}
-
-# make sure the zip file exists
-function ensureArchive() {
-  if [ ! -f "$1" ]; then
-    echo "Please provide a correct archive to process. '$1' not found"
-    fail
+  if [ ! -e "$JAVA_HOME/bin/java" ]; then
+    echo "Invalid Java home, set JAVA_HOME or specify parameter -j"
+    printUsage
+    exit 1
   fi
-  echo "Using $1 as repository archive."
-}
-
-# make sure the runtime exists
-function ensureRuntime() {
-  if [ ! -d "$1/plugins" ]; then
-    echo "Please provide a path to an eclipse runtime."
-    fail
+  if [ -z "$ECLIPSE_HOME" ]; then
+    echo "Eclipse runtime missing, set ECLIPSE_HOME or specify -e"
+    printUsage
+    exit 1
   fi
-  echo "Using $1 as runtime."
-}
-
-# make sure the remote repository exists
-function ensureRemoteRepository() {
-  if [ -z $1 ]; then
-    echo "Please provide a path to the remote update site (eg. '1.2/update')."
-    fail
-  fi
-  echo "Using $DOWNLOAD_LOCATION/$1 as remote update site."
-}
-
-# make sure the remote repository exists
-function ensurePack200() {
-  packVersion=`pack200 -V`
-  correctVersion=`grep -q 1.23 <<< $packVersion`
-  if [ $? != 0 ]; then
-    echo "We need pack200 v1.23. Installed is the following:"
-    echo $packVersion
-    fail
-  else 
-    echo "Correct version of pack200 found."
+  if [ ! -d "$ECLIPSE_HOME/plugins" ]; then
+    echo "Invalid Eclipse home, set ECLIPSE_HOME or specify -e"
+    printUsage
+    exit 1
   fi
 }
 
-# upload build for signing
+################################################################################
+# Utility function for building sub-tasks.
+
+# Uploads given srcFile to build.eclipse.org and signs it. When signing is
+# finished, the signed file is downloaded to dstFile.
+#
+# Parameter: srcFile dstFile
+#
 function signBuild() {
-  scp $REPOSITORY_ARCHIVE $username@build.eclipse.org:$SIGNING_LOCATION
-
-  echo "Initiating signing process..."
-
-  ssh -T $username@build.eclipse.org << EOI
-  cd $SIGNING_LOCATION
-  sign $REPOSITORY_FILE_NAME mail ./signedOutput
-  cd ./signedOutput
-  echo "Waiting for $REPOSITORY_FILE_NAME"
-  while [ ! -f $REPOSITORY_FILE_NAME ]
-  do
-    sleep 20
-    echo -n "."
-  done
+  local srcFile=`readlink -nm "$1"`
+  local dstFile=`readlink -nm "$2"`
+  local srcFileName="${srcFile##*/}"
+  # parameter checking
+  if [ $# -ne 2 ]; then
+    echo "Usage: signBuild srcFile dstFile"
+    return 1
+  fi
+  if [ ! -f "$srcFile" ]; then
+    echo "Source file does not exist: $srcFile"
+    return 1
+  fi
+  if [ -e "$dstFile" ]; then
+    echo "Target file exists already: $dstFile, skipping"
+    return 0
+  fi
+  # upload file
+  echo "Upload file $srcFile for signing..."
+  echo rsync -v --progress "$srcFile" $BUILD_USER@build.eclipse.org:$SIGNING_LOCATION/
+  rsync -v --progress "$srcFile" $BUILD_USER@build.eclipse.org:$SIGNING_LOCATION/
+  test $? -eq 0 || return 1
+  # initiate signing process
+  echo "Signing $srcFileName..."
+  ssh -T $BUILD_USER@build.eclipse.org << EOI
+  cd "$SIGNING_LOCATION"
+  if [ -e "signedOutput/$srcFileName" ]; then
+    echo "signed file signedOutput/$srcFileName exists already, skipping"
+  else
+    chmod g+w "$srcFileName"
+    mkdir -p signedOutput
+    sign "$srcFileName" nomail signedOutput
+    echo "Waiting for $srcFileName..."
+    while [ ! -f "signedOutput/$srcFileName" ]; do
+      sleep 20
+      echo -n "."
+    done
+  fi
+  echo
 EOI
-  
-  mkdir -p newSite
-  mkdir -p signed
-
-  echo ""
-  echo "Downloading signed jar again..."
-  scp $username@build.eclipse.org:$SIGNING_LOCATION/signedOutput/$REPOSITORY_FILE_NAME ./signed
-  ssh -T $username@build.eclipse.org << EOI
-  cd $SIGNING_LOCATION
-  rm $REPOSITORY_FILE_NAME
+  test $? -eq 0 || return 1
+  # download signed file
+  test $? -eq 0 || return 1
+  echo "Downloading signed file to $dstFile..."
+  rsync -v --progress "$BUILD_USER@build.eclipse.org:$SIGNING_LOCATION/signedOutput/$srcFileName" "$dstFile"
+  test $? -eq 0 || return 1
+  # delete file on server
+  echo "Deleting remote files..."
+  ssh -T $BUILD_USER@build.eclipse.org << EOI
+  rm "$SIGNING_LOCATION/$srcFileName"
+  rm "$SIGNING_LOCATION/signedOutput/$srcFileName"
 EOI
+  test $? -eq 0 || return 1
+  # repack zip file since the signing process yields zip files that do not work on Windows Vista
+  echo "repacking..."
+  rm -rf .unzipped && unzip -d .unzipped "$dstFile" && rm "$dstFile" \
+    && cd .unzipped && zip -r "$dstFile" . && cd .. && rm -rf .unzipped || return 1
+  echo ok
 }
 
+# Calls pack200 on the given srcFile and copies the result to dstFile.
+#
+# Parameters: srcFile dstFile
+#
 function packBuild() {
-  outputDir=./packed
-  mkdir -p $outputDir
-  # check correct version of pack200
-  ensurePack200
-  echo "Packing the archive..."
-  # TODO [bm] ensure that we really use the correct pack200
-  java -cp $ECLIPSE_LAUNCHER org.eclipse.core.launcher.Main \
-    -application org.eclipse.update.core.siteOptimizer \
-    -jarProcessor -processAll -repack -pack \
-    -digestBuilder -digestOutputDir=$outputDir \
-    -outputDir $outputDir $REPOSITORY_ARCHIVE
+  # parameter checking
+  if [ $# -ne 3 ]; then
+    echo "Usage: packBuild (normalize|pack) srcFile dstFile"
+    return 1
+  fi
+  local mode=$1
+  local srcFile=`readlink -nm "$2"`
+  local dstFile=`readlink -nm "$3"`
+  local srcFileName="${srcFile##*/}"
+  if [ "$mode" != "pack" -a "$mode" != "normalize" ]; then
+    echo "Invalid mode: $mode, use pack or normalize"
+    return 1
+  fi
+  if [ ! -e "$srcFile" ]; then
+    echo "Source file does not exist: $srcFile"
+    return 1
+  fi
+  if [ -e "$dstFile" ]; then
+    echo "Target file exists already: $dstFile, skipping"
+    return 0
+  fi
+  # Ensure that we use Java 1.5 for pack200
+  $JAVA_HOME/bin/java -version 2>&1 | grep -q "1.5.0"
+  if [ $? -ne 0 ]; then
+    echo "Incorrect Java version. 1.5.0 needed for pack200."
+    return 1
+  fi
+  mkdir -p .packed && rm -f .packed/* || return 1
+  if [ "$mode" == "normalize" ]; then
+    $JAVA_HOME/bin/java \
+      -Dorg.eclipse.update.jarprocessor.pack200=@jre \
+      -cp "$ECLIPSE_LAUNCHER" org.eclipse.core.launcher.Main \
+      -application org.eclipse.update.core.siteOptimizer \
+      -jarProcessor -processAll -repack -outputDir .packed "$srcFile"
+  else
+    $JAVA_HOME/bin/java \
+      -Dorg.eclipse.update.jarprocessor.pack200=@jre \
+      -cp "$ECLIPSE_LAUNCHER" org.eclipse.core.launcher.Main \
+      -application org.eclipse.update.core.siteOptimizer \
+      -jarProcessor -pack -outputDir .packed "$srcFile"
+#      -digestBuilder -digestOutputDir=.packed \
+  fi
+  test $? -eq 0 || return 1
+  mv ".packed/$srcFileName" "$dstFile" || return 1
+  rm -rf .packed || return 1
+  echo ok
 }
 
+# Generates p2 metadata for a given directory.
+#
+# Parameter: inputDir
+#
 function generateMetadata() {
-  java -Xmx512m -cp $ECLIPSE_LAUNCHER org.eclipse.core.launcher.Main \
-    -application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator \
-    -updateSite $1 \
-    -site file:$1/site.xml \
-    -metadataRepository file:$1 \
-    -metadataRepositoryName "Rich Ajax Platform (RAP)" \
-    -artifactRepository file:$1 \
-    -artifactRepositoryName "Rich Ajax Platform (RAP)" \
-    -compress \
-    -reusePack200Files \
-    -noDefaultIUs
+  if [ $# -ne 1 ]; then
+    echo "Usage: generateMetadata <directory>"
+    return 1
+  fi
+  # Canonicalize path, the p2 generator needs absolute paths to work correctly.
+  local inputDir=`readlink -mn "$1"`
+  if [ ! -d "$inputDir" ]; then
+    echo "No such directory: $inputDir"
+    return 1
+  fi
+  # remove exiting metadata
+  rm -f "$inputDir/artifacts.jar" && rm -f "$inputDir/content.jar" || return 1
+  # create new metadata
+  if [ -e "$inputDir/site.xml" ]; then
+    $JAVA_HOME/bin/java -Xmx512m -cp $ECLIPSE_LAUNCHER org.eclipse.core.launcher.Main \
+      -application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator \
+      -updateSite "$inputDir" \
+      -site "file:$inputDir/site.xml" \
+      -metadataRepository "file:$inputDir" \
+      -metadataRepositoryName "RAP Update Site" \
+      -artifactRepository "file:$inputDir" \
+      -artifactRepositoryName "RAP Artifacts" \
+      -compress \
+      -reusePack200Files \
+      -noDefaultIUs || return 1
+  else
+    echo no site version
+    $JAVA_HOME/bin/java -Xmx512m -cp $ECLIPSE_LAUNCHER org.eclipse.core.launcher.Main \
+      -application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator \
+      -updateSite "$inputDir" \
+      -metadataRepository "file://$inputDir" \
+      -metadataRepositoryName "RAP Update Site" \
+      -artifactRepository "file://$inputDir" \
+      -artifactRepositoryName "RAP Artifacts" \
+      -reusePack200Files -noDefaultIUs || return 1
+#      -compress
+  fi
+  echo ok
 }
 
 function getUsername() {
-  echo -n "Please enter your username for build.eclipse.org: "
-  read username
-  echo ""
+  if [ -z "$BUILD_USER" ]; then
+    echo -n "Enter username for build.eclipse.org: "
+    read BUILD_USER
+    echo
+  fi
+  test "$BUILD_USER" || return 1
 }
 
 function findLauncher() {
   # search equinox launcher
-  ECLIPSE_LAUNCHER=$RUNTIME_DIR/plugins/`ls -1 $RUNTIME_DIR/plugins | grep launcher_ | tail -n 1`
-  echo "Using the following Equinox launcher: $ECLIPSE_LAUNCHER"
+  echo "Using this Eclipse runtime: $ECLIPSE_HOME"
+  ECLIPSE_LAUNCHER=$ECLIPSE_HOME/plugins/`ls -1 $ECLIPSE_HOME/plugins | grep launcher_ | tail -n 1`
+  echo "Using this Equinox launcher: $ECLIPSE_LAUNCHER"
 }
 
-function mergeUpdateSite() {
-  mkdir -p oldSite
-  rsync -avz --progress $username@dev.eclipse.org:$DOWNLOAD_LOCATION/$UPDATE_SITE_PATH/ ./oldSite
-  unzip ./signed/$REPOSITORY_FILE_NAME -d newSite
-  java -cp $ECLIPSE_LAUNCHER org.eclipse.core.launcher.Main \
-  -application org.eclipse.update.core.standaloneUpdate \
-  -command mirror \
-  -from file://`readlink -f ./oldSite` \
-  -to ./newSite
-}
+################################################################################
+# MAIN
 
-function upload() {
-  rsync -avz --progress ./newSite/ $username@dev.eclipse.org:$DOWNLOAD_LOCATION/$UPDATE_SITE_PATH/
-}
+if [ $# -gt 0 ]; then
 
-function buildToolingZip() {
-  getUsername
+  parseArguments "$@"
+  findLauncher
+  getUsername || exit 1
+
+else
+
+  # do not call exit if no args - enables sourcing this script
+  printUsage
+
+fi
+
+if [ -n "$ZIP_DOWNLOAD_PATH" -o -n "$UPDATE_SITE_PATH" ]; then
+
+  # pack200 - normalize
+  echo "=== normalize $INPUT_ARCHIVE (pack200)"
+  packBuild normalize "$INPUT_ARCHIVE" normalized-$INPUT_ARCHIVE_NAME || exit 1
+  
   # sign
-  # metadata
-  # upload
-}
+  echo "=== sign normalized $INPUT_ARCHIVE"
+  signBuild normalized-$INPUT_ARCHIVE_NAME signed-normalized-$INPUT_ARCHIVE_NAME || exit 1
 
-function buildToolingRepo() {
-  getUsername
-  # pack200
-  # sign
-  # download
+fi
+
+if [ -n "$ZIP_DOWNLOAD_PATH" ]; then
+
+  # upload zip
+  echo "=== upload zip file to $DOWNLOAD_LOCATION/$ZIP_DOWNLOAD_PATH/$INPUT_ARCHIVE_NAME"
+  rsync -v --progress \
+    signed-normalized-$INPUT_ARCHIVE_NAME \
+    $BUILD_USER@dev.eclipse.org:$DOWNLOAD_LOCATION/$ZIP_DOWNLOAD_PATH/$INPUT_ARCHIVE_NAME
+
+fi
+
+if [ -n "$UPDATE_SITE_PATH" ]; then
+
+  # pack200 - pack
+  echo "=== pack200 signed $INPUT_ARCHIVE_NAME"
+  packBuild pack signed-normalized-$INPUT_ARCHIVE_NAME packed-signed-normalized-$INPUT_ARCHIVE_NAME || exit 1
+  rm -rf newSite && unzip packed-signed-normalized-$INPUT_ARCHIVE_NAME -d newSite || exit 1
+  if [ -d newSite/eclipse ]; then
+    mv newSite/eclipse _eclipse_ && rm -rf newSite && mv _eclipse_ newSite || exit 1
+  fi
+
+  # download old site
+  echo "=== merge repository dev.eclipse.org:$DOWNLOAD_LOCATION/$UPDATE_SITE_PATH/"
+  echo "update local copy of repository..."
+  mkdir -p sites
+  copySite=sites/${UPDATE_SITE_PATH/\//_}
+  rsync -av --delete --progress \
+    $BUILD_USER@dev.eclipse.org:$DOWNLOAD_LOCATION/$UPDATE_SITE_PATH/ \
+    $copySite/ || return 1
+
   # merge (update manager)
+  echo "merge repository..."
+  rsync -av --exclude site.xml newSite/ $copySite/ || exit 1
+  if [ -e newSite/site.xml ]; then
+    cat $copySite/site.xml >> newSite/site.xml \
+      && mv newSite/site.xml $copySite/site.xml \
+      && vi $copySite/site.xml || exit 1
+  fi
+
   # metadata
-  # pack artifacts
-  # upload
-}
+  generateMetadata $copySite || exit 1
 
-function buildRuntimeZip() {
-  getUsername
-  # sign
   # upload
-}
+  echo "=== upload repository"
+  rsync -av --progress \
+    $copySite/ \
+    $BUILD_USER@dev.eclipse.org:$DOWNLOAD_LOCATION/$UPDATE_SITE_PATH/ || exit 1
 
-function buildRuntimeRepo() {
-  getUsername
-  # pack200
-  # sign
-  # merge (copy)
-  # metadata
-  # pack artifacts
-  # upload
-}
-############################### MAIN ###########################################
+fi
 
-parseArguments "$@"
-findLauncher
-case $TARGET in
-  all)
-    buildToolingZip
-    buildToolingRepo
-    buildRuntimeZip
-    buildRuntimeRepo
-  ;;
-  toolingZip)
-    buildToolingZip
-  ;;
-  toolingRepo)
-    buildToolingRepo
-  ;;
-  runtimeZip)
-    buildRuntimeZip
-  ;;
-  runtimeRepo)
-    buildRuntimeRepo
-  ;;
-  ?)
-    echo "Invalid target: $TARGET"
-  ;;
-esac
