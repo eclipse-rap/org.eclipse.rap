ce5930a07d2556c7e506bdd3c94571e4b9e7b543
Ralf Sternberg <rsternberg@eclipsesource.com>
2009-06-29 19:30:31 +0000

    Maintained copyright header

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/theme/ThemeCssElement.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/theme/ThemeCssElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2008, 2009 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.rwt.internal.theme;
 
