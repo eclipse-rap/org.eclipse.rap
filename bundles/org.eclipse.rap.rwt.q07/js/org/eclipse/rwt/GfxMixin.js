9069652544323c5cde94f8e8db4d67ee6d088cfb
Ivan Furnadjiev <ivan@eclipsesource.com>
2009-07-06 12:51:56 +0000

    Fixed - bug 279800: Some focused widgets look strange in webkit https://bugs.eclipse.org/bugs/show_bug.cgi?id=279800

--- bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/rwt/GfxMixin.js
+++ bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/rwt/GfxMixin.js
@@ -319,6 +319,12 @@ qx.Mixin.define("org.eclipse.rwt.GfxMixin", {
               es[i] = "";
           }
         }
+        // [if] Fix for bug
+        // 279800: Some focused widgets look strange in webkit
+        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=279800
+        if( qx.core.Variant.isSet( "qx.client", "webkit" ) ) {
+          es.outline = "none";
+        }
 
         for (var i in this._htmlProperties) {
           switch( i ) {
