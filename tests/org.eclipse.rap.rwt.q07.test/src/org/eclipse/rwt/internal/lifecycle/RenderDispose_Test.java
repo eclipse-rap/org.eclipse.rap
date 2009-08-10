d1e74d32f776bc789e2178dfb022e0e3d2a603c9
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2009-08-10 22:05:35 +0000

    removed obsolete test

--- tests/org.eclipse.rap.rwt.q07.test/src/org/eclipse/rwt/internal/lifecycle/RenderDispose_Test.java
+++ tests/org.eclipse.rap.rwt.q07.test/src/org/eclipse/rwt/internal/lifecycle/RenderDispose_Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2008 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2009 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 
 package org.eclipse.rwt.internal.lifecycle;
@@ -25,51 +26,6 @@ public class RenderDispose_Test extends TestCase {
 
   private final PreserveWidgetsPhaseListener preserveWidgetsPhaseListener
     = new PreserveWidgetsPhaseListener();
-
-  // TODO [rst] reactivate when PUSH button takes part in object pooling again
-  /*
-  public void testDispose() throws IOException {
-    RWTLifeCycle lifeCycle = new RWTLifeCycle();
-    // set up the test widget hierarchy
-    Display display = new Display();
-    Composite shell = new Shell( display , SWT.NONE );
-    final Button button = new Button( shell, SWT.PUSH );
-    String displayId = DisplayUtil.getAdapter( display ).getId();
-    // first rendering: html document that contains the javaScript 'application'
-    RWTFixture.fakeNewRequest();
-    lifeCycle.execute();
-    // second rendering: initial markup that constructs the above created
-    // widget hierarchy (display, shell and button)
-    RWTFixture.fakeNewRequest();
-    Fixture.fakeRequestParam( RequestParams.UIROOT, displayId );
-    lifeCycle.execute();
-    // dispose of the button
-    button.addSelectionListener( new SelectionAdapter() {
-      public void widgetSelected( final SelectionEvent event ) {
-        button.dispose();
-      }
-    } );
-    RWTFixture.fakeNewRequest();
-    Fixture.fakeRequestParam( RequestParams.UIROOT, displayId );
-    String buttonId = WidgetUtil.getId( button );
-    Fixture.fakeRequestParam( JSConst.EVENT_WIDGET_SELECTED, buttonId );
-    lifeCycle.execute();
-
-    String expectedStart
-      =   "org.eclipse.swt.EventUtil.suspendEventHandling();"
-        + "var wm = org.eclipse.swt.WidgetManager.getInstance();"
-        + "wm.registerResetHandler( ";
-    String functionHandler = ", function( w ) {";
-    String expectedEnd
-      =   "} );wm.dispose( \"w3\" );"
-        + "qx.ui.core.Widget.flushGlobalQueues();"
-        + "org.eclipse.swt.EventUtil.resumeEventHandling();";
-    String allMarkup = Fixture.getAllMarkup();
-    assertTrue( allMarkup.startsWith( expectedStart ) );
-    assertTrue( allMarkup.indexOf( functionHandler ) != 0 );
-    assertTrue( allMarkup.endsWith( expectedEnd ) );
-  }
-  */
 
   public void testDisposeNotYetInitialized() {
     // set up the test widget hierarchy
