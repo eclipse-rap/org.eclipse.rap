d4f9e6b7cd959cccf6b3dc0a65e7a410afa4e1d9
Ralf Sternberg <rsternberg@eclipsesource.com>
2009-09-23 09:17:02 +0000

    Updated script from hudson

--- releng/org.eclipse.rap.releng/scripts/runtime-hudson.sh
+++ releng/org.eclipse.rap.releng/scripts/runtime-hudson.sh
@@ -8,12 +8,14 @@
 # Cleanup workspace dir
 rm -rf "$WORKSPACE"/*
 
-runtimeDir=/projects/rapbuild/build-runtime/3.4.1/
+runtimeDir=/home/build/rap/build-runtime/eclipse-3.4.1
+scriptsDir=`dirname $0`
 
-./build-common.sh \
+$scriptsDir/build-common.sh \
   --cvs-tag "$CVS_TAG" \
   --build-type "$BUILD_TYPE" \
   --work "$WORKSPACE" \
   --runtime "$runtimeDir" \
   --base-platform "$PLATFORM_DIR" \
-  --builder "org.eclipse.rap/releng/org.eclipse.rap.releng/runtime"
+  --builder "org.eclipse.rap/releng/org.eclipse.rap.releng/runtime" \
+  --output "$WORKSPACE"
