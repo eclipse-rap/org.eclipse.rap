d1ad176a60ab9648030cb01cf65063bb24800799
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2009-09-28 16:39:18 +0000

    Reverted changes for 193703: Shells out of a display bounds makes page scrollable

--- bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/swt/Application.js
+++ bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/swt/Application.js
@@ -35,6 +35,8 @@ qx.Class.define( "org.eclipse.swt.Application", {
                              org.eclipse.swt.Application._onResize );
     doc.removeEventListener( "keypress",
                              org.eclipse.swt.Application._onKeyPress );
+    doc.removeEventListener( "mousewheel",
+                             org.eclipse.swt.Application._onMouseWheel );
     var req = org.eclipse.swt.Request.getInstance();
     req.removeEventListener( "send", this._onSend, this );
   },
@@ -47,17 +49,13 @@ qx.Class.define( "org.eclipse.swt.Application", {
     },
 
     _onKeyPress : function( evt ) {
-      // prevent document from scrolling (see bug 193703)
-      if(    evt.getKeyIdentifier() == "Escape"
-          || evt.getKeyIdentifier() == "Up"
-          || evt.getKeyIdentifier() == "Down"
-          || evt.getKeyIdentifier() == "Left"
-          || evt.getKeyIdentifier() == "Right"
-          || evt.getKeyIdentifier() == "PageUp"
-          || evt.getKeyIdentifier() == "PageDown" ) 
-      {
+      if( evt.getKeyIdentifier() == "Escape" ) {
         evt.preventDefault();
       }
+    },
+    
+    _onMouseWheel : function( evt ) {
+      evt.preventDefault();
     },
     
     _appendWindowSize : function() {
@@ -112,6 +110,8 @@ qx.Class.define( "org.eclipse.swt.Application", {
                             org.eclipse.swt.Application._onResize );
       doc.addEventListener( "keypress",
                             org.eclipse.swt.Application._onKeyPress );
+      doc.addEventListener( "mousewheel",
+                            org.eclipse.swt.Application._onMouseWheel );
       // Initial request to obtain startup-shell
       org.eclipse.swt.Application._appendWindowSize();
       var req = org.eclipse.swt.Request.getInstance();
