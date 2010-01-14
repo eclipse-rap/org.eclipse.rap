9a728d23c35af505e76f45ab99b8a0ea6b1def96
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-01-14 15:29:57 +0000

    Fixed - bug 298181: [encoding problem] encoding of the initial html page is not specified, and thus is not always utf-8 https://bugs.eclipse.org/bugs/show_bug.cgi?id=298181

--- tests/org.eclipse.rap.rwt.testfixture/src/org/eclipse/rwt/TestResponse.java
+++ tests/org.eclipse.rap.rwt.testfixture/src/org/eclipse/rwt/TestResponse.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2009 EclipseSource and others. All rights reserved.
+ * Copyright (c) 2009, 2010 EclipseSource and others. All rights reserved.
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
@@ -18,7 +18,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 public final class TestResponse implements HttpServletResponse {
-  
+
   private ServletOutputStream outStream;
   private String contentType;
   private Map cookies = new HashMap();
@@ -29,137 +29,136 @@ public final class TestResponse implements HttpServletResponse {
   public void addCookie( final Cookie arg0 ) {
     cookies.put( arg0.getName(), arg0 );
   }
-  
+
   public Cookie getCookie( final String cookieName ) {
     return ( Cookie )cookies.get( cookieName );
   }
-  
+
   public boolean containsHeader( final String arg0 ) {
     return false;
   }
-  
+
   public String encodeURL( final String arg0 ) {
     return arg0;
   }
-  
+
   public String encodeRedirectURL( final String arg0 ) {
     return arg0;
   }
-  
+
   public String encodeUrl( final String arg0 ) {
     return arg0;
   }
-  
+
   public String encodeRedirectUrl( final String arg0 ) {
     return arg0;
   }
-  
+
   public void sendError( final int arg0, final String arg1 )
     throws IOException
   {
     errorStatus = arg0;
   }
-  
+
   public void sendError( final int arg0 ) throws IOException {
     errorStatus = arg0;
   }
-  
+
   public int getErrorStatus() {
     return errorStatus;
   }
-  
+
   public void sendRedirect( final String arg0 ) throws IOException {
     redirect = arg0;
   }
-  
+
   public String getRedirect() {
     return redirect;
   }
-  
+
   public void setDateHeader( final String arg0, final long arg1 ) {
   }
-  
+
   public void addDateHeader( final String arg0, final long arg1 ) {
   }
-  
+
   public void setHeader( final String arg0, final String arg1 ) {
     headers.put( arg0, arg1 );
   }
-  
+
   public String getHeader( final String name ) {
     return ( String )headers.get( name );
   }
-  
+
   public void addHeader( final String arg0, final String arg1 ) {
   }
-  
+
   public void setIntHeader( final String arg0, final int arg1 ) {
   }
-  
+
   public void addIntHeader( final String arg0, final int arg1 ) {
   }
-  
+
   public void setStatus( final int arg0 ) {
   }
-  
+
   public void setStatus( final int arg0, final String arg1 ) {
   }
-  
+
   public String getCharacterEncoding() {
     return null;
   }
-  
+
   public ServletOutputStream getOutputStream() throws IOException {
     return outStream;
   }
-  
+
   public void setOutputStream( final ServletOutputStream outStream ) {
     this.outStream = outStream;
   }
-  
+
   public PrintWriter getWriter() throws IOException {
     return new PrintWriter( outStream );
   }
-  
+
   public void setContentLength( final int arg0 ) {
   }
-  
+
   public void setContentType( final String contentType ) {
     this.contentType = contentType;
   }
-  
+
   public String getContentType() {
     return contentType;
   }
-  
+
   public void setBufferSize( final int arg0 ) {
   }
-  
+
   public int getBufferSize() {
     return 0;
   }
-  
+
   public void flushBuffer() throws IOException {
   }
-  
+
   public void resetBuffer() {
   }
-  
+
   public boolean isCommitted() {
     return false;
   }
-  
+
   public void reset() {
   }
-  
+
   public void setLocale( final Locale arg0 ) {
   }
-  
+
   public Locale getLocale() {
     return null;
   }
 
   public void setCharacterEncoding( String charset ) {
-    throw new UnsupportedOperationException();
   }
 }
