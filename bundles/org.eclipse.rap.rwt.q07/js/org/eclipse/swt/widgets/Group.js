710de8dbb848cd4c2791279d453e86ce3e4dc014
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-02-09 13:12:06 +0000

    Fix javascript error related to Group postLayout.

--- bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/swt/widgets/Group.js
+++ bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/swt/widgets/Group.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,7 +86,7 @@ qx.Class.define( "org.eclipse.swt.widgets.Group", {
     
     _layoutPost : function( changes ) {
       this.base( arguments, changes );
-      this._frameObject._layoutPost();
+      this._frameObject._layoutPost( changes );
     }
   }
 });
