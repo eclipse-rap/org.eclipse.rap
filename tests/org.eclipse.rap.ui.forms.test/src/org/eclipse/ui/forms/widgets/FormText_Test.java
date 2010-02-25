a2d3f881cceb0b3d87a1e96556055d141b42396d
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-02-25 23:23:04 +0000

    Fix broken test, because of difference in heights (1px) of Graphics#textExtent and Graphics#stringExtent methods.

--- tests/org.eclipse.rap.ui.forms.test/src/org/eclipse/ui/forms/widgets/FormText_Test.java
+++ tests/org.eclipse.rap.ui.forms.test/src/org/eclipse/ui/forms/widgets/FormText_Test.java
@@ -57,7 +57,7 @@ public class FormText_Test extends TestCase {
       + "<p>Second paragraph</p>"
       + "</form>";
     formText.setText( text, true, false );
-    Point expected = new Point( 86, 85 );
+    Point expected = new Point( 86, 90 );
     assertEquals( expected, formText.computeSize( SWT.DEFAULT, SWT.DEFAULT ) );
     // fixed size
     expected = new Point( 50, 50 );
