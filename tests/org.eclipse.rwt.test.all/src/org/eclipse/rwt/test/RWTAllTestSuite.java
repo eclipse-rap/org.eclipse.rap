8f9f7695c89b0fb50d5dc6d69f373320b2de8c73
Holger Staudacher <hstaudacher@eclipsesource.com>
2010-03-12 07:52:44 +0000

    RESOLVED - 305638: Add Themes tests to RWTAllTestSuite https://bugs.eclipse.org/bugs/show_bug.cgi?id=305638

--- tests/org.eclipse.rwt.test.all/src/org/eclipse/rwt/test/RWTAllTestSuite.java
+++ tests/org.eclipse.rwt.test.all/src/org/eclipse/rwt/test/RWTAllTestSuite.java
@@ -5,6 +5,7 @@ import junit.framework.TestSuite;
 
 import org.eclipse.RWTHostTestSuite;
 import org.eclipse.RWTQ07TestSuite;
+import org.eclipse.rap.rwt.themes.test.ThemesTestSuite;
 
 public class RWTAllTestSuite {
 
@@ -13,6 +14,7 @@ public class RWTAllTestSuite {
     // $JUnit-BEGIN$
     suite.addTest( RWTHostTestSuite.suite() );
     suite.addTest( RWTQ07TestSuite.suite() );
+    suite.addTest( ThemesTestSuite.suite() );
     // $JUnit-END$
     return suite;
   }
