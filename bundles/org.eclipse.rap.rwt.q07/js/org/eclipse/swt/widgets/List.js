929243f21c1d530eef1dadd03c5e9dcc9f1c4481
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-06-30 11:19:45 +0000

    Fixed - bug 316424: List component isn't created when it is invisible https://bugs.eclipse.org/bugs/show_bug.cgi?id=316424

--- bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/swt/widgets/List.js
+++ bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/swt/widgets/List.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -217,7 +217,7 @@ qx.Class.define( "org.eclipse.swt.widgets.List", {
     },
     
     _onSendRequest : function( evt ) {
-      var topIndex = this._getTopIndex();
+      var topIndex = this._isCreated ? this._getTopIndex() : 0;
       if( this._topIndex != topIndex ) {
         var widgetManager = org.eclipse.swt.WidgetManager.getInstance();
         var id = widgetManager.findIdByWidget( this );
