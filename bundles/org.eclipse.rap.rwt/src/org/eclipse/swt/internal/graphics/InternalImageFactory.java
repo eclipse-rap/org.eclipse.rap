5eaa09439b6ed460d6695fc831565bdfccfa187c
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-10-05 07:03:24 +0000

    Fixed - bug 326888: InternalImageFactory uses same HashCode for different images https://bugs.eclipse.org/bugs/show_bug.cgi?id=326888

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/internal/graphics/InternalImageFactory.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/internal/graphics/InternalImageFactory.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2010 EclipseSource and others. All rights reserved.
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
@@ -181,6 +181,9 @@ public final class InternalImageFactory {
       for( int i = 0; i < imageData.data.length; i++ ) {
         result = 31 * result + imageData.data[ i ];
       }
+    }
+    if( imageData.palette != null  ) {
+      result = result * 31 + imageData.palette.hashCode();
     }
     result = result * 31 + imageData.alpha;
     result = result * 31 + imageData.transparentPixel;
