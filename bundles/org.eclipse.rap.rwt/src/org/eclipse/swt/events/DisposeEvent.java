1a7f5b51162501e685dafd4a7967ee022392382f
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-11-05 10:59:37 +0000

    Fixed - bug 328043: ClassCastException in DisposeEvent https://bugs.eclipse.org/bugs/show_bug.cgi?id=328043

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/events/DisposeEvent.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/events/DisposeEvent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2007 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,24 +7,27 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 
 package org.eclipse.swt.events;
 
 import org.eclipse.rwt.Adaptable;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Widget;
 
 
 /**
  * Instances of this class are sent as a result of
  * widgets being disposed.
  *
- * <p><strong>IMPORTANT:</strong> All <code>public static</code> members of 
- * this class are <em>not</em> part of the RWT public API. They are marked 
- * public only so that they can be shared within the packages provided by RWT. 
+ * <p><strong>IMPORTANT:</strong> All <code>public static</code> members of
+ * this class are <em>not</em> part of the RWT public API. They are marked
+ * public only so that they can be shared within the packages provided by RWT.
  * They should never be accessed from application code.
  * </p>
- * 
+ *
  * @see DisposeListener
  */
 public final class DisposeEvent extends TypedEvent {
@@ -36,15 +39,25 @@ public final class DisposeEvent extends TypedEvent {
   private static final Class LISTENER = DisposeListener.class;
 
   /**
-   * Constructs a new instance of this class. 
+   * Constructs a new instance of this class.
    * <p><strong>IMPORTANT:</strong> This method is <em>not</em> part of the RWT
    * public API. It is marked public only so that it can be shared
-   * within the packages provided by RWT. It should never be accessed 
+   * within the packages provided by RWT. It should never be accessed
    * from application code.
    * </p>
    */
-  public DisposeEvent( final Object source ) {
+  public DisposeEvent( final Widget source ) {
     super( source, WIDGET_DISPOSED );
+  }
+
+  /**
+   * Constructs a new instance of this class based on the
+   * information in the given untyped event.
+   *
+   * @param e the untyped event containing the information
+   */
+  public DisposeEvent( final Event event ) {
+    super( event );
   }
 
   protected void dispatchToObserver( final Object listener ) {
@@ -54,27 +67,27 @@ public final class DisposeEvent extends TypedEvent {
   protected Class getListenerType() {
     return LISTENER;
   }
-  
+
   protected boolean allowProcessing() {
     return true;
   }
 
-  public static void addListener( final Adaptable adaptable, 
+  public static void addListener( final Adaptable adaptable,
                                   final DisposeListener listener )
   {
     addListener( adaptable, LISTENER, listener );
   }
 
-  public static void removeListener( final Adaptable adaptable, 
+  public static void removeListener( final Adaptable adaptable,
                                      final DisposeListener listener )
   {
     removeListener( adaptable, LISTENER, listener );
   }
-  
+
   public static boolean hasListener( final Adaptable adaptable ) {
     return hasListener( adaptable, LISTENER );
   }
-  
+
   public static Object[] getListeners( final Adaptable adaptable ) {
     return getListener( adaptable, LISTENER );
   }
--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/events/DisposeEvent_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/events/DisposeEvent_Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2007 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 
 package org.eclipse.swt.events;
@@ -20,20 +21,20 @@ import org.eclipse.swt.widgets.*;
 
 
 public class DisposeEvent_Test extends TestCase {
-  
+
   private static final String WIDGET_DISPOSED = "widgetDiposed|";
-  
+
   private String log = "";
-  
+
   protected void setUp() throws Exception {
     Fixture.setUp();
     Fixture.fakePhase( PhaseId.PROCESS_ACTION );
   }
-  
+
   protected void tearDown() throws Exception {
     Fixture.tearDown();
   }
-  
+
   public void testAddRemoveListener() {
     DisposeListener listener = new DisposeListener() {
       public void widgetDisposed( final DisposeEvent event ) {
@@ -43,15 +44,34 @@ public class DisposeEvent_Test extends TestCase {
     Display display = new Display();
     Composite shell = new Shell( display , SWT.NONE );
     shell.addDisposeListener( listener );
-    
-    DisposeEvent event = new DisposeEvent( shell ); 
+
+    DisposeEvent event = new DisposeEvent( shell );
     event.processEvent();
     assertEquals( WIDGET_DISPOSED, log );
-    
+
     log = "";
     shell.removeDisposeListener( listener );
-    event = new DisposeEvent( shell ); 
+    event = new DisposeEvent( shell );
     event.processEvent();
     assertEquals( "", log );
+  }
+
+  // bug 328043
+  public void testUntypedEvent() {
+    DisposeListener listener = new DisposeListener() {
+      public void widgetDisposed( final DisposeEvent event ) {
+        log += WIDGET_DISPOSED;
+      }
+    };
+    Display display = new Display();
+    Composite shell = new Shell( display , SWT.NONE );
+    shell.addDisposeListener( listener );
+
+    Event event = new Event();
+    event.widget = shell;
+    event.type = SWT.Dispose;
+    event.display = display;
+    shell.notifyListeners( SWT.Dispose, event );
+    assertEquals( WIDGET_DISPOSED, log );
   }
 }
