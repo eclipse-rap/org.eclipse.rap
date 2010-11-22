e1948227d1b1ee350daeefecabb5e359429d44ba
Ralf Sternberg <rsternberg@eclipsesource.com>
2010-11-22 15:50:46 +0000

    Updated JavaDoc which pointed to internal API

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/service/IServiceHandler.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/service/IServiceHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2007 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.rwt.service;
 
@@ -14,44 +15,46 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 
 
-/** 
- * <p>A service handler is responsible for taking a request and sending
- * an appropriate response by bypassing the standard lifecycle. Clients
- * are free to implement custom service handlers. Implementing a custom
- * service handler involves three steps:
- * <ul><li>Implementing the IServiceHandler interface, e.g.
+/**
+ * <p>
+ * A service handler can be used to process requests that bypass the standard
+ * request lifecycle. Clients are free to implement custom service handlers to
+ * deliver custom content. Implementing a custom service handler involves three
+ * steps:
+ * </p>
+ * <ul>
+ * <li>Implementing the IServiceHandler interface, e.g.
  * <pre>
  * public class MyServiceHandler implements IServiceHandler {
+ * 
  *   public void service() throws IOException, ServletException {
- *     HttpServletResponse response = ContextProvider.getResponse();
- *     response.getWriter().write( "Hello World" );
+ *     HttpServletResponse response = RWT.getResponse();
+ *     response.getWriter().write( &quot;Hello World&quot; );
  *   }
  * }
  * </pre>
  * </li>
- * <li>Registering the service handler and associating it with a request parameter 
- * value.
+ * <li>Registering the service handler and associating it with a request
+ * parameter value.
  * <pre>
- *     RWT.getServiceManager().registerServiceHandler( "myServiceHandler",
- *                                                     new MyServiceHandler() );
+ * RWT.getServiceManager().registerServiceHandler( &quot;myServiceHandler&quot;,
+ *                                                 new MyServiceHandler() );
  * </pre>
- * <br /><br />
- *</li> 
+ * </li>
  * <li>Constructing the URL to invoke the service handler. The URL must contain
- * the agreed on parameter value like this: 
+ * the agreed parameter value like this:
  * <code>http://localhost:9090/rap?custom_service_handler=myServiceHandler</code>.
  * The following example code snippet achieves this
  * <pre>
  * StringBuffer url = new StringBuffer();
  * url.append( RWT.getRequest().getContextPath() );
  * url.append( RWT.getRequest().getServletPath() );
- * url.append( "?" );
- * url.append( IServiceHandler.REQUEST_PARAM ); 
- * url.append( "=myServiceHandler" );
+ * url.append( &quot;?&quot; );
+ * url.append( IServiceHandler.REQUEST_PARAM );
+ * url.append( &quot;=myServiceHandler&quot; );
  * String encodedURL = RWT.getResponse().encodeURL( url.toString() );
  * </pre>
  * </li>
- * </p>
  * 
  * @since 1.0
  */
@@ -64,7 +67,7 @@ public interface IServiceHandler {
   static final String REQUEST_PARAM = "custom_service_handler";
 
   /**
-   * <p>This method is called by the RAP lifecycle to allow the service
+   * <p>This method is called by the request lifecycle to allow the service
    * handler to respond to a request.</p>
    * 
    * @throws IOException
