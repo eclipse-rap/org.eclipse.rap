4cfa1664b670277ee957945352988fe534bd60ce
Ralf Sternberg <rsternberg@eclipsesource.com>
2010-11-28 06:25:59 +0000

    Fixed directory in script

--- releng/org.eclipse.rap.releng/scripts/update-scripts.sh
+++ releng/org.eclipse.rap.releng/scripts/update-scripts.sh
@@ -5,5 +5,5 @@ CVS_PATH=org.eclipse.rap/releng/org.eclipse.rap.releng/scripts
 CVS_TAG=HEAD
 
 cd /home/build/rap
-cvs -Q -f -d:pserver:anonymous@dev.eclipse.org:/cvsroot/rt checkout -d ./scripts2 -r "$CVS_TAG" "$CVS_PATH"
+cvs -Q -f -d:pserver:anonymous@dev.eclipse.org:/cvsroot/rt checkout -d ./scripts -r "$CVS_TAG" "$CVS_PATH"
 cd -
