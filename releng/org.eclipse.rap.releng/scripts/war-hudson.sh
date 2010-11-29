d14829f88d23757c17e4f0905c6cd2fd935bfc4e
Ralf Sternberg <rsternberg@eclipsesource.com>
2010-11-29 08:59:39 +0000

    Disabled tomcat deployment in war build, noone used it, and it didn't run since months anyway

--- releng/org.eclipse.rap.releng/scripts/war-hudson.sh
+++ releng/org.eclipse.rap.releng/scripts/war-hudson.sh
@@ -6,11 +6,11 @@
 #
 
 runtimeDir=/home/build/rap/build-runtimes/eclipse-3.4.1
-tomcatDir=/home/build/rap/apache-tomcat-6.0.18/
+#tomcatDir=/home/build/rap/apache-tomcat-6.0.18/
 
 scriptsDir=`dirname $0`
 
-$scriptsDir/build-common.sh \
+exec $scriptsDir/build-common.sh \
   --cvs-tag "$CVS_TAG" \
   --build-type "$BUILD_TYPE" \
   --work "$WORKSPACE" \
@@ -18,28 +18,29 @@ $scriptsDir/build-common.sh \
   --base-platform "$PLATFORM_DIR" \
   --builder "org.eclipse.rap/releng/org.eclipse.rap.releng/warbuild"
 
-# start build-internal tomcat
-$tomcatDir/bin/startup.sh
+## start build-internal tomcat
+#$tomcatDir/bin/startup.sh
+#
+## give tomcat a chance to start
+#sleep 1m
+#
+#if [ $? = 0 ]; then
+#  oldDeployment=`stat -c %Y $tomcatDir/work/Catalina/localhost/rapdemo/`
+#
+#  echo "Deploy new war archive"
+#  cp "`echo $WORKSPACE`/output/rapdemo.war" $tomcatDir/webapps
+#
+#  # give tomcat a chance to deploy
+#  sleep 1m
+# 
+#  # check if the deployment is newer then the last one
+#  newDeployment=`stat -c %Y $tomcatDir/work/Catalina/localhost/rapdemo/`
+#  result=0;
+#  if [ $oldDeployment -ge $newDeployment ]; then
+#    echo "Demo War archive was not successfully deployed to Tomcat."
+#    result=42 
+#  fi
+#  
+#  exit $result
+#fi
 
-# give tomcat a chance to start
-sleep 1m
-
-if [ $? = 0 ]; then
-  oldDeployment=`stat -c %Y $tomcatDir/work/Catalina/localhost/rapdemo/`
-
-  echo "Deploy new war archive"
-  cp "`echo $WORKSPACE`/output/rapdemo.war" $tomcatDir/webapps
-
-  # give tomcat a chance to deploy
-  sleep 1m
- 
-  # check if the deployment is newer then the last one
-  newDeployment=`stat -c %Y $tomcatDir/work/Catalina/localhost/rapdemo/`
-  result=0;
-  if [ $oldDeployment -ge $newDeployment ]; then
-    echo "Demo War archive was not successfully deployed to Tomcat."
-    result=42 
-  fi
-  
-  exit $result
-fi
