53fc4343adaf62c68cce8a1106eb34093a90f130
Ivan Furnadjiev <ivan@eclipsesource.com>
2010-11-29 20:15:11 +0000

    Set lines visible on tree creation.

--- bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/controls/TreeTab.java
+++ bundles/org.eclipse.rap.demo/src/org/eclipse/rap/demo/controls/TreeTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -229,6 +229,7 @@ public class TreeTab extends ExampleTab {
     } );
     tree.setSelection( tree.getItem( 0 ) );
     tree.setHeaderVisible( true );
+    tree.setLinesVisible( linesVisible );
     if( addMouseListener ) {
       MouseListener listener = new MouseListener(  ) {
         public void mouseDoubleClick( MouseEvent e ) {
