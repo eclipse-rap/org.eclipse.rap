3f55324dfc7286e1acb30c2be62ef662e8dc9dde
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-02-08 20:56:21 +0000

    Fixed copyright header, minor refactoring

--- tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/tests/DateTimeDateTest.js
+++ tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/tests/DateTimeDateTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 20011 EclipseSource and others. All rights reserved.
+ * Copyright (c) 2011 EclipseSource and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -7,24 +7,23 @@
  * Contributors:
  *   EclipseSource - initial API and implementation
  ******************************************************************************/
-
- qx.Class.define( "org.eclipse.rwt.test.tests.DateTimeDateTest", {
+qx.Class.define( "org.eclipse.rwt.test.tests.DateTimeDateTest", {
   extend : qx.core.Object,
-  
+
   construct : function() {
     this.base( arguments );
-    this.testUtil = org.eclipse.rwt.test.fixture.TestUtil;        
+    this.testUtil = org.eclipse.rwt.test.fixture.TestUtil;
   },
-  
+
   members : {
-  
+
     testCreateDateTimeDate : function() {
       var dateTime = this._createDefaultDateTime();
       assertTrue( dateTime instanceof org.eclipse.swt.widgets.DateTimeDate );
       assertEquals( "datetime-date", dateTime.getAppearance() );
       dateTime.destroy();
     },
-        
+
     testSendAllFieldsTogether : function() {
       var dateTime = this._createDefaultDateTime();
       dateTime.setDay( 10 );
@@ -33,23 +32,15 @@
       this.testUtil.clearRequestLog();
       dateTime._sendChanges();
       assertEquals( 0, this.testUtil.getRequestsSend() );
-      assertEquals(
-        10,
-        org.eclipse.swt.Request.getInstance()._parameters[ "w3.day" ]
-      );
-      assertEquals(
-        10,
-        org.eclipse.swt.Request.getInstance()._parameters[ "w3.month" ]
-      );
-      assertEquals(
-        2010,
-        org.eclipse.swt.Request.getInstance()._parameters[ "w3.year" ]
-      );
+      var req = org.eclipse.swt.Request.getInstance();
+      assertEquals( 10, req._parameters[ "w3.day" ] );
+      assertEquals( 10, req._parameters[ "w3.month" ] );
+      assertEquals( 2010, req._parameters[ "w3.year" ] );
     },
-    
+
     //////////
     // Helpers
-    
+
     _createDefaultDateTime : function() {
       var testUtil = org.eclipse.rwt.test.fixture.TestUtil;
       var style = "medium";
@@ -74,7 +65,7 @@
                             "Thursday",
                             "Friday",
                             "Saturday" ];
-      var weekdayShortNames = [ "", 
+      var weekdayShortNames = [ "",
                                 "Sun",
                                 "Mon",
                                 "Tue",
@@ -82,7 +73,6 @@
                                 "Thu",
                                 "Fri",
                                 "Sat" ];
-                        
       var dateSeparator = "/";
       var datePattern = "MDY";
       var dateTime = new org.eclipse.swt.widgets.DateTimeDate( style,
@@ -98,7 +88,7 @@
       testUtil.flush();
       return dateTime;
     }
-    
+
   }
-  
+
 } );
