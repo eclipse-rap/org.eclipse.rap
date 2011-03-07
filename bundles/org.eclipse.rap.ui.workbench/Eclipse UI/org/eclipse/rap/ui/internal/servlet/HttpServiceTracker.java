5bfddb72d3a1b674d6f837a40ff72bbf2ed6ce06
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-03-07 22:44:12 +0000

    United registration of servlets and resources, eliminated resource manager wrapper - bug 337100: Simplify resource registration in ResourceManagerFactory https://bugs.eclipse.org/bugs/show_bug.cgi?id=337100

--- bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/rap/ui/internal/servlet/HttpServiceTracker.java
+++ bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/rap/ui/internal/servlet/HttpServiceTracker.java
@@ -12,19 +12,27 @@
  ******************************************************************************/
 package org.eclipse.rap.ui.internal.servlet;
 
-import java.text.MessageFormat;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.http.registry.HttpContextExtensionService;
 import org.eclipse.rwt.internal.engine.RWTContext;
 import org.eclipse.rwt.internal.engine.RWTContextUtil;
 import org.eclipse.rwt.internal.engine.RWTDelegate;
+import org.eclipse.rwt.internal.resources.ResourceManagerImpl;
+import org.eclipse.rwt.internal.service.ContextProvider;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.osgi.framework.BundleContext;
@@ -35,29 +43,66 @@ import org.osgi.util.tracker.ServiceTracker;
 
 
 public class HttpServiceTracker extends ServiceTracker {
+
+  private static final class HttpContextWrapper implements HttpContext {
+
+    private final HttpContext context;
+
+    private HttpContextWrapper( HttpContext context ) {
+      Assert.isNotNull( context );
+      this.context = context;
+    }
+
+    public String getMimeType( String name ) {
+      return context.getMimeType( name );
+    }
+
+    public URL getResource( String name ) {
+      URL result = null;
+      try {
+        // Preliminary fix for bug 268759
+        // 268759: ResourceManager handles non-existing resources incorrectly
+        File file = new File( name );
+        if( file.exists() && !file.isDirectory() ) {
+          result = new URL( "file", "", name );
+        }
+      } catch( MalformedURLException shouldNotHappen ) {
+        throw new RuntimeException( shouldNotHappen );
+      }
+      return result;
+    }
+
+    public boolean handleSecurity( HttpServletRequest request, HttpServletResponse response ) {
+      return true;
+    }
+  }
+
   public static final String DEFAULT_SERVLET = "rap";
   public static final String ID_HTTP_CONTEXT = "org.eclipse.rap.httpcontext";
 
   private final List servletAliases;
-  private RWTContext rwtContext;
+  private final String resourceAlias;
   private HttpContextExtensionService httpCtxExtService;
 
   public HttpServiceTracker( BundleContext context ) {
     super( context, HttpService.class.getName(), null );
     servletAliases = new LinkedList();
+    resourceAlias = ResourceManagerImpl.RESOURCES;
   }
 
   public Object addingService( ServiceReference reference ) {
-    rwtContext = createAndInitializeRWTContext();
     HttpService httpService = getHttpService( reference );
     HttpContext httpContext = getHttpContext( reference );
-    ensureDefaultAlias();
-    registerServlets( httpService, httpContext );
+    HttpContext wrappedHttpContext = createContextWrapper( httpService, httpContext );
+    RWTContext rwtContext = createAndInitializeRWTContext();
+    registerServlets( httpService, wrappedHttpContext, rwtContext );
+    registerResourceDir( httpService, wrappedHttpContext, rwtContext );
     return httpService;
   }
 
   public void removedService( ServiceReference reference, Object service ) {
     HttpService httpService = ( HttpService )service;
+    deregisterResourceDir( httpService );
     deregisterServlets( httpService );
     super.removedService( reference, service );
   }
@@ -73,8 +118,8 @@ public class HttpServiceTracker extends ServiceTracker {
   private void openWhenHttpContextServiceHasBeenStarted() {
     String service = HttpContextExtensionService.class.getName();
     ServiceTracker httpContextTracker = new ServiceTracker( context, service, null ) {
-      public Object addingService( final ServiceReference reference ) {
-        HttpContextExtensionService result = (HttpContextExtensionService)super.addingService( reference );
+      public Object addingService( ServiceReference reference ) {
+        Object result = super.addingService( reference );
         httpCtxExtService = createHttpCtxExtService( reference );
         HttpServiceTracker.super.open();
         return result;
@@ -86,18 +131,16 @@ public class HttpServiceTracker extends ServiceTracker {
     };
     httpContextTracker.open();
   }
-  
-  private void ensureDefaultAlias() {
-    if( servletAliases.size() == 0 ) {
-      servletAliases.add( DEFAULT_SERVLET );
-    }
-  }
 
-  private void registerServlets( HttpService httpService, HttpContext httpContext ) {
+  private void registerServlets( HttpService httpService,
+                                 HttpContext httpContext,
+                                 RWTContext rwtContext )
+  {
+    ensureDefaultAlias();
     Iterator aliases = servletAliases.iterator();
     while( aliases.hasNext() ) {
       String alias = ( String )aliases.next();
-      registerServlet( alias, httpService, httpContext );
+      registerServlet( alias, httpService, httpContext, rwtContext );
     }
   }
 
@@ -105,26 +148,55 @@ public class HttpServiceTracker extends ServiceTracker {
     Iterator aliases = servletAliases.iterator();
     while( aliases.hasNext() ) {
       String alias = ( String )aliases.next();
-      httpService.unregister( "/" + alias );
+      deregisterAlias( alias, httpService );
     }
   }
 
-  private void registerServlet( String name, HttpService httpService, HttpContext httpContext ) {
+  private void registerServlet( String name,
+                                HttpService httpService,
+                                HttpContext httpContext,
+                                RWTContext rwtContext )
+  {
     try {
       RWTDelegate handler = new RWTDelegate();
       httpService.registerServlet( "/" + name, handler, null, httpContext );
       ServletContext servletContext = handler.getServletContext();
       RWTContextUtil.registerRWTContext( servletContext, rwtContext );
-    } catch( Exception e ) {
-      String text = "Could not register servlet mapping ''{0}''.";
-      Object[] param = new Object[] { name };
-      String msg = MessageFormat.format( text, param );
-      Status status = new Status( IStatus.ERROR,
-                                  PlatformUI.PLUGIN_ID,
-                                  IStatus.OK,
-                                  msg,
-                                  e );
-      WorkbenchPlugin.getDefault().getLog().log( status );
+    } catch( Exception exception ) {
+      logError( "Failed to register servlet " + name, exception );
+    }
+  }
+
+  private void registerResourceDir( HttpService httpService,
+                                    HttpContext httpContext,
+                                    RWTContext rwtContext )
+  {
+    if( httpService != null ) {
+      String contextRoot = getContextRoot( rwtContext );
+      String location = contextRoot + "/" + ResourceManagerImpl.RESOURCES;
+      try {
+        httpService.registerResources( "/" + resourceAlias, location, httpContext );
+      } catch( Exception exception ) {
+        logError( "Failed to register resource alias", exception );
+      }
+    }
+  }
+
+  private void deregisterResourceDir( HttpService httpService ) {
+    deregisterAlias( resourceAlias, httpService );
+  }
+
+  private void deregisterAlias( String alias, HttpService httpService ) {
+    try {
+      httpService.unregister( "/" + alias );
+    } catch( Exception exception ) {
+      logError( "Failed to unregister servlet " + alias, exception );
+    }
+  }
+
+  private void ensureDefaultAlias() {
+    if( servletAliases.size() == 0 ) {
+      servletAliases.add( DEFAULT_SERVLET );
     }
   }
 
@@ -132,7 +204,7 @@ public class HttpServiceTracker extends ServiceTracker {
     return httpCtxExtService.getHttpContext( httpServiceReference, ID_HTTP_CONTEXT );
   }
 
-  private RWTContext createAndInitializeRWTContext() {
+  private static RWTContext createAndInitializeRWTContext() {
     RWTContext result = RWTContextUtil.createRWTContext();
     RWTContextUtil.runWithInstance( result, new EngineConfigWrapper() );
     return result;
@@ -144,5 +216,38 @@ public class HttpServiceTracker extends ServiceTracker {
 
   private HttpContextExtensionService createHttpCtxExtService( ServiceReference reference ) {
     return ( HttpContextExtensionService )context.getService( reference );
+  }
+
+  private static String getContextRoot( RWTContext rwtContext ) {
+    final String[] result = new String[ 1 ];
+    RWTContextUtil.runWithInstance( rwtContext, new Runnable() {
+      public void run() {
+        String contextRoot = ContextProvider.getWebAppBase();
+        result[ 0 ] = ( new Path( contextRoot ) ).toString();
+      }
+    } );
+    return result[ 0 ];
+  }
+
+  private static HttpContext createContextWrapper( HttpService httpService,
+                                                   HttpContext httpContext )
+  {
+    HttpContext result;
+    if( httpContext != null ) {
+      result = new HttpContextWrapper( httpContext );
+    } else {
+      HttpContext defaultHttpContext = httpService.createDefaultHttpContext();
+      result = new HttpContextWrapper( defaultHttpContext );
+    }
+    return result;
+  }
+
+  private static void logError( String message, Exception exception ) {
+    Status status = new Status( IStatus.ERROR,
+                                PlatformUI.PLUGIN_ID,
+                                IStatus.OK,
+                                message,
+                                exception );
+    WorkbenchPlugin.getDefault().getLog().log( status );
   }
 }
--- bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/rap/ui/internal/servlet/ResourceManagerFactory.java
+++ bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/rap/ui/internal/servlet/ResourceManagerFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2006, 2011 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,247 +11,20 @@
  ******************************************************************************/
 package org.eclipse.rap.ui.internal.servlet;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Enumeration;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.core.runtime.*;
-import org.eclipse.equinox.http.registry.HttpContextExtensionService;
 import org.eclipse.rwt.Adaptable;
 import org.eclipse.rwt.internal.resources.JsConcatenator;
 import org.eclipse.rwt.internal.resources.ResourceManagerImpl;
-import org.eclipse.rwt.internal.service.ContextProvider;
 import org.eclipse.rwt.resources.IResourceManager;
 import org.eclipse.rwt.resources.IResourceManagerFactory;
-import org.eclipse.ui.internal.WorkbenchPlugin;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.http.*;
 
 
 final class ResourceManagerFactory implements IResourceManagerFactory {
 
-  private final class HttpContextWrapper implements HttpContext {
-    private final HttpContext context;
-    
-    private HttpContextWrapper( final HttpContext context ) {
-      Assert.isNotNull( context );
-      this.context = context;
-    }
-    
-    public String getMimeType( final String name ) {
-      return context.getMimeType( name );
-    }
-    
-    public URL getResource( final String name ) {
-      URL result = null;
-      try {
-        // Preliminary fix for bug 268759
-        // 268759: ResourceManager handles non-existing resources incorrectly
-        File file = new File( name );
-        if( file.exists() && !file.isDirectory() ) {
-          result = new URL( "file", "", name );
-        }
-      } catch( final MalformedURLException shouldNotHappen ) {
-        throw new RuntimeException( shouldNotHappen );
-      }
-      return result;
-    }
-    
-    public boolean handleSecurity( final HttpServletRequest request,
-                                   final HttpServletResponse response )
-    {
-      return true;
-    }
-  }
-  
-  private final class ResourceManagerWrapper 
-    implements IResourceManager, Adaptable
-  {
-    private final IResourceManager resourceManager;
-    // TODO [fappel]: think of a better solution
-    // This field is set to true the first time the HttpService is used.
-    // If the HttpService isn't available anymore we assume that the
-    // system is in shutdown process.
-    private boolean httpServiceLoaded;
-
-    private ResourceManagerWrapper( final IResourceManager internal ) {
-      this.resourceManager = internal;
-      Adaptable adaptable = ( Adaptable )resourceManager;
-      JsConcatenator jsConcatenator
-        = ( JsConcatenator )adaptable.getAdapter( JsConcatenator.class );
-      jsConcatenator.startJsConcatenation();
-    }
-
-    public Object getAdapter( Class adapter ) {
-      return ( ( Adaptable )resourceManager ).getAdapter( adapter );
-    }
-    
-    public String getCharset( final String name ) {
-      return resourceManager.getCharset( name );
-    }
-
-    public ClassLoader getContextLoader() {
-      return resourceManager.getContextLoader();
-    }
-
-    public String getLocation( final String name ) {
-      return resourceManager.getLocation( name );
-    }
-
-    public URL getResource( final String name ) {
-      return resourceManager.getResource( name );
-    }
-
-    public InputStream getResourceAsStream( final String name ) {
-      return resourceManager.getResourceAsStream( name );
-    }
-
-    public Enumeration getResources( final String name ) throws IOException {
-      return resourceManager.getResources( name );
-    }
-
-    public boolean isRegistered( final String name ) {
-      return resourceManager.isRegistered( name );
-    }
-
-    public void register( final String name ) {
-      resourceManager.register( name );
-      registerAtHttpService( name );
-    }
-    
-    public void register( final String name, final InputStream is ) {
-      resourceManager.register( name, is );
-      registerAtHttpService( name );
-    }
-
-    public void register( final String name, final String charset ) {
-      resourceManager.register( name, charset );
-      registerAtHttpService( name );
-    }
-
-    public void register( final String name,
-                          final String charset,
-                          final RegisterOptions options )
-    {
-      resourceManager.register( name, charset, options );
-      registerAtHttpService( name );
-    }
-    
-    public void register( String name,
-                          InputStream is,
-                          String charset,
-                          RegisterOptions options )
-    {
-      resourceManager.register( name, is, charset, options );
-      registerAtHttpService( name );
-    }
-    
-    public boolean unregister( final String name ) {
-      return resourceManager.unregister( name );
-    }
-
-    public void setContextLoader( final ClassLoader classLoader ) {
-      resourceManager.setContextLoader( classLoader );
-    }
-
-    private void registerAtHttpService( final String name ) {
-      HttpService httpService = getHttpService();
-      if( httpService != null ) {
-        HttpContext httpContext = getHttpContext();
-        HttpContext wrapper = new HttpContextWrapper( httpContext );
-        StringBuffer alias = new StringBuffer();
-        alias.append( "/" );
-        alias.append( ResourceManagerImpl.RESOURCES );
-        String contextRoot = ContextProvider.getWebAppBase();
-        StringBuffer location 
-          = new StringBuffer( ( new Path( contextRoot ) ).toString() );
-        location.append( "/" );
-        location.append( ResourceManagerImpl.RESOURCES );
-        IPath path = new Path( name ).removeLastSegments( 1 );
-        if( !path.isEmpty() ) {
-          alias.append( "/" );
-          alias.append( path.toString() );
-          location.append( "/" );
-          location.append( path.toString() );
-        }
-        try {
-          httpService.registerResources( alias.toString(),
-                                         location.toString(),
-                                         wrapper );
-        } catch( final NamespaceException ignore ) {
-          // TODO: [fappel] for the first shot we simply ignore the exception
-          //                that's thrown if we register an alias twice. A better
-          //                approach could be to take track of the namespaces that
-          //                have already been registered
-        }
-      }
-    }
-
-    private HttpContext getHttpContext() {
-      String contextExtension = HttpContextExtensionService.class.getName();
-      BundleContext context = WorkbenchPlugin.getDefault().getBundleContext();
-      ServiceReference ref = context.getServiceReference( contextExtension );
-      HttpContextExtensionService service
-        = ( HttpContextExtensionService )context.getService( ref );
-      String id = HttpServiceTracker.ID_HTTP_CONTEXT;
-      HttpContext result = null;
-      ServiceReference httpServiceRef = getHttpServiceRef();
-      if( httpServiceRef != null ) {
-        result = service.getHttpContext( httpServiceRef, id );
-      } else {
-        throw new IllegalStateException( "HttpService is not available." );
-      }
-      return result;
-    }
-
-    private HttpService getHttpService() {
-      ServiceReference reference = getHttpServiceRef();
-      BundleContext context = WorkbenchPlugin.getDefault().getBundleContext();
-      HttpService result = null;
-      // TODO [fappel]: think of a better solution
-      // This field httpServiceLoaded is set to true the first time the
-      // HttpService is used. If the HttpService isn't available anymore we
-      // assume that the system is in shutdown process. NPE would prevent
-      // a proper shutdown of the workbench due to some unneeded image
-      // registration.
-      if( reference != null ) {
-        result = ( HttpService )context.getService( reference );
-        httpServiceLoaded = true;
-      } else if( !httpServiceLoaded ) {
-        throw new IllegalStateException( "HttpService is not available." );
-      }
-      return result;
-    }
-
-    private ServiceReference getHttpServiceRef() {
-      BundleContext context = WorkbenchPlugin.getDefault().getBundleContext();
-      ServiceReference result = null;
-      String serviceName = HttpService.class.getName();
-      try {
-        result = context.getServiceReference( serviceName );
-      } catch( final IllegalStateException ignore ) {
-        // TODO [fappel]: think of a better solution
-        // ignore exception: nothing we can do about, if called during
-        // shutdown. Calling methods have to handle the return value null.
-        // Throwing the Exception would prevent a proper shutdown of the
-        // workbench due to unneeded image registration.
-      }
-      return result;
-    }
-
-    public InputStream getRegisteredContent( final String name ) {
-      return resourceManager.getRegisteredContent( name );
-    }
-  }
-
   public IResourceManager create() {
-    return new ResourceManagerWrapper( ResourceManagerImpl.getInstance() );
+    IResourceManager result = ResourceManagerImpl.getInstance();
+    Adaptable adaptable = ( Adaptable )result;
+    JsConcatenator jsConcatenator = ( JsConcatenator )adaptable.getAdapter( JsConcatenator.class );
+    jsConcatenator.startJsConcatenation();
+    return result;
   }
 }
