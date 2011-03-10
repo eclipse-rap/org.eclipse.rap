65cf443686782959737f5c37394e8b6f8b966e28
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-03-10 09:57:45 +0000

    Formatting

--- bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/rwt/widgets/ToolItem.js
+++ bundles/org.eclipse.rap.rwt.q07/js/org/eclipse/rwt/widgets/ToolItem.js
@@ -1,15 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 EclipseSource and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009, 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   EclipseSource - initial API and implementation
+ *    EclipseSource - initial API and implementation
  ******************************************************************************/
 
-
 qx.Class.define( "org.eclipse.rwt.widgets.ToolItem", {
+
   extend : org.eclipse.rwt.widgets.BasicButton,
 
   construct : function( itemType, flat ) {
