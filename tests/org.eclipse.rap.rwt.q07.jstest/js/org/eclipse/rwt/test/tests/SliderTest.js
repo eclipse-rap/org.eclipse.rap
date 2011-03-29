5d771bc5c7abcbf818be87f89285ac39107ce17d
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-03-29 20:34:24 +0000

    formatted copyright

--- tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/tests/SliderTest.js
+++ tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/tests/SliderTest.js
@@ -1,14 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 EclipseSource and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2010, 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   EclipseSource - initial API and implementation
+ *    EclipseSource - initial API and implementation
  ******************************************************************************/
 
 qx.Class.define( "org.eclipse.rwt.test.tests.SliderTest", {
+
   extend : qx.core.Object,
   
   construct : function() {
