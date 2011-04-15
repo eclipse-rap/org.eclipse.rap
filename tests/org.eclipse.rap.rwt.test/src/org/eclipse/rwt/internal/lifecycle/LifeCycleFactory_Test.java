9d73d3e2aefbca959f349ef313fe73a2b2117d5d
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2011-04-15 10:22:34 +0000

    Added tests to ensure that session scoped life cycles are handled correctly

--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/lifecycle/LifeCycleFactory_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/lifecycle/LifeCycleFactory_Test.java
@@ -22,8 +22,8 @@ import org.eclipse.rwt.lifecycle.PhaseListener;
 
 
 public class LifeCycleFactory_Test extends TestCase {
-  
-  public final static class TestLifeCycle extends LifeCycle {
+
+  private static class ApplicationScopedLifeCycle extends LifeCycle {
     public void addPhaseListener( PhaseListener listener ) {
     }
     public void removePhaseListener( PhaseListener listener ) {
@@ -34,21 +34,57 @@ public class LifeCycleFactory_Test extends TestCase {
       return Scope.APPLICATION; 
     }
   }
+
+  private static class SessionScopedLifeCycle extends LifeCycle {
+    public void addPhaseListener( PhaseListener listener ) {
+    }
+    public void removePhaseListener( PhaseListener listener ) {
+    }
+    public void execute() throws ServletException {
+    }
+    public Scope getScope() { 
+      return Scope.SESSION; 
+    }
+  }
   
-  public void testGetUserdefinedLifeCycle() {
-    ILifeCycle lifeCycle1 = RWTFactory.getLifeCycleFactory().getLifeCycle();
-    ILifeCycle lifeCycle2 = RWTFactory.getLifeCycleFactory().getLifeCycle();
-    assertTrue( lifeCycle1 instanceof TestLifeCycle );
+  private LifeCycleFactory lifeCycleFactory;
+  
+  public void testGetLifeCycleWithApplicationScopedLifeCycle() {
+    System.setProperty( "lifecycle", ApplicationScopedLifeCycle.class.getName() );
+    ILifeCycle lifeCycle1 = lifeCycleFactory.getLifeCycle();
+    ILifeCycle lifeCycle2 = lifeCycleFactory.getLifeCycle();
+    assertTrue( lifeCycle1 instanceof ApplicationScopedLifeCycle );
     assertSame( lifeCycle1, lifeCycle2 );
   }
   
+  public void testGetLifeCycleWithSessionScopedLifeCycle() {
+    System.setProperty( "lifecycle", SessionScopedLifeCycle.class.getName() );
+    ILifeCycle lifeCycleForSession1 = lifeCycleFactory.getLifeCycle();
+    newSession();
+    ILifeCycle lifeCycleForSession2 = lifeCycleFactory.getLifeCycle();
+    assertTrue( lifeCycleForSession1 instanceof SessionScopedLifeCycle );
+    assertNotSame( lifeCycleForSession1, lifeCycleForSession2 );
+  }
+  
+  public void testGetLifeCycleWithSessionScopedLifeCycleReturnsSameForOneSession() {
+    System.setProperty( "lifecycle", SessionScopedLifeCycle.class.getName() );
+    ILifeCycle lifeCycle1 = lifeCycleFactory.getLifeCycle();
+    ILifeCycle lifeCycle2 = lifeCycleFactory.getLifeCycle();
+    assertSame( lifeCycle1, lifeCycle2 );
+  }
+
   protected void setUp() throws Exception {
     Fixture.setUp();
-    System.setProperty( "lifecycle", TestLifeCycle.class.getName() );
+    lifeCycleFactory = RWTFactory.getLifeCycleFactory();
   }
   
   protected void tearDown() throws Exception {
     System.getProperties().remove( "lifecycle" );
     Fixture.tearDown();
+  }
+
+  private static void newSession() {
+    Fixture.disposeOfServiceContext();
+    Fixture.createServiceContext();
   }
 }
