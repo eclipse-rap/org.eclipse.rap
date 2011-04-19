7835cd3363074a7cf1ea7a904bdf109e59560ddb
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2011-04-19 08:50:35 +0000

    Removed unused import

--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/textsize/TextSizeEstimation_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/textsize/TextSizeEstimation_Test.java
@@ -19,7 +19,6 @@ import org.eclipse.rwt.internal.textsize.TextSizeProbeStore.Probe;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.internal.graphics.*;
 
 public class TextSizeEstimation_Test extends TestCase {
 
