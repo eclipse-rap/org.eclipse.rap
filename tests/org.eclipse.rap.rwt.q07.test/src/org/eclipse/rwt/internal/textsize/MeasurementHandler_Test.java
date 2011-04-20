77b39ddeec1c1c570d25c526aed4ffdb53028d1b
Frank Appel <fappel>
2011-04-20 07:17:06 +0000

    Refactoring of test method

--- tests/org.eclipse.rap.rwt.q07.test/src/org/eclipse/rwt/internal/textsize/MeasurementHandler_Test.java
+++ tests/org.eclipse.rap.rwt.q07.test/src/org/eclipse/rwt/internal/textsize/MeasurementHandler_Test.java
@@ -13,12 +13,9 @@ package org.eclipse.rwt.internal.textsize;
 import junit.framework.TestCase;
 
 import org.eclipse.rwt.Fixture;
+import org.eclipse.rwt.RWT;
 import org.eclipse.rwt.graphics.Graphics;
-import org.eclipse.rwt.internal.engine.RWTFactory;
-import org.eclipse.rwt.internal.lifecycle.*;
-import org.eclipse.rwt.internal.service.ContextProvider;
 import org.eclipse.rwt.lifecycle.*;
-import org.eclipse.rwt.service.ISessionStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
@@ -26,42 +23,71 @@ import org.eclipse.swt.widgets.Display;
 
 public class MeasurementHandler_Test extends TestCase {
 
-  public void testRequestCycle() {
-    Display display = new Display();
+  private class AskForTextSizeListener implements PhaseListener {
+    private static final long serialVersionUID = 1L;
 
-    // Let pass one startup request to init the 'system'
-    Fixture.fakeNewRequest( display );
-    RWTFactory.getPhaseListenerRegistry().add( new PreserveWidgetsPhaseListener() );
-    RWTFactory.getPhaseListenerRegistry().add( new CurrentPhase.Listener() );
-    RWTLifeCycle lifeCycle = ( RWTLifeCycle )RWTFactory.getLifeCycleFactory().getLifeCycle();
-    ISessionStore session = ContextProvider.getSession();
-    String id = LifeCycle.class.getName();
-    session.setAttribute( id, lifeCycle );
-    Fixture.executeLifeCycleFromServerThread( );
+    public void afterPhase( final PhaseEvent event ) {
+    }
 
-    // The actual test request
-    Fixture.fakeResponseWriter();
-    Fixture.fakeNewRequest( display );
-    lifeCycle.addPhaseListener( new PhaseListener() {
-      private static final long serialVersionUID = 1L;
-      public void afterPhase( final PhaseEvent event ) {
-      }
-      public void beforePhase( final PhaseEvent event ) {
-        Font font = Graphics.getFont( "arial", 10, SWT.BOLD );
-        Graphics.stringExtent( font, "FirstString" );
-        font = Graphics.getFont( "helvetia, ms sans serif", 12, SWT.BOLD );
-        Graphics.stringExtent( font, "SecondString" );
-        font = Graphics.getFont( "\"Bogus\" \\ Font \" Name", 12, SWT.BOLD );
-        Graphics.stringExtent( font, "Weird \" String \\" );
-      }
-      public PhaseId getPhaseId() {
-        return PhaseId.RENDER;
-      }
-    } );
+    public void beforePhase( final PhaseEvent event ) {
+      askForTextSizes();
+    }
+
+    public PhaseId getPhaseId() {
+      return PhaseId.RENDER;
+    }
+  }
+
+  public void testProbeStatementCreation() {
+    prepareRequestThatAsksForTextSizes();
+
     Fixture.executeLifeCycleFromServerThread();
 
+    checkProbeStatement();
+  }
+
+  protected void setUp() throws Exception {
+    Fixture.setUp();
+  }
+
+  protected void tearDown() throws Exception {
+    Fixture.tearDown();
+  }
+
+  private void prepareRequestThatAsksForTextSizes() {
+    Display display = new Display();
+    Fixture.fakeNewRequest( display );
+    RWT.getLifeCycle().addPhaseListener( new AskForTextSizeListener() );
+  }
+
+  private void checkProbeStatement() {
+    String[] expected = getExpectedProbeStatement();
+    String allMarkup = Fixture.getAllMarkup();
+    for( int i = 0; i < expected.length; i++ ) {
+      assertTrue( "Expected: " + expected[ i ], contains( allMarkup, expected[ i ] ) );
+    }
+  }
+
+  private boolean contains( String allMarkup, String snippet ) {
+    return allMarkup.indexOf( snippet ) != -1;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // Note [fappel]: The literals used in askForTextSizes and getExpectedProbeStatement work
+  //                belong together. I do this without constant extraction since I feel
+  //                this makes the probestatment array even more unreadable.
+  private void askForTextSizes() {
+    Font font = Graphics.getFont( "arial", 10, SWT.BOLD );
+    Graphics.stringExtent( font, "FirstString" );
+    font = Graphics.getFont( "helvetia, ms sans serif", 12, SWT.BOLD );
+    Graphics.stringExtent( font, "SecondString" );
+    font = Graphics.getFont( "\"Bogus\" \\ Font \" Name", 12, SWT.BOLD );
+    Graphics.stringExtent( font, "Weird \" String \\" );
+  }
+  
+  private String[] getExpectedProbeStatement() {
     String probe = TextSizeProbeStore.DEFAULT_PROBE;
-    String[] expected = new String[] {
+    return new String[] {
       "org.eclipse.swt.FontSizeCalculation.probe( [ [",
       ", \"" + probe + "\", [ \"arial\" ], 10, true, false ]",
       ", [ ",
@@ -72,17 +98,7 @@ public class MeasurementHandler_Test extends TestCase {
       ", \"SecondString\", [ \"helvetia\", \"ms sans serif\" ], 12, true, false, -1 ], [",
       ", \"Weird &quot; String \\\\\", [ \"Bogus  Font  Name\" ], 12, true, false, -1 ] ] );"
     };
-    String allMarkup = Fixture.getAllMarkup();
-    for( int i = 0; i < expected.length; i++ ) {
-      assertTrue( "Expected: " + expected[ i ], allMarkup.indexOf( expected[ i ] ) != -1 );
-    }
   }
-
-  protected void setUp() throws Exception {
-    Fixture.setUp();
-  }
-
-  protected void tearDown() throws Exception {
-    Fixture.tearDown();
-  }
+  // END NOTE
+  //////////////////////////////////////////////////////////////////////////////////////////////////
 }
