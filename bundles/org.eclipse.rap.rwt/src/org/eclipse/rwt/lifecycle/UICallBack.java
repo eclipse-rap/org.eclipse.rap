419db41d6aea79442404961f30daca53c57757f4
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-05-21 09:34:56 +0000

    Fixed Copyright headers

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/lifecycle/UICallBack.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/lifecycle/UICallBack.java
@@ -1,20 +1,21 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2010 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2011 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *    Innoopract Informationssysteme GmbH - initial API and implementation
+ *    EclipseSource - ongoing development
  ******************************************************************************/
-
 package org.eclipse.rwt.lifecycle;
 
 import org.eclipse.rwt.internal.lifecycle.*;
 import org.eclipse.rwt.internal.service.ContextProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+
 
 /**
  * A utility class that provides some static helper methods to perform
