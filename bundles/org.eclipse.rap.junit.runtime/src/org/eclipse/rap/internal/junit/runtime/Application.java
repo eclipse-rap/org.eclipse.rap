0998ee8f560eab8fd40eaafdae45b7f92d30edef
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2011-06-01 10:12:14 +0000

    Set javacSource and javacTarget to 1.5 in order to fix bug 347888: [releng] Build fails after changing execution environment to Java 1.5 Added missing about.html files to org.eclipse.rap.rwt.cluster.* bundles

--- bundles/org.eclipse.rap.junit.runtime/src/org/eclipse/rap/internal/junit/runtime/Application.java
+++ bundles/org.eclipse.rap.junit.runtime/src/org/eclipse/rap/internal/junit/runtime/Application.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2008, 2011 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,8 +7,8 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
-
 package org.eclipse.rap.internal.junit.runtime;
 
 import org.eclipse.core.runtime.Platform;
@@ -27,6 +27,7 @@ import org.eclipse.ui.testing.TestableObject;
  * This class controls all aspects of the application's execution and is
  * contributed through the plugin.xml.
  */
+@SuppressWarnings("restriction")
 public class Application implements IEntryPoint, ITestHarness {
 
   private TestableObject fTestableObject;
