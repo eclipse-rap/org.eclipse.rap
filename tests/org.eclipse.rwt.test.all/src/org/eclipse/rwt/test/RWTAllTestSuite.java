4b88017191c53bea53c6e596b4778b749ab5f2a6
Frank Appel <fappel>
2011-06-04 10:34:22 +0000

    Fixed Problem of URL encoding of file names, improved performance using threads and a include list - in particular the latter may be subject of further improval...

--- tests/org.eclipse.rwt.test.all/src/org/eclipse/rwt/test/RWTAllTestSuite.java
+++ tests/org.eclipse.rwt.test.all/src/org/eclipse/rwt/test/RWTAllTestSuite.java
@@ -11,12 +11,14 @@
 package org.eclipse.rwt.test;
 
 import java.io.*;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.net.*;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
 import junit.framework.*;
+
+import org.eclipse.rwt.Fixture;
 
 
 public class RWTAllTestSuite {
@@ -27,34 +29,134 @@ public class RWTAllTestSuite {
     return new RWTAllTestSuite().createSuite();
   }
   
-  private final TestSuite suite;
   private final URLClassLoader classLoader;
+  private final List<Thread> threads;
+  private final List<IOException> problems;
+  private final List<Class<? extends TestCase>> tests;
 
   RWTAllTestSuite() {
-    suite = new TestSuite( "RWT Test Suite" );
     classLoader = ( URLClassLoader )getClass().getClassLoader();
+    threads = new Vector<Thread>();
+    problems = new Vector<IOException>();
+    tests = new Vector<Class<? extends TestCase>>();
   }
   
   Test createSuite() {
+    initializeFixture();
     try {
       scanClasspath();
     } catch( IOException ioe ) {
       throw new RuntimeException( ioe );
     }
-    return suite;
+    return createSortedSuite();
+  }
+
+  private void initializeFixture() {
+    Fixture.setUp();
+    Fixture.tearDown();
+  }
+
+  private TestSuite createSortedSuite() {
+    Class<? extends TestCase>[] testCases = sortTests();
+    return createSuite( testCases );
+  }
+
+  private TestSuite createSuite( Class<? extends TestCase>[] testCases ) {
+    TestSuite result = new TestSuite( "RWT Test Suite" );
+    for( Class<? extends TestCase> testClass : testCases ) {
+      result.addTestSuite( testClass );
+    }
+    return result;
+  }
+
+  private Class<? extends TestCase>[] sortTests() {
+    @SuppressWarnings("unchecked")
+    Class<? extends TestCase>[] testCases = new Class[ tests.size() ];
+    tests.toArray( testCases );
+    Arrays.sort( testCases, new Comparator<Class<? extends TestCase>>() {
+      public int compare( Class<? extends TestCase> o1, Class<? extends TestCase> o2 ) {
+        return o1.getName().compareTo( o2.getName() );
+      }
+    } );
+    return testCases;
   }
 
   private void scanClasspath() throws IOException {
     URL[] urls = classLoader.getURLs();
     for( int i = 0; i < urls.length; i++ ) {
-      File file = new File( urls[ i ].getFile() );
-      if( file.exists() ) {
-        if( file.getName().endsWith( JAR_EXTENSION ) ) {
-          scanJar( file );
-        } else {
-          scanDirectory( file, "", file.getPath() );
-        }
+      File file = new File( URLDecoder.decode( urls[ i ].getFile(), "UTF-8" ) );
+      if( file.exists() && isInInclusionList( file ) ) {
+        runScan( file );
       }  
+    }
+    waitTillScanIsFinished();
+  }
+
+  private boolean isInInclusionList( File file ) {
+    String[] exclusionList = new String[] {
+      ".test"
+    };
+    boolean result = false;
+    for( int i = 0; !result && i < exclusionList.length; i++ ) {
+      result = file.toString().contains( exclusionList[ i ] );
+    }
+    return result;
+  }
+
+  private void waitTillScanIsFinished() {
+    if( !threads.isEmpty() ) {
+      synchronized( tests ) {
+        try {
+          tests.wait();
+        } catch( InterruptedException shouldNotHappen ) {
+          throw new RuntimeException( shouldNotHappen );
+        }
+      }
+    }
+    handleProblems();
+  }
+
+  private void handleProblems() {
+    if( !problems.isEmpty() ) {
+      Iterator<IOException> iterator = problems.iterator();
+      while( iterator.hasNext() ) {
+        iterator.next().printStackTrace();
+      }
+      String msg = "Unable to start testsuite due to IO problems, see stacktraces.";
+      throw new RuntimeException( msg );
+    }
+  }
+
+  private void runScan( final File file ) {
+    Thread thread = new Thread( new Runnable() {
+      public void run() {
+        try {
+          scan( file );
+        } catch( IOException ioe ) {
+          problems.add( ioe );
+        }
+        notifyFileScanComplete();
+      }
+    } );
+    threads.add( thread );
+    thread.setDaemon( true );
+    thread.start();
+  }
+  
+  private void notifyFileScanComplete() {
+    threads.remove( 0 );
+    if( threads.isEmpty() ) {
+      synchronized( tests ) {
+        tests.notify();
+      }
+    }
+  }
+
+  private void scan( final File file ) throws IOException {
+    if( file.getName().endsWith( JAR_EXTENSION ) ) {
+      scanJar( file );
+    } else {
+      scanDirectory( file, "", file.getPath() );
     }
   }
 
@@ -104,7 +206,9 @@ public class RWTAllTestSuite {
   private void addToSuite( String className ) {
     if( className.endsWith( "_Test" ) ) {
       try {
-        suite.addTestSuite( ( Class<? extends TestCase> )classLoader.loadClass( className ) );
+        synchronized( tests ) {
+          tests.add( ( Class<? extends TestCase> )classLoader.loadClass( className ) );
+        }
       } catch( ClassNotFoundException cnfe ) {
         throw new RuntimeException( cnfe );
       }
