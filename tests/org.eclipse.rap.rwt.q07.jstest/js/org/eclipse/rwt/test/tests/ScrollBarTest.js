bb52b7e24ea0772dec02e05819143310271085a7
Tim Buschtöns <tbuschto@eclipsesource.com>
2011-06-10 09:54:04 +0000

    fixed test (failed after Tree-Table-Merge)

--- tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/tests/ScrollBarTest.js
+++ tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/tests/ScrollBarTest.js
@@ -339,7 +339,7 @@ qx.Class.define( "org.eclipse.rwt.test.tests.ScrollBarTest", {
       assertFalse( bar.getMergeEvents() );
       bar.autoEnableMerge( 600 );
       assertFalse( bar.getMergeEvents() );
-      bar.autoEnableMerge( 710 );
+      bar.autoEnableMerge( 1010 );
       assertTrue( bar.getMergeEvents() );
       bar.destroy();
     },
@@ -354,7 +354,7 @@ qx.Class.define( "org.eclipse.rwt.test.tests.ScrollBarTest", {
       bar.autoEnableMerge( 0 );
       bar.autoEnableMerge( 0 );
       assertFalse( bar.getMergeEvents() );
-      bar.autoEnableMerge( 710 );
+      bar.autoEnableMerge( 1010 );
       assertTrue( bar.getMergeEvents() );
       bar.destroy();
     },
