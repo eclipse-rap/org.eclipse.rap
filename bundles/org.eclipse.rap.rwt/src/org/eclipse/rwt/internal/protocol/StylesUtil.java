70e02551a2caa387420b896730f6704e6ed1e933
Holger Staudacher <hstaudacher@eclipsesource.com>
2011-07-04 12:18:27 +0000

    RESOLVED 350906: Create an Util to convert SWT styles https://bugs.eclipse.org/bugs/show_bug.cgi?id=350906

---
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/protocol/StylesUtil.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rwt.internal.protocol;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Widget;
+
+
+public final class StylesUtil {
+
+  private static final Map<String, Integer> availableStyles;
+  
+  static {
+    availableStyles = new LinkedHashMap<String, Integer>();
+    initializeAvailableStyles();
+  }
+
+  private static void initializeAvailableStyles() {
+    Field[] constants = SWT.class.getDeclaredFields();
+    for( Field constant : constants ) {
+      if( constant.getType() == int.class ) {
+        addStyleToMap( constant );
+      }
+    }
+  }
+
+  private static void addStyleToMap( Field constant ) {
+    String styleName = constant.getName();
+    try {
+      constant.setAccessible( true );
+      Integer value = new Integer( constant.getInt( null ) ); // use null because we access statics 
+      availableStyles.put( styleName, value );
+    } catch( Exception e ) {
+      String causeMessage = "Could not initialize SWT styles map with constant " + styleName;
+      throw new RuntimeException( causeMessage );
+    }
+  }
+  
+  public static String[] getContainedStyles( Widget widget, String... allowedStyles ) {
+    List<String> containedStyles = findContainedStyles( widget, allowedStyles );
+    if( containedStyles.isEmpty() ) {
+      containedStyles.add( "NONE" );
+    }
+    String[] result = new String[ containedStyles.size() ];
+    return containedStyles.toArray( result );
+  }
+
+  private static List<String> findContainedStyles( Widget widget, String... allowedStyles ) {
+    List<String> containedStyles = new ArrayList<String>();
+    for( String allowedStyle : allowedStyles ) {
+      Integer object = availableStyles.get( allowedStyle );
+      if( object == null ) {
+        throw new IllegalArgumentException( allowedStyle + " is not an existing SWT style" );
+      }
+      if( ( widget.getStyle() & object.intValue() ) != 0 ) {
+        containedStyles.add( allowedStyle );
+      }
+    }
+    return containedStyles;
+  }
+
+  private StylesUtil() {
+    // prevent instantiation
+  }
+}
---
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/internal/protocol/StylesUtil_Test.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rwt.internal.protocol;
+
+import org.eclipse.rwt.Fixture;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.*;
+
+import junit.framework.TestCase;
+
+public class StylesUtil_Test extends TestCase {
+  
+  private Shell shell;
+
+  @Override
+  protected void setUp() throws Exception {
+    Fixture.setUp();
+    Display display = new Display();
+    shell = new Shell( display );
+  }
+  
+  @Override
+  protected void tearDown() throws Exception {
+    Fixture.tearDown();
+  }
+  
+  public void testHasOneStyle() {
+    Button button = new Button( shell, SWT.PUSH );
+    
+    String[] styles = StylesUtil.getContainedStyles( button, new String[] { "PUSH" } );
+    
+    assertEquals( 1, styles.length );
+    assertEquals( "PUSH", styles[ 0 ] );
+  }
+  
+  public void testHasTwoStyles() {
+    Button button = new Button( shell, SWT.PUSH  | SWT.BORDER );
+    
+    String[] styles = StylesUtil.getContainedStyles( button, new String[] { "PUSH", "BORDER" } );
+    
+    assertEquals( 2, styles.length );
+    assertEquals( "PUSH", styles[ 0 ] );
+    assertEquals( "BORDER", styles[ 1 ] );
+  }
+  
+  public void testHasOnlyOneOfTwoPossibleStyles() {
+    Button button = new Button( shell, SWT.PUSH  );
+    
+    String[] styles = StylesUtil.getContainedStyles( button, new String[] { "PUSH", "BORDER" } );
+    
+    assertEquals( 1, styles.length );
+    assertEquals( "PUSH", styles[ 0 ] );
+  }
+  
+  public void testHasNoAllowedStyles() {
+    Button button = new Button( shell, SWT.NONE  );
+    
+    String[] styles = StylesUtil.getContainedStyles( button, new String[] {} );
+    
+    assertEquals( 1, styles.length );
+    assertEquals( "NONE", styles[ 0 ] );
+  }
+  
+  public void testNoneStyles() {
+    Composite composite = new Composite( shell, SWT.NONE  );
+    
+    String[] styles = StylesUtil.getContainedStyles( composite, new String[] { "NO_RADIO_GROUP" } );
+    
+    assertEquals( 1, styles.length );
+    assertEquals( "NONE", styles[ 0 ] );
+  }
+  
+  public void testWithNonExistingStyle() {
+    Button button = new Button( shell, SWT.NONE  );
+    
+    try {
+      StylesUtil.getContainedStyles( button, new String[] { "FOO" } );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+}
