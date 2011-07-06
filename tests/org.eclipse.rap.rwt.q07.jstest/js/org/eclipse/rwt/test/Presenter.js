4ba28730c8770a6e22278d8072af3b9f3c64ff5c
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-07-06 21:22:37 +0000

    Fixed missing semicolons, copyright headers

--- tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/Presenter.js
+++ tests/org.eclipse.rap.rwt.q07.jstest/js/org/eclipse/rwt/test/Presenter.js
@@ -1,28 +1,31 @@
 /*******************************************************************************
- * Copyright (c) 2009 EclipseSource and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009, 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   EclipseSource - initial API and implementation
+ *    EclipseSource - initial API and implementation
  ******************************************************************************/
 
-qx.Class.define("org.eclipse.rwt.test.Presenter", {
+qx.Class.define( "org.eclipse.rwt.test.Presenter", {
+
   extend : qx.core.Target,
+
   type : "singleton",
-  
+
   construct : function() {
     this.base( arguments );
-    
+
     var content = document.createElement( "div" );
     content.style.zIndex = 1000000;
     content.style.position = "absolute";
-    content.style.border = "2px solid black";    
+    content.style.border = "2px solid black";
     content.style.backgroundColor = "white";
     document.body.appendChild( content );
     this._content = content;
-    
+
     var text = document.createElement( "div" );
     text.style.overflow = "scroll";
     text.style.top = 0;
@@ -31,14 +34,14 @@ qx.Class.define("org.eclipse.rwt.test.Presenter", {
     text.style.fontFamily = "monospace";
     this._content.appendChild( text );
     this._text = text;
-    
-    var bar = document.createElement( "div" );    
+
+    var bar = document.createElement( "div" );
     bar.style.position = "absolute";
     bar.style.left = 0;
     bar.style.width = "100%";
     bar.style.borderTopWidth = "2px";
     bar.style.borderTopColor = "black";
-    bar.style.borderTopStyle = "solid";        
+    bar.style.borderTopStyle = "solid";
     this._progressBar = bar;
     content.appendChild( bar );
 
@@ -48,15 +51,15 @@ qx.Class.define("org.eclipse.rwt.test.Presenter", {
     indicator.style.top = "0px";
     indicator.style.width = "0";
     indicator.style.height = "100%";
-    indicator.style.border = "0px";    
+    indicator.style.border = "0px";
     bar.appendChild( indicator );
     this._progressIndicator = indicator;
-    this.setFailed( false );    
+    this.setFailed( false );
   },
 
   members : {
-    
-    setNumberTestsFinished : function( finished, total ) {      
+
+    setNumberTestsFinished : function( finished, total ) {
       var pos;
       if( finished == total ) {
         pos = 100;
@@ -65,24 +68,23 @@ qx.Class.define("org.eclipse.rwt.test.Presenter", {
       }
       this._progressIndicator.style.width = pos + "%";
     },
-    
+
     setFailed : function( failed ) {
-      this._progressIndicator.style.backgroundColor = 
-        failed ? "#FA0000" : "#00F700";
+      this._progressIndicator.style.backgroundColor = failed ? "#FA0000" : "#00F700";
     },
-    
+
     setFullScreen : function( isFullScreen ) {
       var content = this._content;
       var text = this._text;
       var bar = this._progressBar;
-      if( !isFullScreen ) {        
+      if( !isFullScreen ) {
         content.style.left = "50%";
         content.style.top = "50%";
         content.style.marginLeft = "-35%";
         content.style.marginTop = "-250px";
         content.style.width = "70%";
-        content.style.height = "500px";        
-        text.style.height = "446px";        
+        content.style.height = "500px";
+        text.style.height = "446px";
         bar.style.top = 446;
         bar.style.height = "50px";
       } else {
@@ -101,8 +103,8 @@ qx.Class.define("org.eclipse.rwt.test.Presenter", {
     forceReload : function( e ) {
       var current = location.toString();
       var current = current.slice( 0, current.indexOf( "?" ) );
-      var event = e || window.event
-      var target = event.target || event.srcElement
+      var event = e || window.event;
+      var target = event.target || event.srcElement;
       var newLoc = current + "?" + target.link;
       location.href = newLoc;
     },
@@ -125,5 +127,5 @@ qx.Class.define("org.eclipse.rwt.test.Presenter", {
       span.scrollIntoView();
     }
   }
-  
+
 } );
