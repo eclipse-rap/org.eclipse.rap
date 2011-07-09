50ae2d1b729f8bc3ce365d4dc706321c6ee5dcdf
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-07-09 10:35:42 +0000

    Updated copyright headers

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/UITestUtil.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/UITestUtil.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2007, 2011 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Innoopract Informationssysteme GmbH - initial API and implementation
- *     EclipseSource - ongoing development
+ *    Innoopract Informationssysteme GmbH - initial API and implementation
+ *    EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.rwt.internal.lifecycle;
 
@@ -17,6 +17,7 @@ import org.eclipse.rwt.lifecycle.IWidgetAdapter;
 import org.eclipse.rwt.lifecycle.WidgetUtil;
 import org.eclipse.swt.internal.widgets.displaykit.DisplayLCAFacade;
 import org.eclipse.swt.widgets.Widget;
+
 
 public final class UITestUtil {
 
