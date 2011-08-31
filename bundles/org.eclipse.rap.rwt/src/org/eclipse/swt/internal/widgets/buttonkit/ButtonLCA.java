cf92bc54d7514ff8929e013ea87f4524b82a5432
Ivan Furnadjiev <ivan@eclipsesource.com>
2011-08-31 10:53:50 +0000

    Dispose all buttons with the protocol - bug 351945: Migrate widget LCAs to write JSON responses https://bugs.eclipse.org/bugs/show_bug.cgi?id=351945

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/internal/widgets/buttonkit/ButtonLCA.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/internal/widgets/buttonkit/ButtonLCA.java
@@ -1,53 +1,52 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2011 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Innoopract Informationssysteme GmbH - initial API and implementation
- *     EclipseSource - ongoing development
+ *    Innoopract Informationssysteme GmbH - initial API and implementation
+ *    EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.swt.internal.widgets.buttonkit;
 
 import java.io.IOException;
 
+import org.eclipse.rwt.internal.protocol.ClientObjectFactory;
 import org.eclipse.rwt.lifecycle.AbstractWidgetLCA;
-import org.eclipse.rwt.lifecycle.JSWriter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Widget;
 
 
 public final class ButtonLCA extends AbstractWidgetLCA {
-  
+
   private final static ButtonDelegateLCA PUSH = new PushButtonDelegateLCA();
   private final static ButtonDelegateLCA CHECK = new CheckButtonDelegateLCA();
   private final static ButtonDelegateLCA RADIO = new RadioButtonDelegateLCA();
 
-  public void preserveValues( final Widget widget ) {
+  public void preserveValues( Widget widget ) {
     getLCADelegate( widget ).preserveValues( ( Button )widget );
   }
 
-  public void readData( final Widget widget ) {
+  public void readData( Widget widget ) {
     getLCADelegate( widget ).readData( ( Button )widget );
   }
 
-  public void renderInitialization( final Widget widget ) throws IOException {
+  public void renderInitialization( Widget widget ) throws IOException {
     getLCADelegate( widget ).renderInitialization( ( Button )widget );
   }
 
-  public void renderChanges( final Widget widget ) throws IOException {
+  public void renderChanges( Widget widget ) throws IOException {
     getLCADelegate( widget ).renderChanges( ( Button )widget );
   }
 
-  public void renderDispose( final Widget widget ) throws IOException {
-    JSWriter writer = JSWriter.getWriterFor( widget );
-    writer.dispose();
+  public void renderDispose( Widget widget ) throws IOException {
+    ClientObjectFactory.getForWidget( widget ).destroy();
   }
 
-  private static ButtonDelegateLCA getLCADelegate( final Widget widget ) {
+  private static ButtonDelegateLCA getLCADelegate( Widget widget ) {
     ButtonDelegateLCA result;
     int style = ( ( Button )widget ).getStyle();
     if( ( style & SWT.CHECK ) != 0 ) {
