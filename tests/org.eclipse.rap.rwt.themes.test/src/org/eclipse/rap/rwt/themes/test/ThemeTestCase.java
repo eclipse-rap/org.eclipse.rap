c11de1d7928438f0cc51b7adfd94aa971108654f
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-09-28 12:54:15 +0000

    code cleanup

--- tests/org.eclipse.rap.rwt.themes.test/src/org/eclipse/rap/rwt/themes/test/ThemeTestCase.java
+++ tests/org.eclipse.rap.rwt.themes.test/src/org/eclipse/rap/rwt/themes/test/ThemeTestCase.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
-* Copyright (c) 2010, 2011 EclipseSource and others. All rights reserved. This
-* program and the accompanying materials are made available under the terms of
-* the Eclipse Public License v1.0 which accompanies this distribution, and is
-* available at http://www.eclipse.org/legal/epl-v10.html
+* Copyright (c) 2010, 2011 EclipseSource and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
 *
 * Contributors:
-*   EclipseSource - initial API and implementation
+*    EclipseSource - initial API and implementation
 *******************************************************************************/
 package org.eclipse.rap.rwt.themes.test;
 
@@ -39,9 +40,7 @@ public abstract class ThemeTestCase extends TestCase {
     Fixture.tearDown();
   }
 
-  protected void processCssTestFile( final Class clazz, final String fileName )
-    throws IOException
-  {
+  protected void processCssTestFile( Class clazz, String fileName ) throws IOException {
     StyleRule[] rules = readTestCssFile( clazz, fileName );
     for( int i = 0; i < rules.length; i++ ) {
       StyleRule styleRule = rules[ i ];
@@ -49,7 +48,7 @@ public abstract class ThemeTestCase extends TestCase {
     }
   }
 
-  private static void processTestStyleRule( final StyleRule styleRule ) {
+  private static void processTestStyleRule( StyleRule styleRule ) {
     SelectorList selectors = styleRule.getSelectors();
     IStylePropertyMap properties = styleRule.getProperties();
     int length = selectors.getLength();
@@ -60,17 +59,13 @@ public abstract class ThemeTestCase extends TestCase {
     }
   }
 
-  private static void checkProperties( final SelectorExt selector,
-                                       final IStylePropertyMap expectedValues )
-  {
+  private static void checkProperties( SelectorExt selector, IStylePropertyMap expectedValues ) {
     String[] properties = expectedValues.getProperties();
     for( int i = 0; i < properties.length; i++ ) {
       String property = properties[ i ];
       QxType expected = expectedValues.getValue( property );
       SimpleSelector config = new SimpleSelector( selector.getConstraints() );
-      QxType actual = ThemeUtil.getCssValue( selector.getElementName(),
-                                             property,
-                                             config );
+      QxType actual = ThemeUtil.getCssValue( selector.getElementName(), property, config );
       if( !actual.equals( expected ) ) {
         String message =   "Css test failed for "
                          + createSelectorString( selector )
@@ -81,7 +76,7 @@ public abstract class ThemeTestCase extends TestCase {
     }
   }
 
-  private static String createSelectorString( final SelectorExt item ) {
+  private static String createSelectorString( SelectorExt item ) {
     StringBuffer result = new StringBuffer();
     String elementName = item.getElementName();
     result.append( elementName );
@@ -96,19 +91,14 @@ public abstract class ThemeTestCase extends TestCase {
     return result.toString();
   }
 
-  private static StyleRule[] readTestCssFile( final Class clazz,
-                                              final String fileName )
-    throws IOException
-  {
-    final ClassLoader classLoader = clazz.getClassLoader();
+  private static StyleRule[] readTestCssFile( Class clazz, String fileName ) throws IOException {
+    ClassLoader classLoader = clazz.getClassLoader();
     String packageName = clazz.getPackage().getName().replace( '.', '/' );
     String filePath = packageName + "/" + fileName;
     InputStream inputStream = classLoader.getResourceAsStream( filePath );
     ResourceLoader loader = ThemesTestUtil.RESOURCE_LOADER;
-    StyleSheet styleSheet = CssFileReader.readStyleSheet( inputStream,
-                                                          filePath,
-                                                          loader );
-    StyleRule[] rules = styleSheet.getStyleRules();
-    return rules;
+    StyleSheet styleSheet = CssFileReader.readStyleSheet( inputStream, filePath, loader );
+    return styleSheet.getStyleRules();
   }
+
 }
