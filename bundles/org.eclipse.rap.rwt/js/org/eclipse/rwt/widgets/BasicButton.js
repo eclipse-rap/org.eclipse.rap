d7aa7babf3a87192a5cd555d392542601f8d116b
Tim Buschtöns <tbuschto@eclipsesource.com>
2012-01-12 15:35:08 +0000

    Partial fix for Bug 366788 - KeyEvent management differs depending browser(retun key keyEvent not sent on ie, safari and chrome)

--- bundles/org.eclipse.rap.rwt/js/org/eclipse/rwt/widgets/BasicButton.js
+++ bundles/org.eclipse.rap.rwt/js/org/eclipse/rwt/widgets/BasicButton.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 EclipseSource and others. All rights reserved.
+ * Copyright (c) 2009, 2012 EclipseSource and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -251,7 +251,6 @@ qx.Class.define( "org.eclipse.rwt.widgets.BasicButton", {
         case "Space":
           this.removeState( "abandoned" );
           this.addState( "pressed" );
-          event.preventDefault();
           event.stopPropagation();
       }
     },
--- bundles/org.eclipse.rap.rwt/js/org/eclipse/swt/widgets/Combo.js
+++ bundles/org.eclipse.rap.rwt/js/org/eclipse/swt/widgets/Combo.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 EclipseSource and others.
+ * Copyright (c) 2009, 2012 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -547,7 +547,6 @@ qx.Class.define( "org.eclipse.swt.widgets.Combo", {
           }
           this.setFocused( true );
           evt.stopPropagation();
-          evt.preventDefault();
           break;
         case "Escape":
           if( this._dropped ) {
