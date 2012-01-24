bcb8f0362cb582d4c994983522abe4078772d36d
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-01-24 16:36:21 +0000

    Switched BrowserHistory to protocol and fixed - bug 360674: [History] prevent unnecessary timer calls on client https://bugs.eclipse.org/bugs/show_bug.cgi?id=360674

--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/widgets/BrowserHistory_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rwt/widgets/BrowserHistory_Test.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009 EclipseSource and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009, 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   EclipseSource - initial API and implementation
+ *    EclipseSource - initial API and implementation
  ******************************************************************************/
 package org.eclipse.rwt.widgets;
 
@@ -13,20 +14,34 @@ package org.eclipse.rwt.widgets;
 import junit.framework.TestCase;
 
 import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.rap.rwt.testfixture.Message;
+import org.eclipse.rap.rwt.testfixture.Message.CallOperation;
 import org.eclipse.rwt.IBrowserHistory;
 import org.eclipse.rwt.RWT;
+import org.eclipse.rwt.events.BrowserHistoryEvent;
+import org.eclipse.rwt.events.BrowserHistoryListener;
+import org.eclipse.rwt.internal.protocol.ProtocolTestUtil;
+import org.eclipse.rwt.lifecycle.PhaseId;
+import org.eclipse.rwt.lifecycle.ProcessActionRunner;
+import org.eclipse.swt.widgets.Display;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 
 public class BrowserHistory_Test extends TestCase {
-  
+
+  private Display display;
+
   protected void setUp() throws Exception {
     Fixture.setUp();
+    display = new Display();
+    Fixture.fakeNewRequest( display );
   }
 
   protected void tearDown() throws Exception {
     Fixture.tearDown();
   }
-  
+
   public void testCreateEntry() {
     IBrowserHistory history = RWT.getBrowserHistory();
     try {
@@ -48,7 +63,7 @@ public class BrowserHistory_Test extends TestCase {
       // expected
     }
   }
-  
+
   public void testAddBrowserHistoryListener() {
     final IBrowserHistory history = RWT.getBrowserHistory();
     try {
@@ -67,5 +82,91 @@ public class BrowserHistory_Test extends TestCase {
     } catch( IllegalArgumentException e ) {
       // expected
     }
+  }
+
+  public void testRenderAddNavigationListener() {
+    Fixture.fakePhase( PhaseId.READ_DATA );
+    ProcessActionRunner.add( new Runnable() {
+      public void run() {
+        RWT.getBrowserHistory().addBrowserHistoryListener( new BrowserHistoryListener() {
+          public void navigated( BrowserHistoryEvent event ) {
+          }
+        } );
+      }
+    } );
+
+    Fixture.executeLifeCycleFromServerThread();
+
+    Message message = Fixture.getProtocolMessage();
+    assertEquals( Boolean.TRUE, message.findListenProperty( "bh", "navigation" ) );
+  }
+
+  public void testRenderRemoveNavigationListener() {
+    final BrowserHistoryListener listener = new BrowserHistoryListener() {
+      public void navigated( BrowserHistoryEvent event ) {
+      }
+    };
+    RWT.getBrowserHistory().addBrowserHistoryListener( listener );
+    Fixture.fakePhase( PhaseId.READ_DATA );
+    ProcessActionRunner.add( new Runnable() {
+      public void run() {
+        RWT.getBrowserHistory().removeBrowserHistoryListener( listener );
+      }
+    } );
+
+    Fixture.executeLifeCycleFromServerThread();
+
+    Message message = Fixture.getProtocolMessage();
+    assertEquals( Boolean.FALSE, message.findListenProperty( "bh", "navigation" ) );
+  }
+
+  public void testRenderNavigationListenerUnchanged() {
+    RWT.getBrowserHistory().addBrowserHistoryListener( new BrowserHistoryListener() {
+      public void navigated( BrowserHistoryEvent event ) {
+      }
+    } );
+
+    Fixture.executeLifeCycleFromServerThread();
+
+    Message message = Fixture.getProtocolMessage();
+    assertNull( message.findListenOperation( "bh", "navigation" ) );
+  }
+
+  public void testRenderAdd() throws JSONException {
+    RWT.getBrowserHistory().createEntry( "testId", "testText" );
+
+    Fixture.executeLifeCycleFromServerThread();
+
+    Message message = Fixture.getProtocolMessage();
+    CallOperation operation = message.findCallOperation( "bh", "add" );
+    JSONArray entries = ( JSONArray )operation.getProperty( "entries" );
+    JSONArray actual1 = entries.getJSONArray( 0 );
+    assertTrue( ProtocolTestUtil.jsonEquals( "[\"testId\",\"testText\"]", actual1 ) );
+  }
+
+  public void testRenderAdd_NoEntries() {
+    RWT.getBrowserHistory().createEntry( "testId", "testText" );
+
+    Fixture.executeLifeCycleFromServerThread();
+    Fixture.fakeNewRequest( display );
+    Fixture.executeLifeCycleFromServerThread();
+
+    Message message = Fixture.getProtocolMessage();
+    assertNull( message.findCallOperation( "bh", "add" ) );
+  }
+
+  public void testRenderAddOrder() throws JSONException {
+    RWT.getBrowserHistory().createEntry( "testId1", "testText1" );
+    RWT.getBrowserHistory().createEntry( "testId2", "testText2" );
+
+    Fixture.executeLifeCycleFromServerThread();
+
+    Message message = Fixture.getProtocolMessage();
+    CallOperation operation = message.findCallOperation( "bh", "add" );
+    JSONArray entries = ( JSONArray )operation.getProperty( "entries" );
+    JSONArray actual1 = entries.getJSONArray( 0 );
+    assertTrue( ProtocolTestUtil.jsonEquals( "[\"testId1\",\"testText1\"]", actual1 ) );
+    JSONArray actual2 = entries.getJSONArray( 1 );
+    assertTrue( ProtocolTestUtil.jsonEquals( "[\"testId2\",\"testText2\"]", actual2 ) );
   }
 }
