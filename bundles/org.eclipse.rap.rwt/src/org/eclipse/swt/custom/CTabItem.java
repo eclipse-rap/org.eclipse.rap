4e23a742599326aff515b523b5e8153eacc46fef
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-02-17 09:07:45 +0000

    Fixed - bug 370333: CTabFolder item texts cut off in first session https://bugs.eclipse.org/bugs/show_bug.cgi?id=370333

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/custom/CTabItem.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/custom/CTabItem.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2011 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2002, 2012 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Innoopract Informationssysteme GmbH - initial API and implementation
- *     EclipseSource - ongoing development
+ *    Innoopract Informationssysteme GmbH - initial API and implementation
+ *    EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.swt.custom;
 
@@ -547,9 +547,8 @@ public class CTabItem extends Item {
         textWidth -= closeRect.width + parent.getItemSpacing( isSelected );
       }
       if( textWidth > 0 ) {
-        Font itemFont = font == null ? parent.getFont() : font;
         if( shortenedText == null || shortenedTextWidth != textWidth ) {
-          shortenedText = shortenText( itemFont, getText(), textWidth );
+          shortenedText = shortenText( getFont(), getText(), textWidth );
           shortenedTextWidth = textWidth;
         }
       }
--- bundles/org.eclipse.rap.rwt/widgetkits/org/eclipse/swt/internal/custom/ctabfolderkit/CTabFolder.appearances.js
+++ bundles/org.eclipse.rap.rwt/widgetkits/org/eclipse/swt/internal/custom/ctabfolderkit/CTabFolder.appearances.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2011 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2007, 2012 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -90,6 +90,7 @@ appearances = {
       result.paddingLeft = padding[ 3 ];
       result.paddingRight = padding[ 1 ];
       result.spacing = tv.getCssDimension( "CTabItem", "spacing" );
+      result.font = tv.getCssFont( "CTabItem", "font" );
       result.textColor = tv.getCssColor( "CTabItem", "color" );
       result.textShadow = tv.getCssShadow( "CTabItem", "text-shadow" );
       var color = tv.getCssColor( "CTabFolder", "border-color" );
