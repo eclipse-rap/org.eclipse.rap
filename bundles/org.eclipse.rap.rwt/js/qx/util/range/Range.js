2a6f04772af789e0f8bef25cbb6131bcc9dabc3b
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-02-21 22:52:38 +0000

    Code formatting

--- bundles/org.eclipse.rap.rwt/js/qx/util/range/Range.js
+++ bundles/org.eclipse.rap.rwt/js/qx/util/range/Range.js
@@ -1,12 +1,12 @@
 /*******************************************************************************
- *  Copyright: 2004, 2010 1&1 Internet AG, Germany, http://www.1und1.de,
+ *  Copyright: 2004, 2012 1&1 Internet AG, Germany, http://www.1und1.de,
  *                        and EclipseSource
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  *
- *  Contributors:
+ * Contributors:
  *    1&1 Internet AG and others - original API and implementation
  *    EclipseSource - adaptation for the Eclipse Rich Ajax Platform
  ******************************************************************************/
@@ -14,37 +14,18 @@
 /**
  * This manager is used by all objects which needs ranges like qx.ui.form.Spinner, ...
  */
-qx.Class.define("qx.util.range.Range",
-{
+qx.Class.define( "qx.util.range.Range", {
+
   extend : qx.core.Target,
-
-
-
-  /*
-  *****************************************************************************
-     EVENTS
-  *****************************************************************************
-  */
 
   events: {
     "change" : "qx.event.type.Event"
   },
 
-
-
-
-
-  /*
-  *****************************************************************************
-     PROPERTIES
-  *****************************************************************************
-  */
-
-  properties :
-  {
+  properties : {
+ 
     /** current value of the Range object */
-    value :
-    {
+    value : {
       check : "!isNaN(value)&&value>=this.getMin()&&value<=this.getMax()",
       nullable : true,
       event : "change",
@@ -52,8 +33,7 @@ qx.Class.define("qx.util.range.Range",
     },
 
     /** maximum fraction digits */
-    precision :
-    {
+    precision : {
       check : "Integer",
       nullable : true,
       event : "change",
@@ -61,8 +41,7 @@ qx.Class.define("qx.util.range.Range",
     },
 
     /** minimal value of the Range object */
-    min :
-    {
+    min : {
       check : "Number",
       apply : "_applyMin",
       event : "change",
@@ -70,8 +49,7 @@ qx.Class.define("qx.util.range.Range",
     },
 
     /** maximal value of the Range object */
-    max :
-    {
+    max : {
       check : "Number",
       apply : "_applyMax",
       event : "change",
@@ -79,24 +57,14 @@ qx.Class.define("qx.util.range.Range",
     },
 
     /** whether the value should wrap around */
-    wrap :
-    {
+    wrap : {
       check : "Boolean",
       init : false
     }
   },
 
+  members : {
 
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
-  {
     /**
      * TODOC
      *
@@ -104,8 +72,8 @@ qx.Class.define("qx.util.range.Range",
      * @param value {var} Current value
      * @param old {var} Previous value
      */
-    _applyMax : function(value, old) {
-      this.setValue(Math.min(this.getValue(), value));
+    _applyMax : function( value, old ) {
+      this.setValue( Math.min( this.getValue(), value ) );
     },
 
     /**
@@ -115,43 +83,43 @@ qx.Class.define("qx.util.range.Range",
      * @param value {var} Current value
      * @param old {var} Previous value
      */
-    _applyMin : function(value, old) {
-      this.setValue(Math.max(this.getValue(), value));
+    _applyMin : function( value, old ) {
+      this.setValue( Math.max( this.getValue(), value) );
     },
 
-    limit : function(value)
-    {
+    limit : function( value ) {
       var precision = this.getPrecision();
-      if (precision != null)
-        var mover = Math.pow(10, precision);
+      if( precision != null ) {
+        var mover = Math.pow( 10, precision );
+      }
 
       if (this.getWrap()) {
         if (precision != null) {
           // round to the precision'th digit
-          var value = Math.round(value * mover) / mover;
+          var value = Math.round( value * mover ) / mover;
         }
 
         if (value < this.getMin()) {
-          return (this.getMax() - (this.getMin() - value)) + 1;
+          return ( this.getMax() - ( this.getMin() - value ) ) + 1;
         }
         if (value > this.getMax()) {
-          return (this.getMin() + (value - this.getMax())) - 1;
+          return ( this.getMin() + ( value - this.getMax() ) ) - 1;
         }
       }
 
-      if (value < this.getMin()) {
+      if( value < this.getMin() ) {
         return this.getMin();
       }
 
-      if (value > this.getMax()) {
+      if( value > this.getMax() ) {
         return this.getMax();
       }
 
-      if (precision != null) {
-        return Math.round(value * mover) / mover;
+      if( precision != null ) {
+        return Math.round( value * mover ) / mover;
       } else {
         return value;
       }
     }
   }
-});
+} );
