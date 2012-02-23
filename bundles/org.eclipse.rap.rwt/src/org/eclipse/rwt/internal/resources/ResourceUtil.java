fc82a97f181565669bd99db177fae0d4d2b9eb5c
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-02-23 20:52:14 +0000

    Replaced unnecessary use of deprecated method

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/resources/ResourceUtil.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/resources/ResourceUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2011 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2002, 2012 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -140,7 +140,7 @@ public final class ResourceUtil {
     ClassLoader loader = ResourceManagerImpl.class.getClassLoader();
     URL resource = loader.getResource( name );
     if( resource == null ) {
-      resource = resourceManager.getResource( name );
+      resource = ResourceUtil.class.getClassLoader().getResource( name );
     }
     if( resource == null ) {
       throw new IOException( "Resource to read not found: " + name );
