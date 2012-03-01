b3846a64956f3786c62058fb6120796c0cc28704
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-03-01 08:30:29 +0000

    Add test for control disposal.

--- tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/dnd/DragSource_Test.java
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/dnd/DragSource_Test.java
@@ -1,46 +1,61 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 EclipseSource and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, 
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009, 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   EclipseSource - initial API and implementation
+ *    EclipseSource - initial API and implementation
  ******************************************************************************/
 package org.eclipse.swt.dnd;
 
 import junit.framework.TestCase;
 
 import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.rwt.lifecycle.PhaseId;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.*;
 
 
 public class DragSource_Test extends TestCase {
-  
-  private Control control;
+
+  private Shell shell;
+
+  @Override
+  protected void setUp() throws Exception {
+    Fixture.setUp();
+    Fixture.fakePhase( PhaseId.PROCESS_ACTION );
+    Display display = new Display();
+    shell = new Shell( display );
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    Fixture.tearDown();
+  }
 
   public void testAddDragListener() {
-    DragSource dragSource = new DragSource( control, DND.DROP_DEFAULT );
+    DragSource dragSource = new DragSource( shell, DND.DROP_DEFAULT );
     assertEquals( 0, dragSource.getDragListeners().length );
     dragSource.addDragListener( new DragSourceAdapter() );
     assertEquals( 1, dragSource.getDragListeners().length );
   }
 
   public void testRemoveDragListener() {
-    DragSource dragSource = new DragSource( control, DND.DROP_DEFAULT );
+    DragSource dragSource = new DragSource( shell, DND.DROP_DEFAULT );
     DragSourceAdapter listener = new DragSourceAdapter();
     dragSource.addDragListener( listener );
     dragSource.removeDragListener( listener );
     assertEquals( 0, dragSource.getDragListeners().length );
   }
 
-  protected void setUp() throws Exception {
-    Fixture.setUp();
-    Display display = new Display();
-    control = new Shell( display );
-  }
+  public void testDisposeControl() throws Exception {
+    List list = new List( shell, SWT.None );
+    DragSource dragSource = new DragSource( list, DND.DROP_MOVE );
 
-  protected void tearDown() throws Exception {
-    Fixture.tearDown();
+    list.dispose();
+
+    assertTrue( dragSource.isDisposed() );
   }
 }
