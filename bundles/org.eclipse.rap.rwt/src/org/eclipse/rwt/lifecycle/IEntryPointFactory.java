c5d128119967b9d385419c7081b332cebf9b1f18
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-03-08 22:57:32 +0000

    Added JavaDoc

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/lifecycle/IEntryPointFactory.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/lifecycle/IEntryPointFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Frank Appel and others.
+ * Copyright (c) 2011, 2012 Frank Appel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,11 +7,23 @@
  *
  * Contributors:
  *    Frank Appel - initial API and implementation
+ *    EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.rwt.lifecycle;
 
 
+/**
+ * Implementations of this interface can be used to register entrypoints with
+ * the framework. It is also possible to register the class that implements
+ * {@link IEntryPoint} directly, but using a factory provides greater
+ * flexibility as it leaves the creation of the class to the application.
+ *
+ * @since 1.5
+ */
 public interface IEntryPointFactory {
-  
+
+  /**
+   * Creates a new entrypoint instance.
+   */
   IEntryPoint create();
 }
