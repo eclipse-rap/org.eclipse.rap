9fa8b30d74b4135c3a7c01e2a35be32816c9f012
Tim Buschtöns <tbuschto@eclipsesource.com>
2012-03-08 16:14:55 +0000

    refactored label tests

--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/LabelTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/LabelTest.js
@@ -9,6 +9,15 @@
  *    EclipseSource - initial API and implementation
  ******************************************************************************/
 
+(function(){
+
+var Processor = org.eclipse.rwt.protocol.Processor;
+var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
+var ObjectManager = org.eclipse.rwt.protocol.ObjectManager;
+
+var shell;
+var labelWidget;
+
 qx.Class.define( "org.eclipse.rwt.test.tests.LabelTest", {
 
   extend : qx.core.Object,
@@ -16,134 +25,104 @@ qx.Class.define( "org.eclipse.rwt.test.tests.LabelTest", {
   members : {
 
     testCreateByProtocol : function() {
-      var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
-      var shell = TestUtil.createShellByProtocol( "w2" );
-      var processor = org.eclipse.rwt.protocol.Processor;
-      processor.processOperation( {
-        "target" : "w3",
-        "action" : "create",
-        "type" : "rwt.widgets.Label",
-        "properties" : {
-          "style" : [ "LEFT" ],
-          "parent" : "w2"
-        }
-      } );
-      var widgetManager = org.eclipse.swt.WidgetManager.getInstance();
-      var shell = widgetManager.findWidgetById( "w2" );
-      var widget = widgetManager.findWidgetById( "w3" );
-      assertTrue( widget instanceof qx.ui.basic.Atom );
-      assertIdentical( shell, widget.getParent() );
-      assertTrue( widget.getUserData( "isControl") );
-      assertTrue( widget.hasState( "rwt_LEFT" ) );
-      // properties set by LabelUtil:
-      assertEquals( "left", widget.getHorizontalChildrenAlign() );
-      assertEquals( "top", widget.getVerticalChildrenAlign() );
-      assertEquals( "label-wrapper", widget.getAppearance() );
-      assertEquals( "hidden", widget.getOverflow() );
-      var labelObject = widget.getLabelObject();
+      this.createLabel( [ "LEFT" ] );
+
+      assertIdentical( shell, labelWidget.getParent() );
+      assertTrue( labelWidget.getUserData( "isControl") );
+      assertTrue( labelWidget.hasState( "rwt_LEFT" ) );
+      assertEquals( "left", labelWidget.getHorizontalChildrenAlign() );
+      assertEquals( "top", labelWidget.getVerticalChildrenAlign() );
+      assertEquals( "label-wrapper", labelWidget.getAppearance() );
+      assertEquals( "hidden", labelWidget.getOverflow() );
+      var labelObject = labelWidget.getLabelObject();
       assertEquals( "html", labelObject.getMode() );
       assertEquals( false, labelObject.getTextOverflow() );
       assertEquals( "label-graytext", labelObject.getAppearance() );
-      assertEquals( "", widget.getLabel() );
-      assertEquals( true, widget.getHideFocus() );
-      assertTrue( widget.hasEventListeners( "mouseover" ) );
-      assertTrue( widget.hasEventListeners( "mouseout" ) );
+      assertEquals( "", labelWidget.getLabel() );
+      assertEquals( true, labelWidget.getHideFocus() );
+      assertTrue( labelWidget.hasEventListeners( "mouseover" ) );
+      assertTrue( labelWidget.hasEventListeners( "mouseout" ) );
       assertFalse( labelObject.getWrap() );
-      shell.destroy();
-      widget.destroy();
     },
 
     testCreateByProtocolWithWRAP : function() {
-      var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
-      var shell = TestUtil.createShellByProtocol( "w2" );
-      var processor = org.eclipse.rwt.protocol.Processor;
-      processor.processOperation( {
-        "target" : "w3",
-        "action" : "create",
-        "type" : "rwt.widgets.Label",
-        "properties" : {
-          "style" : [ "WRAP" ],
-          "parent" : "w2"
-        }
-      } );
-      var widgetManager = org.eclipse.swt.WidgetManager.getInstance();
-      var shell = widgetManager.findWidgetById( "w2" );
-      var widget = widgetManager.findWidgetById( "w3" );
-      var labelObject = widget.getLabelObject();
-      assertTrue( labelObject.getWrap() );
-      shell.destroy();
-      widget.destroy();
+      this.createLabel( [ "WRAP" ] );
+
+      assertTrue( labelWidget.getLabelObject().getWrap() );
     },
 
     testSetText : function() {
-      var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
-      var shell = TestUtil.createShellByProtocol( "w2" );
-      var processor = org.eclipse.rwt.protocol.Processor;
-      processor.processOperation( {
-        "target" : "w3",
-        "action" : "create",
-        "type" : "rwt.widgets.Label",
-        "properties" : {
-          "style" : [ "WRAP" ],
-          "parent" : "w2",
-          "text" : "bla  \n<"
-        }
-      } );
-      TestUtil.flush(); // LabelUtil delays setter for some reason
-      var widgetManager = org.eclipse.swt.WidgetManager.getInstance();
-      var shell = widgetManager.findWidgetById( "w2" );
-      var widget = widgetManager.findWidgetById( "w3" );
-      var labelObject = widget.getLabelObject();
-      var expected = "bla&nbsp; <br/>&lt;"
-      assertEquals( expected, labelObject.getText() );
-      shell.destroy();
-      widget.destroy();
+      this.createLabel( [ "LEFT" ], { "text" : "bla  \n<" } );
+
+      var content = this.getTextElement().innerHTML.toLowerCase().replace(/^\s+|\s+$/g, '');
+      assertTrue( content === "bla&nbsp; <br>&lt;" || content === "bla&nbsp; <br/>&lt;" );
     },
 
     testSetImageByProtocol : function() {
-      var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
-      var shell = TestUtil.createShellByProtocol( "w2" );
-      var processor = org.eclipse.rwt.protocol.Processor;
-      processor.processOperation( {
-        "target" : "w3",
-        "action" : "create",
-        "type" : "rwt.widgets.Label",
-        "properties" : {
-          "style" : [ "WRAP" ],
-          "parent" : "w2",
-          "image" : [ "image.png", 10, 20 ]
-        }
-      } );
-      TestUtil.flush(); // LabelUtil delays setter for some reason
-      var ObjectManager = org.eclipse.rwt.protocol.ObjectManager;
-      var widget = ObjectManager.getObject( "w3" );
-      assertEquals( "image.png", widget.getIcon() );
-      shell.destroy();
-      widget.destroy();
+      this.createLabel( [ "LEFT" ], { "image" : [ "image.png", 10, 20 ] } );
+
+      assertEquals( "image.png", labelWidget.getIcon() );
+      // TODO [tb] : test url directly, currently not possible
+//      var src = TestUtil.getCssBackgroundImage( this.getImageElement() );
+//      assertTrue( src.indexOf( "image.png" ) !== -1 );
     },
 
     testSetAlignmentByProtocol : function() {
-      var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
-      var shell = TestUtil.createShellByProtocol( "w2" );
-      var processor = org.eclipse.rwt.protocol.Processor;
-      processor.processOperation( {
+      // TODO [tb] : setting a text should not be necessary
+      this.createLabel( [ "LEFT" ], { "alignment" : "right", "text" : "bla" } ); 
+
+      assertEquals( "right", labelWidget.getHorizontalChildrenAlign() );
+      assertEquals( "right", this.getTextElement().style.textAlign );
+    },
+    
+    /////////
+    // helper
+    
+    setUp : function() {
+      shell = TestUtil.createShellByProtocol( "w2" );
+    },
+    
+    tearDown : function() {
+      shell.destroy();
+      shell = null;
+      if( labelWidget !== null ) {
+        labelWidget.destroy();
+      }
+      labelWidget = null;
+    },
+    
+    createLabel : function( styles, properties ) {
+      Processor.processOperation( {
         "target" : "w3",
         "action" : "create",
         "type" : "rwt.widgets.Label",
         "properties" : {
-          "style" : [ "WRAP" ],
-          "parent" : "w2",
-          "alignment" : "right"
+          "style" : styles,
+          "parent" : "w2"
         }
       } );
-      var ObjectManager = org.eclipse.rwt.protocol.ObjectManager;
-      var widget = ObjectManager.getObject( "w3" );
-      assertEquals( "right", widget.getLabelObject().getTextAlign() );
-      assertEquals( "right", widget.getHorizontalChildrenAlign() );
-      shell.destroy();
-      widget.destroy();
+      if( properties ) {
+        Processor.processOperation( {
+          "target" : "w3",
+          "action" : "set",
+          "properties" : properties
+        } );
+      }
+      labelWidget = ObjectManager.getObject( "w3" );
+      TestUtil.flush(); // LabelUtil delays setter for some BS reason...
+      TestUtil.flush(); // TWICE!!
+    },
+    
+    getTextElement : function() {
+      return labelWidget.getLabelObject()._getTargetNode();
+    },
+
+    getImageElement : function() {
+      return labelWidget.getIconObject()._image;
     }
 
   }
   
 } );
+
+}());
