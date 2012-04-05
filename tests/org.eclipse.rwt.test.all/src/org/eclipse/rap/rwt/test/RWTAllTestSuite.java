21c7a46e19497c861f4149fc4062c5da3676620c
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-04-05 08:50:34 +0000

    Fixed - 305639: Rename org.eclipse.rwt.test.all https://bugs.eclipse.org/bugs/show_bug.cgi?id=305639

--- tests/org.eclipse.rwt.test.all/src/org/eclipse/rap/rwt/test/RWTAllTestSuite.java
+++ tests/org.eclipse.rwt.test.all/src/org/eclipse/rap/rwt/test/RWTAllTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2009, 2011 EclipseSource and others.
+* Copyright (c) 2009, 2012 EclipseSource and others.
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
 * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
 * Contributors:
 *    EclipseSource - initial API and implementation
 *******************************************************************************/
-package org.eclipse.rwt.test;
+package org.eclipse.rap.rwt.test;
 
 import java.io.*;
 import java.net.*;
