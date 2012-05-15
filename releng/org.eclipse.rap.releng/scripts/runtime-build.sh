e23380b15b58ae769206c31ccaaf3c8abe603045
Markus Knauer <mknauer@eclipsesource.com>
2012-05-15 13:52:43 +0200

    Update the runtime-build script to Git and the new layout

--- releng/org.eclipse.rap.releng/scripts/runtime-build.sh
+++ releng/org.eclipse.rap.releng/scripts/runtime-build.sh
@@ -7,31 +7,18 @@
 SCRIPTS_DIR=$(dirname $(readlink -nm $0))
 . $SCRIPTS_DIR/build-environment.sh
 
-if [ -z "$CVS_TAG" ]; then
-  echo CVS_TAG is not set
-  exit 1
-fi
-
 if [ "${BUILD_TYPE:0:1}" == "S" ]; then
   sign=true
 else
   sign=false
 fi
 
-# Cleanup workspace dir
-test -n "$WORKSPACE" -a -d "$WORKSPACE" && rm -rf "$WORKSPACE"/*
-
-######################################################################
-# Checkout Repository
-
-cd "$WORKSPACE"
-echo "checking out $CVS_TAG"
-cvs -Q -d :local:/cvsroot/rt co -P -d source -r $CVS_TAG org.eclipse.rap || exit 1
+cd "$WORKSPACE/org.eclipse.rap"
 
 ######################################################################
 # Build RAP Runtime
 
-cd "$WORKSPACE/source/releng/org.eclipse.rap.releng/runtime"
+cd "$WORKSPACE/org.eclipse.rap/releng/org.eclipse.rap.releng"
 echo "Running maven on $PWD, sign=$sign"
 $MVN -e clean package -Dsign=$sign
 exitcode=$?
@@ -39,10 +26,10 @@ if [ "$exitcode" != "0" ]; then
   echo "Maven exited with error code " + $exitcode
 fi
 
-if [ -d runtime-repository/target/fixedPacked ]; then
-  mv runtime-repository/target/fixedPacked "$WORKSPACE/runtimeRepo"
+if [ -d repository/target/fixedPacked ]; then
+  mv repository/target/fixedPacked "$WORKSPACE/runtimeRepo"
 else
-  mv runtime-repository/target/repository "$WORKSPACE/runtimeRepo"
+  mv repository/target/repository "$WORKSPACE/runtimeRepo"
 fi
 
 VERSION=$(ls "$WORKSPACE"/runtimeRepo/features/org.eclipse.rap.runtime_1.5.0.*.jar | sed 's/.*_\([0-9.-]\+\)\..*\.jar/\1/')
@@ -55,7 +42,7 @@ test -n "$TIMESTAMP" || exit 1
 ######################################################################
 # Build Aggregation Repository
 
-cd "$WORKSPACE/source/releng/org.eclipse.rap.releng/runtime"
+cd "$WORKSPACE/org.eclipse.rap/releng/org.eclipse.rap.releng"
 echo "Running maven with pom-aggregation.xml on $PWD, sign=$sign"
 $MVN -e -f pom-aggregation.xml clean package -DruntimeRepo="file://$WORKSPACE/runtimeRepo" || exit 1
 
@@ -63,16 +50,16 @@ $MVN -e -f pom-aggregation.xml clean package -DruntimeRepo="file://$WORKSPACE/ru
 zipFileName=rap-runtime-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip
 compatZipFileName=rap-runtime-compatibility-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip
 
-mv runtime-aggregation-repository/target/*.zip "$WORKSPACE/$zipFileName" || exit 1
+mv aggregation-repository/target/*.zip "$WORKSPACE/$zipFileName" || exit 1
 
-if [ "$sign" == "true" -a -d runtime-compatibility-repository/target ]; then
-  mv runtime-compatibility-repository/target/*.zip "$WORKSPACE/$compatZipFileName" || exit 1
+if [ "$sign" == "true" -a -d compatibility-repository/target ]; then
+  mv compatibility-repository/target/*.zip "$WORKSPACE/$compatZipFileName" || exit 1
 fi
 
 ######################################################################
 # Include legal files in zip
 
 cd "$WORKSPACE"
-cp -f source/releng/org.eclipse.rap.releng/legal/notice.html .
-cp -f source/releng/org.eclipse.rap.releng/legal/epl-v10.html .
+cp -f org.eclipse.rap/releng/org.eclipse.rap.releng/legal/notice.html .
+cp -f org.eclipse.rap/releng/org.eclipse.rap.releng/legal/epl-v10.html .
 zip "$zipFileName" notice.html epl-v10.html
