58019364cd69ca52b09b2d300208479156f16456
Tim Buschtoens <tbuschto@eclipsesource.com>
2012-05-15 13:24:26 +0200

    remove scrollbar from widget frame calculation
    See Bug 378951 - [Client] Widget.js uses dead constant SCROLLBAR_SIZE
    Overflow is only set to scroll in Scrollable.js, which hides the 
    scrollbar. Therefore this code is no longer needed.

--- bundles/org.eclipse.rap.rwt/js/qx/ui/core/Widget.js
+++ bundles/org.eclipse.rap.rwt/js/qx/ui/core/Widget.js
@@ -2425,29 +2425,11 @@ qx.Class.define( "qx.ui.core.Widget", {
 
     _computeFrameWidth : function() {
       var fw = this._cachedBorderLeft + this.getPaddingLeft() + this.getPaddingRight() + this._cachedBorderRight;
-      switch(this.getOverflow()) {
-        case "scroll":
-        case "scrollY":
-          fw += qx.ui.core.Widget.SCROLLBAR_SIZE;
-          break;
-        case "auto":
-          // This seems to be really hard to implement
-          break;
-      }
       return fw;
     },
 
     _computeFrameHeight : function() {
       var fh = this._cachedBorderTop + this.getPaddingTop() + this.getPaddingBottom() + this._cachedBorderBottom;
-      switch(this.getOverflow()) {
-        case "scroll":
-        case "scrollX":
-          fh += qx.ui.core.Widget.SCROLLBAR_SIZE;
-          break;
-        case "auto":
-          // This seems to be really hard to implement
-          break;
-      }
       return fh;
     },
 
