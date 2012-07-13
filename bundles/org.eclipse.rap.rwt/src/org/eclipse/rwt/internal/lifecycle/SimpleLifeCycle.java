38f49530e37418f762c89722e2dd901135d4e057
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-07-13 11:11:39 +0300

    Fix the error message in SimpleLifeCycle#sleep()
    382307: Improve exception message: SimpleLifeCycle does not support
    Display#sleep()
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=382307

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/SimpleLifeCycle.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rwt/internal/lifecycle/SimpleLifeCycle.java
@@ -153,7 +153,7 @@ public class SimpleLifeCycle extends LifeCycle {
 
   @Override
   public void sleep() {
-    String msg = "The " + getClass().getSimpleName() + " does not support Display#sleep().";
+    String msg = "The JEE compatibility mode does not support Display#sleep().";
     throw new UnsupportedOperationException( msg );
   }
 
