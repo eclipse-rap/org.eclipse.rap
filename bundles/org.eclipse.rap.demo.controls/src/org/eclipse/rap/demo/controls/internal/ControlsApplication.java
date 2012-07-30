ac5dd315441fde9cf075dc6ef26f88aa8337ae1d
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-07-30 09:38:51 +0300

    Split Controls and Workbench demos
    - new org.eclipse.rap.demo.controls bundles is created
    - make use of new OSGi API in Controls demo
    - remove Controls demo classes and resources from org.eclipse.rap.demo
    bundle
    
    386215: [Demo] Split Controls and Workbench demos
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=386215

---
+++ bundles/org.eclipse.rap.demo.controls/src/org/eclipse/rap/demo/controls/internal/ControlsApplication.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.demo.controls.internal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.demo.controls.ControlsDemo;
+import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.application.Application.OperationMode;
+import org.eclipse.rap.rwt.client.WebClient;
+
+
+public class ControlsApplication implements ApplicationConfiguration {
+
+  public void configure( Application application ) {
+    Map<String, String> properties = new HashMap<String, String>();
+    properties.put( WebClient.PAGE_TITLE, "RWT Controls Demo" );
+    application.setOperationMode( OperationMode.SWT_COMPATIBILITY );
+    application.addEntryPoint( "/controls", ControlsDemo.class, properties );
+    application.addStyleSheet( RWT.DEFAULT_THEME_ID, "theme/theme.css" );
+  }
+}
