260f96a00af9b82ccc06979decd3fec4ef381a79
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-09-05 17:00:23 +0300

    Add ClientMessage class and tests
    355728: Replace Ajax request parameters with JSON
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=355728

---
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/protocol/ClientMessage.java
@@ -0,0 +1,277 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.internal.protocol;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class ClientMessage {
+
+  private final JSONObject message;
+  private HashMap<String,List<Operation>> operationsMap;
+
+  public ClientMessage( String string ) {
+    String json = string.trim();
+    try {
+      message = new JSONObject( json );
+    } catch( JSONException e ) {
+      throw new IllegalArgumentException( "Could not parse json: " + json );
+    }
+    JSONArray operations;
+    try {
+      operations = message.getJSONArray( "operations" );
+    } catch( JSONException e ) {
+      throw new IllegalArgumentException( "Missing operations array: " + json );
+    }
+    try {
+      createOperationsMap( operations );
+    } catch( JSONException e ) {
+      throw new IllegalArgumentException( "Invalid operations array: " + json );
+    }
+  }
+
+  public Operation[] getAllOperations( String target ) {
+    return getOperations( Operation.class, target, null );
+  }
+
+  public SetOperation[] getSetOperations( String target ) {
+    return getOperations( SetOperation.class, target, null );
+  }
+
+  public SetOperation[] getSetOperations( String target, String property ) {
+    return getOperations( SetOperation.class, target, property );
+  }
+
+  public NotifyOperation[] getNotifyOperations( String target ) {
+    return getOperations( NotifyOperation.class, target, null );
+  }
+
+  public NotifyOperation[] getNotifyOperations( String target, String eventType ) {
+    List<NotifyOperation> result = new ArrayList<NotifyOperation>();
+    List<Operation> operations = operationsMap.get( target );
+    if( operations != null ) {
+      for( Operation operation : operations ) {
+        if( operation instanceof NotifyOperation ) {
+          NotifyOperation currentOperation = ( NotifyOperation )operation;
+          if( eventType == null || currentOperation.getEventType().equals( eventType ) ) {
+            result.add( currentOperation );
+          }
+        }
+      }
+    }
+    return result.toArray( new NotifyOperation[ 0 ] );
+  }
+
+  public CallOperation[] getCallOperations( String target ) {
+    return getOperations( CallOperation.class, target, null );
+  }
+
+  @Override
+  public String toString() {
+    try {
+      return message.toString( 2 );
+    } catch( JSONException e ) {
+      throw new RuntimeException( "Formatting failed" );
+    }
+  }
+
+  private void createOperationsMap( JSONArray operations ) throws JSONException {
+    operationsMap = new HashMap<String,List<Operation>>();
+    for( int i = 0; i < operations.length(); i++ ) {
+      Operation operation = createOperation( operations.getJSONArray( i ) );
+      appendOperation( operation );
+    }
+  }
+
+  private Operation createOperation( JSONArray data ) {
+    Operation result = null;
+    String action = getOperationAction( data );
+    if( action.equals( "set" ) ) {
+      result = new SetOperation( data );
+    } else if( action.equals( "notify" ) ) {
+      result = new NotifyOperation( data );
+    } else if( action.equals( "call" ) ) {
+      result = new CallOperation( data );
+    } else {
+      throw new IllegalArgumentException( "Unknown operation action: " + action );
+    }
+    return result;
+  }
+
+  private void appendOperation( Operation operation ) {
+    String target = operation.getTarget();
+    List<Operation> operationsList = operationsMap.get( target );
+    if( operationsList == null ) {
+      operationsList = new ArrayList<Operation>();
+    }
+    operationsList.add( operation );
+    operationsMap.put( target, operationsList );
+  }
+
+  private String getOperationAction( JSONArray operation ) {
+    String result;
+    try {
+      result = operation.getString( 0 );
+    } catch( JSONException e ) {
+      throw new IllegalArgumentException( "Could not find action for operation " + operation );
+    }
+    return result;
+  }
+
+  @SuppressWarnings( "unchecked" )
+  private <T> T[] getOperations( Class<T> opClass, String target, String property ) {
+    List<T> result = new ArrayList<T>();
+    List<Operation> operations = operationsMap.get( target );
+    if( operations != null ) {
+      for( Operation operation : operations ) {
+        if(    opClass.isInstance( operation )
+            && ( property == null || operation.getPropertyNames().contains( property ) ) ) {
+          result.add( ( T )operation );
+        }
+      }
+    }
+    return result.toArray( ( T[] )Array.newInstance( opClass, 0 ) );
+  }
+
+  public abstract class Operation {
+
+    private final String target;
+
+    private Operation( JSONArray operation ) {
+      try {
+        target = operation.getString( 1 );
+      } catch( JSONException e ) {
+        throw new IllegalArgumentException( "Invalid operation target", e );
+      }
+    }
+
+    public String getTarget() {
+      return target;
+    }
+
+    public List<String> getPropertyNames() {
+      JSONObject properties = getProperties();
+      String[] names = JSONObject.getNames( properties );
+      return Arrays.asList( names );
+    }
+
+    public Object getProperty( String key ) throws PropertyNotFoundException {
+      Object result;
+      JSONObject properties = getProperties();
+      try {
+        result = properties.get( key );
+      } catch( JSONException exception ) {
+        throw new PropertyNotFoundException( "Property does not exist for key: " + key );
+      }
+      return result;
+    }
+
+    abstract protected JSONObject getProperties();
+
+  }
+
+  public final class SetOperation extends Operation {
+
+    private final JSONObject properties;
+
+    private SetOperation( JSONArray operation ) {
+      super( operation );
+      try {
+        properties = operation.getJSONObject( 2 );
+      } catch( JSONException e ) {
+        throw new IllegalArgumentException( "Properties object missing in operation", e );
+      }
+    }
+
+    @Override
+    protected JSONObject getProperties() {
+      return properties;
+    }
+
+  }
+
+  public final class NotifyOperation extends Operation {
+
+    private final String eventType;
+    private final JSONObject properties;
+
+    private NotifyOperation( JSONArray operation ) {
+      super( operation );
+      try {
+        eventType = operation.getString( 2 );
+      } catch( JSONException e ) {
+        throw new IllegalArgumentException( "Event type missing in operation", e );
+      }
+      try {
+        properties = operation.getJSONObject( 3 );
+      } catch( JSONException e ) {
+        throw new IllegalArgumentException( "Properties object missing in operation", e );
+      }
+    }
+
+    public String getEventType() {
+      return eventType;
+    }
+
+    @Override
+    protected JSONObject getProperties() {
+      return properties;
+    }
+
+  }
+
+  public final class CallOperation extends Operation {
+
+    private final String methodName;
+    private final JSONObject properties;
+
+    private CallOperation( JSONArray operation ) {
+      super( operation );
+      try {
+        methodName = operation.getString( 2 );
+      } catch( JSONException e ) {
+        throw new IllegalArgumentException( "Method name missing in operation", e );
+      }
+      try {
+        properties = operation.getJSONObject( 3 );
+      } catch( JSONException e ) {
+        throw new IllegalArgumentException( "Properties object missing in operation", e );
+      }
+    }
+
+    public String getMethodName() {
+      return methodName;
+    }
+
+    @Override
+    protected JSONObject getProperties() {
+      return properties;
+    }
+
+  }
+
+  public final class PropertyNotFoundException extends Exception {
+
+    public PropertyNotFoundException( String message ) {
+      super(message);
+    }
+
+  }
+
+}
---
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rap/rwt/internal/protocol/ClientMessage_Test.java
@@ -0,0 +1,304 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.internal.protocol;
+
+import org.eclipse.rap.rwt.internal.protocol.ClientMessage.CallOperation;
+import org.eclipse.rap.rwt.internal.protocol.ClientMessage.Operation;
+import org.eclipse.rap.rwt.internal.protocol.ClientMessage.NotifyOperation;
+import org.eclipse.rap.rwt.internal.protocol.ClientMessage.PropertyNotFoundException;
+import org.eclipse.rap.rwt.internal.protocol.ClientMessage.SetOperation;
+
+import junit.framework.TestCase;
+
+
+public class ClientMessage_Test extends TestCase {
+
+  public void testConstructWithNull() {
+    try {
+      new ClientMessage( null );
+      fail();
+    } catch( NullPointerException expected ) {
+    }
+  }
+
+  public void testConstructWithEmptyString() {
+    try {
+      new ClientMessage( "" );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+      assertTrue( expected.getMessage().contains( "Could not parse json" ) );
+    }
+  }
+
+  public void testConstructWithInvalidJson() {
+    try {
+      new ClientMessage( "{" );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+      assertTrue( expected.getMessage().contains( "Could not parse json" ) );
+    }
+  }
+
+  public void testConstructWithoutOperations() {
+    try {
+      new ClientMessage( "{ \"foo\": 23 }" );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+      assertTrue( expected.getMessage().contains( "Missing operations array" ) );
+    }
+  }
+
+  public void testConstructWithInvalidOperations() {
+    try {
+      new ClientMessage( "{ \"operations\": 23 }" );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+      assertTrue( expected.getMessage().contains( "Missing operations array" ) );
+    }
+  }
+
+  public void testConstructWithOperationUnknownType() {
+    try {
+      new ClientMessage( "{ \"operations\" : [ [ \"abc\", \"w3\", { \"action\" : \"foo\" } ] ] }" );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+      assertTrue( expected.getMessage().contains( "Unknown operation action: abc" ) );
+    }
+  }
+
+  public void testGetAllOperations() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\" } ]," );
+    json.append( "[ \"set\", \"w4\", { \"p2\" : \"bar\" } ]," );
+    json.append( "[ \"notify\", \"w3\", \"widgetSelected\", {} ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    Operation[] operations = message.getAllOperations( "w3" );
+
+    assertEquals( 2, operations.length );
+    assertTrue( operations[ 0 ] instanceof SetOperation );
+    assertTrue( operations[ 1 ] instanceof NotifyOperation );
+  }
+
+  public void testGetAllOperations_NoOperations() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\" } ]," );
+    json.append( "[ \"set\", \"w4\", { \"p2\" : \"bar\" } ]," );
+    json.append( "[ \"notify\", \"w3\", \"widgetSelected\", {} ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    Operation[] operations = message.getAllOperations( "w5" );
+
+    assertEquals( 0, operations.length );
+  }
+
+  public void testGetSetOperations() throws PropertyNotFoundException {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\" } ]," );
+    json.append( "[ \"notify\", \"w3\", \"widgetSelected\", {} ]," );
+    json.append( "[ \"set\", \"w3\", { \"p2\" : \"bar\" } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    SetOperation[] operations = message.getSetOperations( "w3" );
+
+    assertEquals( 2, operations.length );
+    assertEquals( "foo", operations[ 0 ].getProperty( "p1" ) );
+    assertEquals( "bar", operations[ 1 ].getProperty( "p2" ) );
+  }
+
+  public void testGetSetOperations_ByProperty() throws PropertyNotFoundException {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\" } ]," );
+    json.append( "[ \"set\", \"w3\", { \"p2\" : \"foo\" } ]," );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"bar\" } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    SetOperation[] operations = message.getSetOperations( "w3", "p1" );
+
+    assertEquals( 2, operations.length );
+    assertEquals( "foo", operations[ 0 ].getProperty( "p1" ) );
+    assertEquals( "bar", operations[ 1 ].getProperty( "p1" ) );
+  }
+
+  public void testGetNotifyOperations() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\" } ]," );
+    json.append( "[ \"notify\", \"w3\", \"widgetSelected\", {} ]," );
+    json.append( "[ \"set\", \"w3\", { \"p2\" : \"bar\" } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    NotifyOperation[] operations = message.getNotifyOperations( "w3" );
+
+    assertEquals( 1, operations.length );
+    assertEquals( "widgetSelected", operations[ 0 ].getEventType() );
+  }
+
+  public void testGetCallOperations() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\" } ]," );
+    json.append( "[ \"call\", \"w3\", \"store\", {} ]," );
+    json.append( "[ \"set\", \"w3\", { \"p2\" : \"bar\" } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    CallOperation[] operations = message.getCallOperations( "w3" );
+
+    assertEquals( 1, operations.length );
+    assertEquals( "store", operations[ 0 ].getMethodName() );
+  }
+
+  public void testSetOperation() throws PropertyNotFoundException {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\", \"p2\" : true } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    SetOperation operation = message.getSetOperations( "w3" )[ 0 ];
+
+    assertEquals( "w3", operation.getTarget() );
+    assertEquals( "foo", operation.getProperty( "p1" ) );
+    assertEquals( Boolean.TRUE, operation.getProperty( "p2" ) );
+  }
+
+  public void testSetOperation_WithoutTarget() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", { \"p1\" : \"foo\", \"p2\" : true } ]" );
+    json.append( "] }" );
+
+    try {
+      new ClientMessage( json.toString() );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+
+  public void testSetOperation_WithoutProperties() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\" ]" );
+    json.append( "] }" );
+
+    try {
+      new ClientMessage( json.toString() );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+
+  public void testSetOperation_InvalidProperty() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"set\", \"w3\", { \"p1\" : \"foo\", \"p2\" : true } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+    SetOperation operation = message.getSetOperations( "w3" )[ 0 ];
+
+    try {
+      operation.getProperty( "abc" );
+      fail();
+    } catch( PropertyNotFoundException expected ) {
+    }
+  }
+
+  public void testNotifyOperation() throws PropertyNotFoundException {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"notify\", \"w3\", \"widgetSelected\", { \"check\" : true } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    NotifyOperation operation = message.getNotifyOperations( "w3" )[ 0 ];
+
+    assertEquals( "w3", operation.getTarget() );
+    assertEquals( "widgetSelected", operation.getEventType() );
+    assertEquals( Boolean.TRUE, operation.getProperty( "check" ) );
+  }
+
+  public void testNotifyOperation_WithoutEventType() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"notify\", \"w3\", { \"check\" : true } ]" );
+    json.append( "] }" );
+
+    try {
+      new ClientMessage( json.toString() );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+
+  public void testNotifyOperation_WithoutProperties() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"notify\", \"w3\", \"widgetSelected\"" );
+    json.append( "] }" );
+
+    try {
+      new ClientMessage( json.toString() );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+
+  public void testCallOperation() throws PropertyNotFoundException {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"call\", \"w3\", \"store\", { \"id\" : 123 } ]" );
+    json.append( "] }" );
+    ClientMessage message = new ClientMessage( json.toString() );
+
+    CallOperation operation = message.getCallOperations( "w3" )[ 0 ];
+
+    assertEquals( "w3", operation.getTarget() );
+    assertEquals( "store", operation.getMethodName() );
+    assertEquals( Integer.valueOf( 123 ), operation.getProperty( "id" ) );
+  }
+
+  public void testCallOperation_WithoutMethodName() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"call\", \"w3\", { \"id\" : 123 } ]" );
+    json.append( "] }" );
+
+    try {
+      new ClientMessage( json.toString() );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+
+  public void testCallOperation_WithoutProperties() {
+    StringBuilder json = new StringBuilder();
+    json.append( "{ \"operations\" : [" );
+    json.append( "[ \"call\", \"w3\", \"store\"" );
+    json.append( "] }" );
+
+    try {
+      new ClientMessage( json.toString() );
+      fail();
+    } catch( IllegalArgumentException expected ) {
+    }
+  }
+
+}
