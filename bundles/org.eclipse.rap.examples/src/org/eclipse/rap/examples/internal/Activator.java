c090a32cb9d2a2061049472975b1e3c6e0a9e77d
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-09-22 23:11:52 +0200

    Register service without DS in examples demo
    Declarative services are not supported in every environment by defualt.
    Since we have an activator anyway, register the configuration there.

--- bundles/org.eclipse.rap.examples/src/org/eclipse/rap/examples/internal/Activator.java
+++ bundles/org.eclipse.rap.examples/src/org/eclipse/rap/examples/internal/Activator.java
@@ -10,22 +10,34 @@
  ******************************************************************************/
 package org.eclipse.rap.examples.internal;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 
 public class Activator implements BundleActivator {
 
   private static Activator plugin;
   private ExampleContributionsTracker serviceTracker;
+  private ServiceRegistration<?> registration;
 
   public void start( BundleContext context ) throws Exception {
     plugin = this;
     serviceTracker = new ExampleContributionsTracker( context );
     serviceTracker.open();
+    Dictionary<String, String> properties = new Hashtable<String, String>();
+    properties.put( "contextName", "rapdemo" );
+    registration = context.registerService( ApplicationConfiguration.class.getName(),
+                                            ExampleApplication.class,
+                                            properties );
   }
 
   public void stop( BundleContext context ) throws Exception {
+    registration.unregister();
     serviceTracker.close();
     serviceTracker = null;
     plugin = null;
