c40b29a964309f7f505de5378ae7afaf04d33bb8
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-11-26 10:56:15 +0100

    Introduce registry for RemoteObjects
    RemoteObjectRegistry keeps instances of RemoteObjectImpl by id.
    This registry will be used to find the remote object for an incoming
    message. As long as the phase model is in place, the registry will also
    be used to iterate over all remote objects.

---
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/remote/RemoteObjectRegistry.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.internal.remote;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.SingletonUtil;
+
+
+public class RemoteObjectRegistry {
+
+  private final Map<String, RemoteObjectImpl> remoteObjects;
+
+  private RemoteObjectRegistry() {
+    // TODO [rst] This is a linked list in order to provide an ordered list of remote objects to
+    //            iterate in the render phase. Once messages are rendered directly, this doesn't
+    //            have to be a linked list anymore.
+    remoteObjects = new LinkedHashMap<String, RemoteObjectImpl>();
+  }
+
+  public static RemoteObjectRegistry getInstance() {
+    return SingletonUtil.getSessionInstance( RemoteObjectRegistry.class );
+  }
+
+  public void register( RemoteObjectImpl object ) {
+    String id = object.getId();
+    if( remoteObjects.containsKey( id ) ) {
+      throw new IllegalArgumentException( "Remote object already registered, id: " + id );
+    }
+    remoteObjects.put( id, object );
+  }
+
+  public void remove( RemoteObjectImpl object ) {
+    String id = object.getId();
+    if( !remoteObjects.containsKey( id ) ) {
+      throw new IllegalArgumentException( "Remote object not found in registry, id: " + id );
+    }
+    remoteObjects.remove( id );
+  }
+
+  public RemoteObject get( String id ) {
+    return remoteObjects.get( id );
+  }
+
+  public List<RemoteObjectImpl> getAllObjects() {
+    return new ArrayList<RemoteObjectImpl>( remoteObjects.values() );
+  }
+
+}
---
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rap/rwt/internal/remote/RemoteObjectRegistry_Test.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+* Copyright (c) 2012 EclipseSource and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    EclipseSource - initial API and implementation
+*******************************************************************************/
+package org.eclipse.rap.rwt.internal.remote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.eclipse.rap.rwt.testfixture.Fixture;
+
+
+public class RemoteObjectRegistry_Test extends TestCase {
+
+  private RemoteObjectRegistry registry;
+
+  @Override
+  protected void setUp() throws Exception {
+    Fixture.setUp();
+    registry = RemoteObjectRegistry.getInstance();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    Fixture.tearDown();
+  }
+
+  public void testReturnsSingletonInstance() {
+    assertNotNull( registry );
+    assertSame( registry, RemoteObjectRegistry.getInstance() );
+  }
+
+  public void testCanRegisterRemoteObject() {
+    RemoteObjectImpl remoteObject = new RemoteObjectImpl( "id", "type" );
+    registry.register( remoteObject );
+
+    RemoteObject result = registry.get( "id" );
+
+    assertSame( remoteObject, result );
+  }
+
+  public void testCanRemoveRegisteredObject() {
+    RemoteObjectImpl remoteObject = new RemoteObjectImpl( "id", "type" );
+    registry.register( remoteObject );
+
+    registry.remove( remoteObject );
+
+    assertNull( registry.get( "id" ) );
+  }
+
+  public void testPreventsRegisterDuplicateIds() {
+    registry.register( new RemoteObjectImpl( "id", "type" ) );
+
+    try {
+      registry.register( new RemoteObjectImpl( "id", "type" ) );
+      fail();
+    } catch( IllegalArgumentException exception ) {
+      assertEquals( "Remote object already registered, id: id", exception.getMessage() );
+    }
+  }
+
+  public void testPreventsRemoveNonExisting() {
+    try {
+      registry.remove( new RemoteObjectImpl( "id", "type" ) );
+      fail();
+    } catch( IllegalArgumentException exception ) {
+      assertEquals( "Remote object not found in registry, id: id", exception.getMessage() );
+    }
+  }
+
+  public void testReturnsOrderedListOfRegisteredObjects() {
+    for( int i = 0; i < 10; i++ ) {
+      registry.register( new RemoteObjectImpl( "id" + i, "type" ) );
+    }
+
+    List<RemoteObjectImpl> allObjects = registry.getAllObjects();
+
+    assertEquals( "id0 id1 id2 id3 id4 id5 id6 id7 id8 id9", join( getIds( allObjects ), " " ) );
+  }
+
+  private static List<String> getIds( List<RemoteObjectImpl> allObjects ) {
+    List<String> ids = new ArrayList<String>();
+    for( RemoteObjectImpl object : allObjects ) {
+      ids.add( object.getId() );
+    }
+    return ids;
+  }
+
+  private static String join( List<String> list, String glue ) {
+    StringBuilder builder = new StringBuilder();
+    for( String string : list ) {
+      if( builder.length() > 0 ) {
+        builder.append( glue );
+      }
+      builder.append( string );
+    }
+    return builder.toString();
+  }
+
+}
