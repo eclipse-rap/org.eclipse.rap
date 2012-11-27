9b748d5a759e246cb7755c350319475afc9f7191
Tim Buschtöns <tbuschto@eclipsesource.com>
2012-11-27 16:19:54 +0100

    Implement function rap.getRemoteObject()

--- bundles/org.eclipse.rap.rwt/js/rap.js
+++ bundles/org.eclipse.rap.rwt/js/rap.js
@@ -14,9 +14,64 @@
 rap = {
 
   registerTypeHandler : function( type, handler ) {
+    handler.isPublic = true;
     rwt.protocol.AdapterRegistry.add( type, handler );
+  },
+
+  getObject : function( id ) {
+    var entry = rwt.protocol.ObjectRegistry.getEntry( id );
+    var result;
+    if( entry && entry.adapter.isPublic ) {
+      result = entry.object;
+    } else {
+      result = getWrapperFor( entry.object );
+    }
+    return result;
+  },
+
+  getRemoteObject : function( obj ) {
+    return rwt.remote.Server.getInstance().getServerObject( obj );
   }
 
 };
+
+var wrapperMap = {};
+
+function getWrapperFor( obj ) {
+  var result = null;
+  if( obj instanceof Object ) {
+    var hash = qx.core.Object.toHashCode( obj );
+    if( wrapperMap[ hash ] == null ) {
+      if( obj instanceof rwt.widgets.Composite ) {
+        wrapperMap[ hash ] = new ParentWrapper( obj );
+      } else {
+        wrapperMap[ hash ] = {};
+      }
+    }
+    result = wrapperMap[ hash ];
+  }
+  return result;
+}
+
+function ParentWrapper( widget ) {
+  var children = null;
+  if( !widget.isCreated() ) {
+    children = [];
+    widget.addEventListener( "create", function() {
+      for( var i = 0; i < children.length; i++ ) {
+        widget._getTargetNode().appendChild( children[ i ] );
+      }
+      widget.removeEventListener( "create", arguments.callee );
+      children = null;
+    } );
+  }
+  this.append = function( childElement ) {
+    if( children ) {
+      children.push( childElement );
+    } else {
+      widget._getTargetNode().appendChild( childElement );
+    }
+  };
+}
 
 }());
--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/ClientAPITest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/ClientAPITest.js
@@ -9,6 +9,13 @@
  *    EclipseSource - initial API and implementation
  ******************************************************************************/
 
+(function(){
+
+var ObjectRegistry = rwt.protocol.ObjectRegistry;
+var AdapterRegistry = rwt.protocol.AdapterRegistry;
+var MessageProcessor = rwt.protocol.MessageProcessor;
+var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
+
 qx.Class.define( "org.eclipse.rwt.test.tests.ClientAPITest", {
 
   extend : qx.core.Object,
@@ -20,11 +27,124 @@ qx.Class.define( "org.eclipse.rwt.test.tests.ClientAPITest", {
 
       rap.registerTypeHandler( "myTestType", handler );
 
-      assertIdentical( handler, rwt.protocol.AdapterRegistry.getAdapter( "myTestType" ) );
-      rwt.protocol.AdapterRegistry.remove( "myTestType" );
+      assertIdentical( handler, AdapterRegistry.getAdapter( "myTestType" ) );
+      AdapterRegistry.remove( "myTestType" );
+    },
+
+    testGetServerObjectDelegation : function() {
+      var handler = {};
+      var obj = {};
+      rap.registerTypeHandler( "myTestType", handler );
+      ObjectRegistry.add( "r1", obj, handler );
+
+      var result = rap.getRemoteObject( obj );
+
+      assertIdentical( result, rwt.remote.Server.getInstance().getServerObject( obj ) );
+      AdapterRegistry.remove( "myTestType" );
+    },
+
+    testGetObject : function() {
+      var handler = {};
+      var obj = {};
+      rap.registerTypeHandler( "myTestType", handler );
+      ObjectRegistry.add( "r1", obj, handler );
+
+      var result = rap.getObject( "r1" );
+
+      assertIdentical( obj, result );
+      AdapterRegistry.remove( "myTestType" );
+    },
+
+    testGetInternalObject : function() {
+      var result = rap.getObject( "w2" );
+
+      assertFalse( result === ObjectRegistry.getObject( "w2" ) );
+    },
+
+    testGetInternalObjectTwice : function() {
+      var resultOne = rap.getObject( "w2" );
+      var resultTwo = rap.getObject( "w2" );
+
+      assertTrue( resultOne === resultTwo );
+    },
+
+    testCompositeWrapperAppend : function() {
+      MessageProcessor.processOperationArray( [ "create", "w3", "rwt.widgets.Composite", {
+          "style" : [ "BORDER" ],
+          "parent" : "w2"
+        }
+      ] );
+      TestUtil.flush();
+      var element = document.createElement( "div" );
+
+      rap.getObject( "w3" ).append( element );
+
+      var composite = ObjectRegistry.getObject( "w3" );
+      assertIdentical( composite._getTargetNode(), element.parentNode );
+    },
+
+    testCompositeWrapperAppendBeforeFlush : function() {
+      MessageProcessor.processOperationArray( [ "create", "w3", "rwt.widgets.Composite", {
+          "style" : [ "BORDER" ],
+          "parent" : "w2"
+        }
+      ] );
+      var element = document.createElement( "div" );
+
+      rap.getObject( "w3" ).append( element );
+      TestUtil.flush();
+
+      var composite = ObjectRegistry.getObject( "w3" );
+      assertIdentical( composite._getTargetNode(), element.parentNode );
+    },
+
+    testCompositeWrapperAppendMultipleBeforeFlush : function() {
+      MessageProcessor.processOperationArray( [ "create", "w3", "rwt.widgets.Composite", {
+          "style" : [ "BORDER" ],
+          "parent" : "w2"
+        }
+      ] );
+      var element = document.createElement( "div" );
+      var otherElement = document.createElement( "div" );
+
+      rap.getObject( "w3" ).append( element );
+      rap.getObject( "w3" ).append( otherElement );
+      TestUtil.flush();
+
+      var composite = ObjectRegistry.getObject( "w3" );
+      assertIdentical( composite._getTargetNode(), element.parentNode );
+      assertIdentical( composite._getTargetNode(), otherElement.parentNode );
+    },
+
+    testCompositeWrapperAppendMultipleBeforeAndAfterFlush : function() {
+      MessageProcessor.processOperationArray( [ "create", "w3", "rwt.widgets.Composite", {
+          "style" : [ "BORDER" ],
+          "parent" : "w2"
+        }
+      ] );
+      var element = document.createElement( "div" );
+      var otherElement = document.createElement( "div" );
+
+      rap.getObject( "w3" ).append( element );
+      TestUtil.flush();
+      rap.getObject( "w3" ).append( otherElement );
+
+      var composite = ObjectRegistry.getObject( "w3" );
+      assertIdentical( composite._getTargetNode(), element.parentNode );
+      assertIdentical( composite._getTargetNode(), otherElement.parentNode );
+    },
+
+    setUp : function() {
+      TestUtil.createShellByProtocol( "w2" );
+    },
+
+    tearDown : function() {
+      MessageProcessor.processOperationArray( [ "destroy", "w2"] );
     }
 
 
   }
 
 } );
+
+}());
