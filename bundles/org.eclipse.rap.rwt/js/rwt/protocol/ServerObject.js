bb96c8d72a35f9712258a95b1953de6d2ed4860a
Tim Buschtöns <tbuschto@eclipsesource.com>
2012-12-11 11:00:55 +0100

    Add JsDoc for RemoteObject

--- bundles/org.eclipse.rap.rwt/js/rwt/protocol/ServerObject.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/protocol/ServerObject.js
@@ -13,18 +13,46 @@
 
 var server = rwt.remote.Server.getInstance();
 
+/**
+ * @private
+ * @class Instances of RemoteObject represents the server-side counterpart of a client object
+ * and are used to write remote operations into the next protocol message. It can be obtained
+ * by rap.getRemoteObject()
+ * @exports rwt.protocol.ServerObject as RemoteObject
+ * @param {}
+ *
+ */
 rwt.protocol.ServerObject = function( id ) {
   this._id = id;
 };
 
 rwt.protocol.ServerObject.prototype = {
 
+  /**
+   * @description Sets the specified property of the remote object to the given value.
+   * Calling this method multiple times for the same key will overwrite the previous value,
+   * the message will not become longer.
+   * This method does not cause the message to be sent immediately.
+   * @param {string} property
+   * @param {var} value
+   */
   set : function( key, value ) {
     server.getMessageWriter().appendSet( this._id, key, value );
   },
 
-  // TODO [tb]: suppressSend should be a temporariy workaround for KeyEventSupport.js
+  /**
+   * @description Notifies the remote object a event of the given type occured.
+   * The properties object may contain any number of additional properties/fields.
+   * It may also be null or ommited. Sending an event of a type the server is currently not
+   * listening for (see TypeHandler#listener) is illegal usage of the RAP protocol, but is not
+   * prevented. Calling this method causes the message to be sent to the server within a
+   * few milliseconds.
+   * @param {string} event
+   * @param {Object} properties
+   * @param {}
+   */
   notify : function( event, properties, suppressSend ) {
+   // TODO [tb]: suppressSend should be a temporariy workaround for KeyEventSupport.js
     var actualProps = properties ? properties : {};
     server.getMessageWriter().appendNotify( this._id, event, actualProps );
     if( suppressSend !== true ) {
@@ -32,6 +60,14 @@ rwt.protocol.ServerObject.prototype = {
     }
   },
 
+  /**
+   * @description Intructs the remote object to call the given method.
+   * The properties object may contain any number of additional properties/fields. It may also be
+   * null or ommited. Calling this method causes the message to be sent to the server within a
+   * few milliseconds.
+   * @param {string} method
+   * @param {Object} properties
+   */
   call : function( method, properties ) {
     var actualProps = properties ? properties : {};
     server.getMessageWriter().appendCall( this._id, method, actualProps );
