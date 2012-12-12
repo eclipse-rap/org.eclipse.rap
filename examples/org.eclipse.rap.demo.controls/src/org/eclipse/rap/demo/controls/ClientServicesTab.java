6fc780bc8440fdfe5bd5f6b4cad363abc04756fb
Tim Buschtöns <tbuschto@eclipsesource.com>
2012-12-12 15:07:58 +0100

    Add ClientInfo Example to ClientServicesTab

--- examples/org.eclipse.rap.demo.controls/src/org/eclipse/rap/demo/controls/ClientServicesTab.java
+++ examples/org.eclipse.rap.demo.controls/src/org/eclipse/rap/demo/controls/ClientServicesTab.java
@@ -12,6 +12,7 @@
 package org.eclipse.rap.demo.controls;
 
 import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.client.service.ClientInfo;
 import org.eclipse.rap.rwt.client.service.JavaScriptExecutor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -20,6 +21,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 
@@ -37,16 +39,25 @@ public class ClientServicesTab extends ExampleTab {
   @Override
   protected void createExampleControls( final Composite parent ) {
     parent.setLayout( new GridLayout( 1, false ) );
+    createClientInfoExample( parent );
     createJavaScriptExecuterExample( parent );
   }
 
+  private void createClientInfoExample( Composite parent ) {
+    Group group = createGroup( parent, "ClientInfo", 2 );
+    ClientInfo info = RWT.getClient().getService( ClientInfo.class );
+    final Label locale = new Label( group, SWT.NONE );
+    locale.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
+    locale.setText( "Locale: " + info.getLocale() );
+    final Label timezone = new Label( group, SWT.NONE );
+    timezone.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
+    timezone.setText( "Timezone Offset: " + info.getTimezoneOffset() );
+  }
+
   private void createJavaScriptExecuterExample( Composite parent ) {
-    Group group = new Group( parent, SWT.NONE );
-    group.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
-    group.setLayout( new GridLayout( 2, false ) );
-    group.setText( "JavaScriptExecuter" );
+    Group group = createGroup( parent, "JavaScriptExecuter", 2 );
     final Text script = new Text( group, SWT.BORDER );
-    script.setText( "alert( \"foo\" ); ");
+    script.setText( "alert( \"foo\" );" );
     GridData layoutData = new GridData( SWT.FILL, SWT.FILL, true, false );
     script.setLayoutData( layoutData );
     Button execute = new Button( group, SWT.PUSH );
@@ -60,5 +71,13 @@ public class ClientServicesTab extends ExampleTab {
     };
     script.addListener( SWT.DefaultSelection, executeListener );
     execute.addListener( SWT.Selection, executeListener );
+  }
+
+  private Group createGroup( Composite parent, String title, int cols ) {
+    Group group = new Group( parent, SWT.NONE );
+    group.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
+    group.setLayout( new GridLayout( cols, false ) );
+    group.setText( title );
+    return group;
   }
 }
