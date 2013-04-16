378424c6b18471deacf3a5499be54ed7e97bde78
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-04-16 13:53:51 +0200

    Complete Javadoc for Client interface

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/client/Client.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/client/Client.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 EclipseSource and others.
+ * Copyright (c) 2012, 2013 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,8 @@ import org.eclipse.rap.rwt.client.service.ClientService;
  * Represents a remote client. The client that is connected to the current session can be obtained
  * by calling <code>RWT.getClient()</code>.
  * <p>
- * RWT clients can provide services, e.g. to allow access to device-specific capabilities.
+ * An RWT client can provide services, e.g. to allow access to device-specific capabilities. Those
+ * client services implement the common interface {@link ClientService}.
  * </p>
  *
  * @see WebClient
@@ -32,7 +33,9 @@ public interface Client extends Serializable {
   /**
    * Returns this client's implementation of a given service, if available.
    *
+   * @param type the type of the requested service, must be a subtype of ClientService
    * @return the requested service if provided by this client, otherwise <code>null</code>
+   * @see ClientService
    */
   <T extends ClientService> T getService( Class<T> type );
 
