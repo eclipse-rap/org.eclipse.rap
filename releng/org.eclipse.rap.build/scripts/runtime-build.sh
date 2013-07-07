d2d21111eb3dfae5940b8e9df2a03c14984cb6c1
Markus Knauer <mknauer@eclipsesource.com>
2013-07-07 09:51:45 +0200

    Merge 2-step Tycho build into single build
    - Merge missing pieces into main org.eclipse.rap.build and 
      update to single parent pom structure
    - Move target repository build into main build
    - Remove org.eclipse.rap.target.build project
    - Rework and simplify build script
    - Update to Tycho to 0.18.0

--- releng/org.eclipse.rap.build/scripts/runtime-build.sh
+++ releng/org.eclipse.rap.build/scripts/runtime-build.sh
@@ -4,6 +4,9 @@
 # All values are retrieved trough system variables set by Hudson.
 # See Job -> Configure... -> This build is parameterized
 
+######################################################################
+# set up environment
+
 SCRIPTS_DIR=$(dirname $(readlink -nm $0))
 . $SCRIPTS_DIR/build-environment.sh
 
@@ -15,22 +18,20 @@ else
   SIGNPROFILE=""
 fi
 
-######################################################################
-# Cleanup left-overs from previous run
-test -d "$WORKSPACE" || exit 1
-rm -rf "$WORKSPACE"/runtimeRepo "$WORKSPACE"/*.zip
+basedirectory="$WORKSPACE/org.eclipse.rap/releng/org.eclipse.rap.build"
 
 ######################################################################
 # clean up local Maven repository to circumvent p2 cache problems
+
 for II in .cache .meta p2 ; do
   echo "Remove directory ${MAVEN_LOCAL_REPO_PATH}/${II}" 
   rm -r ${MAVEN_LOCAL_REPO_PATH}/${II}
 done
 
 ######################################################################
-# Build RAP Runtime
+# build RAP Runtime
 
-cd "$WORKSPACE/org.eclipse.rap/releng/org.eclipse.rap.build"
+cd "$basedirectory"
 echo "Running maven on $PWD, $SIGNPROFILE"
 ${MVN} -e clean package $SIGNPROFILE -Dmaven.repo.local=${MAVEN_LOCAL_REPO_PATH}
 exitcode=$?
@@ -38,37 +39,24 @@ if [ "$exitcode" != "0" ]; then
   echo "Maven exited with error code " + $exitcode
 fi
 
-if [ -d repository/target/fixedPacked ]; then
-  mv repository/target/fixedPacked "$WORKSPACE/runtimeRepo" || exit 1
-else
-  mv repository/target/repository "$WORKSPACE/runtimeRepo" || exit 1
-fi
-
-VERSION=$(ls "$WORKSPACE"/runtimeRepo/features/org.eclipse.rap.sdk.feature_*.jar | sed 's/.*_\([0-9.-]\+\)\..*\.jar/\1/')
-TIMESTAMP=$(ls "$WORKSPACE"/runtimeRepo/features/org.eclipse.rap.sdk.feature_*.jar | sed 's/.*\.\([0-9-]\+\)\.jar/\1/')
-echo "Version is $VERSION"
-echo "Timestamp is $TIMESTAMP"
-test -n "$VERSION" || exit 1
-test -n "$TIMESTAMP" || exit 1
-
 ######################################################################
-# Build Aggregation Repository
+# rename ZIP archive
 
-cd "$WORKSPACE/org.eclipse.rap/releng/org.eclipse.rap.target.build"
-echo "Running maven on $PWD, sign=$sign"
-$MVN -e clean package -DruntimeRepo="file://$WORKSPACE/runtimeRepo" -Dmaven.repo.local=${MAVEN_LOCAL_REPO_PATH} -Dsign=$sign || exit 1
+repoDirectory="$basedirectory"/repository/target/repository
+VERSION=$(ls "$repoDirectory"/features/org.eclipse.rap.sdk.feature_*.jar | sed 's/.*_\([0-9.-]\+\)\..*\.jar/\1/')
+echo "Version is $VERSION"
+test -n "$VERSION" || exit 1
+TIMESTAMP=$(ls "$repoDirectory"/features/org.eclipse.rap.sdk.feature_*.jar | sed 's/.*\.\([0-9-]\+\)\.jar/\1/')
+echo "Timestamp is $TIMESTAMP"
+test -n "$TIMESTAMP" || exit 1
 
 # Example: rap-1.5.0-N-20110814-2110.zip
 zipFileName=rap-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip
 
-if [ -d repository.kepler/target/fixedSigned ]; then
-  mv repository.kepler/target/fixedSigned/*.zip "$WORKSPACE/$zipFileName" || exit 1
-else
-  mv repository.kepler/target/*.zip "$WORKSPACE/$zipFileName" || exit 1
-fi
+mv repository.kepler/target/*.zip "$WORKSPACE/$zipFileName" || exit 1
 
 ######################################################################
-# Include legal files in zip files
+# include legal files in zip files
 
 cd "$WORKSPACE"
 cp -f org.eclipse.rap/releng/org.eclipse.rap.build/legal/notice.html .
