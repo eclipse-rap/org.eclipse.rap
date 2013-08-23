83b3099728a91ea13f07a0e5c942d79a2adedba0
Ivan Furnadjiev <ivan@eclipsesource.com>
2013-08-23 17:46:06 +0300

    Create ToolTipOperationHandler

---
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/internal/widgets/tooltipkit/ToolTipOperationHandler_Test.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2013 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.swt.internal.widgets.tooltipkit;
+
+import static org.eclipse.rap.rwt.internal.protocol.ClientMessageConst.EVENT_SELECTION;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.eclipse.rap.json.JsonObject;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.ToolTip;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+
+public class ToolTipOperationHandler_Test {
+
+  private ToolTip toolTip;
+  private ToolTipOperationHandler handler;
+
+  @Before
+  public void setUp() {
+    toolTip = mock( ToolTip.class );
+    handler = new ToolTipOperationHandler( toolTip );
+  }
+
+  @Test
+  public void testHandleSetVisible() {
+    handler.handleSet( new JsonObject().add( "visible", false ) );
+
+    verify( toolTip ).setVisible( false );
+  }
+
+  @Test
+  public void testHandleNotifySelection() {
+    JsonObject properties = new JsonObject().add( "altKey", true ).add( "shiftKey", true );
+    handler.handleNotify( EVENT_SELECTION, properties );
+
+    ArgumentCaptor<Event> captor = ArgumentCaptor.forClass( Event.class );
+    verify( toolTip ).notifyListeners( eq( SWT.Selection ), captor.capture() );
+    assertEquals( SWT.ALT | SWT.SHIFT, captor.getValue().stateMask );
+  }
+
+}
