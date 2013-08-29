2ed408d0240d63e87d01d7f28d65c54a5867e466
Ivan Furnadjiev <ivan@eclipsesource.com>
2013-08-29 10:32:56 +0300

    Handle some properties sent by the client
    Button#text
    Label#text
    ProgressBar#selection

--- bundles/org.eclipse.rap.rwt/widgetkits/org/eclipse/swt/internal/widgets/labelkit/LabelOperationHandler.java
+++ bundles/org.eclipse.rap.rwt/widgetkits/org/eclipse/swt/internal/widgets/labelkit/LabelOperationHandler.java
@@ -10,14 +10,36 @@
  ******************************************************************************/
 package org.eclipse.swt.internal.widgets.labelkit;
 
+import org.eclipse.rap.json.JsonObject;
+import org.eclipse.rap.json.JsonValue;
 import org.eclipse.rap.rwt.internal.protocol.ControlOperationHandler;
 import org.eclipse.swt.widgets.Label;
 
 
 public class LabelOperationHandler extends ControlOperationHandler<Label> {
 
+  private static final String PROP_TEXT = "text";
+
   public LabelOperationHandler( Label label ) {
     super( label );
+  }
+
+  @Override
+  public void handleSet( Label label, JsonObject properties ) {
+    super.handleSet( label, properties );
+    handleSetText( label, properties );
+  }
+
+  /*
+   * PROTOCOL SET text
+   *
+   * @param text (String) the new label text
+   */
+  public void handleSetText( Label label, JsonObject properties ) {
+    JsonValue text = properties.get( PROP_TEXT );
+    if( text != null ) {
+      label.setText( text.asString() );
+    }
   }
 
 }
