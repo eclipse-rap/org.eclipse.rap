7342f81c7cdbbf13eb5479d475505de3b70fb60d
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-11-29 13:47:08 +0100

    Update copyright headers
    Update the copyright header of all source files that have been modified
    in 2013 but still had outdated copyright header.

--- bundles/org.eclipse.rap.rwt/js/rwt/widgets/base/AbstractSlider.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/widgets/base/AbstractSlider.js
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2008, 2013 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Innoopract Informationssysteme GmbH - initial API and implementation
- *   EclipseSource - ongoing development
+ *    Innoopract Informationssysteme GmbH - initial API and implementation
+ *    EclipseSource - ongoing development
  ******************************************************************************/
 
 rwt.qx.Class.define( "rwt.widgets.base.AbstractSlider", {
