973abfa28bbc3fbab3f8037c4de27c3006d86aad
Ivan Furnadjiev <ivan@eclipsesource.com>
2014-01-06 17:22:46 +0200

    Keep slashes decoded in URL fragment
    424791: Do not encode slashes in BrowserNavigation state
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=424791
    
    Change-Id: I6b926dfd41221b48b548515d523baa95a3a00a6f

--- bundles/org.eclipse.rap.rwt/js/rwt/client/BrowserNavigation.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/client/BrowserNavigation.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2013 1&1 Internet AG, Germany, http://www.1und1.de,
+ * Copyright (c) 2004, 2014 1&1 Internet AG, Germany, http://www.1und1.de,
  *                          EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -322,7 +322,7 @@ rwt.qx.Class.define("rwt.client.BrowserNavigation",
       "default" : function( state ) {
         // RAP [if] Fix for bug 295816
         //top.location.hash = "#" + encodeURIComponent(state);
-        window.location.hash = "#" + encodeURIComponent(state);
+        window.location.hash = "#" + encodeURIComponent( state ).replace( /%2F/g, "/" );
         return true;
       }
     }),
--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/BrowserNavigationTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/BrowserNavigationTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 EclipseSource and others.
+ * Copyright (c) 2012, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,6 +42,16 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.BrowserNavigationTest", {
       this._addEntryByProtocol( "id1", "text1" );
 
       assertEquals( "text1", browserNavigation._titles[ "id1" ] );
+    },
+
+    testAddByProtocol_doesNotEncodeSlashInHash : function() {
+      var browserNavigation = this._createBrowserNavigationByProtocol();
+
+      this._addEntryByProtocol( "id1/foo", "text1" );
+
+      if( !rwt.client.Client.isMshtml() ) {
+        assertEquals( "#id1/foo", window.location.hash );
+      }
     },
 
     testSendNavigated : [
