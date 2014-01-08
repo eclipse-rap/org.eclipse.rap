d78f06c05ec510ef407d38b052bb5f1e919ec822
Ivan Furnadjiev <ivan@eclipsesource.com>
2014-01-08 12:28:55 +0200

    Remove unnecessary SuppressWarnings
    Change-Id: Ie34f926d3ca2aa0e4a76124e2a91dc33826a86dd

--- examples/org.eclipse.rap.examples.pages/src/org/eclipse/rap/examples/pages/internal/PersonsTemplate.java
+++ examples/org.eclipse.rap.examples.pages/src/org/eclipse/rap/examples/pages/internal/PersonsTemplate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 EclipseSource and others.
+ * Copyright (c) 2013, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,6 @@ import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 
-@SuppressWarnings("restriction")
 public class PersonsTemplate extends Template {
   private static final String MY_FONT = "Tahoma, Geneva, sans-serif";
 
