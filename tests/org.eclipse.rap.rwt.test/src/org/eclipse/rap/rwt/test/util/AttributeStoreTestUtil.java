8ed152b5d570cd40c4fd75d564c828d58ace62a9
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-01-26 14:19:35 +0100

    Simplify SingletonManager by using AtomicReference
    Using AtomicReference as container type for the singleton instances
    allows to simplify the SingletonManager. A singletonHolder contains
    the singleton instance for a given type and acts as a type-specific
    lock at the same time.
    
    This two-step implementation ensures that the SingletonManager itself
    is not locked during the creation of a singleton instance.
    
    1) When a singleton is requested, we first obtain the unique instance
    of the singleton holder for this type using the SharedInstanceBuffer.
    This operation is synchronized in SharedInstanceBuffer. However, the
    creation of this AtomicReference is cheap.
    
    2) Then we check-and-create the instance in another synchronized block
    which locks only this specific singleton holder.
    
    This implementation is simpler and faster because it requires only a
    single hash lookup instead of two.
    
    Create a test util that allows us to add the attribute store behavior
    to mocked UISessions.
    
    Change-Id: I4ea09211cef2decafbfd0b653bf36572e85346b9

---
+++ tests/org.eclipse.rap.rwt.test/src/org/eclipse/rap/rwt/test/util/AttributeStoreTestUtil.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2014 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.test.util;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.service.UISession;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+
+public class AttributeStoreTestUtil {
+
+  private AttributeStoreTestUtil() {
+  }
+
+  public static void fakeAttributeStore( UISession mock ) {
+    StubbedAttributeStore store = new StubbedAttributeStore();
+    Mockito.doAnswer( store.setAttribute ).when( mock ).setAttribute( anyString(), any() );
+    Mockito.doAnswer( store.getAttribute ).when( mock ).getAttribute( anyString() );
+    Mockito.doAnswer( store.removeAttribute ).when( mock ).removeAttribute( anyString() );
+  }
+
+  private static class StubbedAttributeStore {
+
+    private final Map<String, Object> map = new HashMap<String, Object>();
+
+    private final Answer setAttribute = new Answer() {
+      public Object answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] arguments = invocation.getArguments();
+        map.put( ( String )arguments[ 0 ], arguments[ 1 ] );
+        return Boolean.TRUE;
+      }
+    };
+
+    private final Answer getAttribute = new Answer() {
+      public Object answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] arguments = invocation.getArguments();
+        return map.get( arguments[ 0 ] );
+      }
+    };
+
+    private final Answer removeAttribute = new Answer() {
+      public Object answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] arguments = invocation.getArguments();
+        return map.remove( arguments[ 0 ] );
+      }
+    };
+
+  }
+
+}
