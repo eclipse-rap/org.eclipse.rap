2fe4abe4c0b9fcf39c28514a08a6473b5bc48987
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-02-12 11:49:46 +0100

    JavaScript code formatting in LegacyProperty
    Change-Id: I5b34720934ec926f8b85b28c8f81984c2e64b648
    Signed-off-by: Ralf Sternberg <rsternberg@eclipsesource.com>

--- bundles/org.eclipse.rap.rwt/js/rwt/qx/LegacyProperty.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/qx/LegacyProperty.js
@@ -28,131 +28,106 @@ rwt.qx.Class.define( "rwt.qx.LegacyProperty", {
     /**
      * Adds a so-named fast property to a prototype.
      *
-     * @param config {Map} Configuration structure
-     * @param proto {Object} Prototype where the methods should be attached
-     */
-    addFastProperty : function(config, proto)
-    {
-      var vName = config.name;
-      var vUpName = rwt.util.Strings.toFirstUp(vName);
-
-      var vStorageField = "_value" + vUpName;
-      var vGetterName = "get" + vUpName;
-      var vSetterName = "set" + vUpName;
-      var vComputerName = "_compute" + vUpName;
-
-      proto[vStorageField] = typeof config.defaultValue !== "undefined" ? config.defaultValue : null;
-
-      if (config.noCompute)
-      {
-        proto[vGetterName] = function() {
-          return this[vStorageField];
-        };
-      }
-      else
-      {
-        proto[vGetterName] = function() {
-          return this[vStorageField] == null ? this[vStorageField] = this[vComputerName]() : this[vStorageField];
-        };
-      }
-
-      proto[vGetterName].self = proto.constructor;
-
-      if (config.setOnlyOnce)
-      {
-        proto[vSetterName] = function(vValue)
-        {
-          this[vStorageField] = vValue;
-          this[vSetterName] = null;
-
-          return vValue;
-        };
-      }
-      else
-      {
-        proto[vSetterName] = function(vValue) {
-          return this[vStorageField] = vValue;
-        };
-      }
-
-      proto[vSetterName].self = proto.constructor;
-
-      if (!config.noCompute)
-      {
-        proto[vComputerName] = function() {
-          return null;
-        };
-
-        proto[vComputerName].self = proto.constructor;
-      }
-    },
-
-
-    /**
-     * Adds a so-named cached property to a prototype
+     * config fields:
+     * - name
+     * - defaultValue
+     * - noCompute
+     * - setOnlyOnce
      *
      * @param config {Map} Configuration structure
      * @param proto {Object} Prototype where the methods should be attached
      */
-    addCachedProperty : function(config, proto)
-    {
+    addFastProperty : function( config, proto ) {
       var vName = config.name;
-      var vUpName = rwt.util.Strings.toFirstUp(vName);
+      var vUpName = rwt.util.Strings.toFirstUp( vName );
+      var vStorageField = "_value" + vUpName;
+      var vGetterName = "get" + vUpName;
+      var vSetterName = "set" + vUpName;
+      var vComputerName = "_compute" + vUpName;
+      proto[ vStorageField ] = typeof config.defaultValue !== "undefined" ? config.defaultValue
+                                                                          : null;
+      if( config.noCompute ) {
+        proto[ vGetterName ] = function() {
+          return this[ vStorageField ];
+        };
+      } else {
+        proto[ vGetterName ] = function() {
+          return this[ vStorageField ] == null ? this[ vStorageField ] = this[ vComputerName ]()
+                                               : this[ vStorageField ];
+        };
+      }
+      proto[ vGetterName ].self = proto.constructor;
+      if( config.setOnlyOnce ) {
+        proto[ vSetterName ] = function( vValue ) {
+          this[ vStorageField ] = vValue;
+          this[ vSetterName ] = null;
+          return vValue;
+        };
+      } else {
+        proto[ vSetterName ] = function( vValue ) {
+          return this[ vStorageField ] = vValue;
+        };
+      }
+      proto[ vSetterName ].self = proto.constructor;
+      if( !config.noCompute ) {
+        proto[ vComputerName ] = function() {
+          return null;
+        };
+        proto[ vComputerName ].self = proto.constructor;
+      }
+    },
 
+    /**
+     * Adds a so-named cached property to a prototype.
+     *
+     * config fields:
+     * - name
+     * - defaultValue
+     * - addToQueueRuntime
+     *
+     * @param config {Map} Configuration structure
+     * @param proto {Object} Prototype where the methods should be attached
+     */
+    addCachedProperty : function( config, proto ) {
+      var vName = config.name;
+      var vUpName = rwt.util.Strings.toFirstUp( vName );
       var vStorageField = "_cached" + vUpName;
       var vComputerName = "_compute" + vUpName;
       var vChangeName = "_change" + vUpName;
-
-      if (typeof config.defaultValue !== "undefined") {
-        proto[vStorageField] = config.defaultValue;
+      if( typeof config.defaultValue !== "undefined" ) {
+        proto[ vStorageField ] = config.defaultValue;
       }
-
-      proto["get" + vUpName] = function()
-      {
-        if (this[vStorageField] == null) {
-          this[vStorageField] = this[vComputerName]();
+      proto[ "get" + vUpName ] = function() {
+        if( this[ vStorageField ] == null ) {
+          this[ vStorageField ] = this[ vComputerName ]();
         }
-
-        return this[vStorageField];
+        return this[ vStorageField ];
       };
-
-      proto["_invalidate" + vUpName] = function()
-      {
-        if (this[vStorageField] != null)
-        {
-          this[vStorageField] = null;
-
-          if (config.addToQueueRuntime) {
-            this.addToQueueRuntime(config.name);
+      proto[ "_invalidate" + vUpName ] = function() {
+        if( this[ vStorageField ] != null ) {
+          this[ vStorageField ] = null;
+          if( config.addToQueueRuntime ) {
+            this.addToQueueRuntime( config.name );
           }
         }
       };
-
-      proto["_recompute" + vUpName] = function()
-      {
-        var vOld = this[vStorageField];
-        var vNew = this[vComputerName]();
-
-        if (vNew != vOld)
-        {
-          this[vStorageField] = vNew;
-          this[vChangeName](vNew, vOld);
-
+      proto[ "_recompute" + vUpName ] = function() {
+        var vOld = this[ vStorageField ];
+        var vNew = this[ vComputerName ]();
+        if( vNew != vOld ) {
+          this[ vStorageField ] = vNew;
+          this[ vChangeName ]( vNew, vOld );
           return true;
         }
-
         return false;
       };
-
-      proto[vChangeName] = function(vNew, vOld) {};
-
-      proto[vComputerName] = function() {
+      proto[ vChangeName ] = function( /* vNew, vOld */ ) {};
+      proto[ vComputerName ] = function() {
         return null;
       };
-
-      proto["get" + vUpName].self = proto.constructor;
-      proto["_invalidate" + vUpName].self = proto.constructor;
-      proto["_recompute" + vUpName].self = proto.constructor;
+      proto[ "get" + vUpName ].self = proto.constructor;
+      proto[ "_invalidate" + vUpName ].self = proto.constructor;
+      proto[ "_recompute" + vUpName ].self = proto.constructor;
     }
 
   }
