2f3825f5f74124c26b21da4a487694d7d4e0e4cd
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-03-04 17:38:15 +0100

    Mark WidgetLifeCycleAdapter as deprecated
    We're going to deprecate the lifecycle package in this release in order
    to replace the concept of lifecycle phases in RAP 3.0.
    
    Move WidgetLifeCycleAdapter interface to an internal package and let the
    public interface inherit from it. This allows us to use the internal
    interface in our code and avoid deprecation warnings.
    
    Change-Id: I417b32c8b246b50a622b39ee58720916152f4610
    Signed-off-by: Ralf Sternberg <rsternberg@eclipsesource.com>

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/lifecycle/WidgetLifeCycleAdapter.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/lifecycle/WidgetLifeCycleAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2012 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2002, 2014 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,8 +24,13 @@ import org.eclipse.swt.widgets.Widget;
  * @see AbstractWidgetLCA
  * @see WidgetLCAUtil
  * @since 2.0
+ * @deprecated New custom widgets should use the RemoteObject API instead of LCAs.
+ * @see org.eclipse.rap.rwt.remote.RemoteObject
  */
-public interface WidgetLifeCycleAdapter {
+@Deprecated
+public interface WidgetLifeCycleAdapter
+  extends org.eclipse.rap.rwt.internal.lifecycle.WidgetLifeCycleAdapter
+{
 
   /**
    * Reads changes for this widget from the message that has been received from the client.
