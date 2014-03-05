eclipse-rap/org.eclipse.rap

commit aaa36da014099d86e507de345b2726e35d9cb75e

Remove faulty template item content render optimization
This caused items to not be re-rendered when their content changed. This
optimization was in place to prevent image flickering issues in google
chrome. To compensate, Style.js now "remembers" if an image was already
set as an elements background image and in that case does not do so
again.

Also added some jsDoc to Template.js

See Bug 425011 - [Template] Tree have problems with filters

Change-Id: Ib80665375186269cd60b955bcaebd33aba480e56

tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/GridRowTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 EclipseSource and others.
+ * Copyright (c) 2010, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2709,6 +2709,20 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.GridRowTest", {
 
       tree.getRenderConfig().rowTemplate = mockTemplate( [ 1, "text", 10, 20 ] );
 
+      row.renderItem( item, tree._config, false, null );
+
+      var nodes = row._getTargetNode().childNodes;
+      assertEquals( "bar", nodes[ 0 ].innerHTML );
+    },
+
+    testRenderTemplate_UpdateCellText : function() {
+      tree.setTreeColumn( -1 );
+      var item = this._createItem( tree );
+      item.setTexts( [ "lee", "loo" ] );
+      tree.getRenderConfig().rowTemplate = mockTemplate( [ 1, "text", 10, 20 ] );
+      row.renderItem( item, tree._config, false, null );
+
+      item.setTexts( [ "foo", "bar" ] );
       row.renderItem( item, tree._config, false, null );
 
       var nodes = row._getTargetNode().childNodes;

bundles/org.eclipse.rap.rwt/js/rwt/html/Style.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright: 2004, 2013 1&1 Internet AG, Germany, http://www.1und1.de,
+ * Copyright: 2004, 2014 1&1 Internet AG, Germany, http://www.1und1.de,
  *                       and EclipseSource
  *
  * This program and the accompanying materials are made available under the
@@ -575,7 +575,7 @@ rwt.qx.Class.define( "rwt.html.Style", {
     },
 
     setStyleProperty : function( target, property, value ) {
-      if( target instanceof rwt.widgets.base.Widget ) {
+      if( target.setStyleProperty ) {
         target.setStyleProperty( property, value );
       } else {
         target.style[ property ] = value;
@@ -613,7 +613,13 @@ rwt.qx.Class.define( "rwt.html.Style", {
 
     _setCssBackgroundImage : function( target, value ) {
       var cssImageStr = value ? "URL(" + this._resolveResource( value ) + ")" : "none";
-      this.setStyleProperty( target, "backgroundImage", cssImageStr );
+      if( target.setStyleProperty ) {
+        target.setStyleProperty( "backgroundImage", cssImageStr );
+      } else if( target.___rwtStyle__backgroundImage !== cssImageStr ){
+        // Not re-applying a current value prevents some occasional flickering in google chrome
+        target.___rwtStyle__backgroundImage = cssImageStr;
+        target.style.backgroundImage = cssImageStr;
+      }
     },
 
     _setCssFilterImage : function( target, value ) {

bundles/org.eclipse.rap.rwt/js/rwt/widgets/util/Template.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2010, 2014 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,6 @@ var renderer = rwt.widgets.util.CellRendererRegistry.getInstance().getAll();
 
 rwt.widgets.util.Template = function( cells ) {
   this._cells = cells;
-  this._cellCatch = [];
   this._cellRenderer = [];
   this._parseCells();
 };
@@ -29,12 +28,23 @@ rwt.widgets.util.Template.prototype = {
 
   hasCellLayout : true,
 
+  /**
+   * @param {Object} options The options object.
+   *
+   * @param {HTMLElement} options.element The element in which to render the cells. The element
+   * may have other children that will be ignored, but no assumptions should be made about the
+   * amount or order of the cell elements added by this template.
+   *
+   * @param {int} options.zIndexOffset The z-index of the first cell.
+   *
+   * @returns {object} the container reference
+   */
   createContainer : function( options ) {
     if( !options.element || typeof options.element.nodeName !== "string" ) {
       throw new Error( "Not a valid target for TemplateContainer:" + options.element );
     }
     if( typeof options.zIndexOffset !== "number" ) {
-      throw new Error( "Not a valid target for TemplateContainer:" + options.element );
+      throw new Error( "Not a valid z-index:" + options.zIndexOffset );
     }
     return {
       "element" : options.element,
@@ -53,6 +63,15 @@ rwt.widgets.util.Template.prototype = {
     return container.cellElements.indexOf( element );
   },
 
+  /**
+   * @param {Object} options The options object.
+   * @param {Object} options.container Reference to a container created by this template
+   * @param {Object} options.item The item to be rendered. Must implement API of GridItem.
+   * @param {boolean} options.enabled Items are renderd as disabled unless this is true
+   * @param {boolean} options.seeable Must indicate whether the container element is already in DOM
+   * @param {boolean} options.markupEnabled Set to true to prevent escaping
+   * @param {int[]} options.bounds The bounds in pixel and order of left, top, width, height
+   */
   render : function( options ) {
     if( !options.container || options.container.template !== this ) {
       throw new Error( "No valid TemplateContainer: " + options.container );
@@ -188,21 +207,17 @@ rwt.widgets.util.Template.prototype = {
       "seeable" : options.seeable
     };
     var container = options.container;
-    var boundsChanged = container.lastBounds !== options.bounds.join( "," );
-    var contentChanged = container.lastItem !== options.item;
-    if( boundsChanged || contentChanged ) {
-      for( var i = 0; i < this._cells.length; i++ ) {
-        var element = container.cellElements[ i ];
-        if( element ) {
-          var cellRenderer = renderer[ this._cells[ i ].type ];
-          cellRenderOptions.width = container.cellCache[ i ].width;
-          cellRenderOptions.height = container.cellCache[ i ].height;
-          var renderContent = cellRenderer.renderContent;
-          renderContent( element,
-                         this.getCellContent( options.item, i, cellRenderOptions ),
-                         this._cells[ i ],
-                         cellRenderOptions );
-        }
+    for( var i = 0; i < this._cells.length; i++ ) {
+      var element = container.cellElements[ i ];
+      if( element ) {
+        var cellRenderer = renderer[ this._cells[ i ].type ];
+        cellRenderOptions.width = container.cellCache[ i ].width;
+        cellRenderOptions.height = container.cellCache[ i ].height;
+        var renderContent = cellRenderer.renderContent;
+        renderContent( element,
+                       this.getCellContent( options.item, i, cellRenderOptions ),
+                       this._cells[ i ],
+                       cellRenderOptions );
       }
     }
   },

tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/TemplateTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 EclipseSource and others.
+ * Copyright (c) 2011, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -413,7 +413,7 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.TemplateTest", {
       assertEquals( [ "x.jpg", 1, 1 ], template.getCellContent( item, 0 ) );
     },
 
-    testgetCellContent_ImageFromNullItem : function() {
+    testGetCellContent_ImageFromNullItem : function() {
       var template = new Template( [ { "type" : "image", "bindingIndex" : 1 } ] );
 
       assertNull( template.getCellContent( null, 0 ) );
@@ -853,7 +853,24 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.TemplateTest", {
       var template = createTemplate( { "name" : "foobar" } );
 
       assertEquals( "foobar", template.getCellName( 0 ) );
+    },
+
+    testRender_RenderItemTwice : function() {
+      var counter = 0;
+      renderer.add( createCellRenderer( {
+        "renderContent" : function() { counter++; }
+      } ) );
+      var template = createTemplate( { "bindingIndex" : 0, "type" : "fooType" } );
+      var item = createGridItem( [ "foo", "bar" ] );
+      var container = createContainer( template );
+      var renderOptions = createRenderOptions( container, item );
+
+      template.render( renderOptions );
+      template.render( renderOptions );
+
+      assertEquals( 2, counter );
     }
+
 
   }
 

commit 3327721354c4e9b0738fdf84728a511afbb6c1f6

Fix images not appearing in grid rows that were cleared
See Bug 429217 - [Tree] Image of TreeItem disappears after scrolling.

The issue was caused by NOT using rwt.html.Style.setBackgroundImage to
clear the images when "null" was rendered as an item. The Style util
stores additional information on the HTML element that are not updated
in this case. The bug was caused
by commit aaa36da014099d86e507de345b2726e35d9cb75e.

Change-Id: Ibf636f7a16fc176ee2f80fb41ed68a9d00460fd2
Signed-off-by: Tim Buschtöns <tbuschto@eclipsesource.com>

bundles/org.eclipse.rap.rwt/js/rwt/widgets/base/GridRow.js
@@ -906,12 +906,12 @@ rwt.qx.Class.define( "rwt.widgets.base.GridRow", {
       }
       for( var i = 0; i < this._cellCheckImages.length; i++ ) {
         if( this._cellCheckImages[ i ] ) {
-          this._cellCheckImages[ i ].style.backgroundImage = "";
+          rwt.html.Style.setBackgroundImage( this._cellCheckImages[ i ], null );
         }
       }
       for( var i = 0; i < this._cellImages.length; i++ ) {
         if( this._cellImages[ i ] ) {
-          this._cellImages[ i ].style.backgroundImage = "";
+          rwt.html.Style.setBackgroundImage( this._cellImages[ i ], null );
         }
       }
       for( var i = 0; i < this._cellLabels.length; i++ ) {
@@ -920,7 +920,7 @@ rwt.qx.Class.define( "rwt.widgets.base.GridRow", {
         }
       }
       if( this._checkBoxElement ) {
-        this._checkBoxElement.style.backgroundImage = "";
+        rwt.html.Style.setBackgroundImage( this._checkBoxElement, null );
       }
       this._renderOverlay( null );
       this._hideRemainingElements();

tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/GridRowTest.js
@@ -1090,6 +1090,21 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.GridRowTest", {
       assertEquals( "Test", text );
     },
 
+    // See Bug 429217 - [Tree] Image of TreeItem disappears after scrolling
+    testRenderImageAfterNullItem : function() {
+      var item = this._createItem( tree );
+      item.setImages( [ [ "bla.jpg", 10, 10 ] ] );
+
+      row.renderItem( item, tree._config, false, null );
+      row.renderItem( null, tree._config, false, null );
+      row.renderItem( item, tree._config, false, null );
+
+      var node = row._getTargetNode().childNodes[ 1 ];
+      var image = TestUtil.getCssBackgroundImage( node );
+      assertTrue( image.indexOf( "bla.jpg" ) != -1 );
+      assertFalse( "none" == node.style.display );
+    },
+
     testHideUnusedBackgoundElement : function() {
       var item1 = this._createItem( tree );
       var item2 = this._createItem( tree );