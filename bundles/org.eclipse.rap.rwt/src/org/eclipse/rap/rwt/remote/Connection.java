9e53ad02843a40167bc41efa17ebf7c6a3f8947c
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-04-25 23:10:31 +0200

    Add @noimplement annotation to Connection interface
    Change-Id: I5eb9389210c44c0361ee66234ba84ce2f0292244
    Signed-off-by: Ralf Sternberg <rsternberg@eclipsesource.com>

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/remote/Connection.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/remote/Connection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 EclipseSource and others.
+ * Copyright (c) 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,8 +18,9 @@ import org.eclipse.rap.rwt.service.UISession;
  * UI session has exactly one connection which can be obtained by calling
  * {@link UISession#getConnection()}.
  *
- * @see UISession#getConnection()
  * @since 2.0
+ * @see UISession#getConnection()
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface Connection {
 
