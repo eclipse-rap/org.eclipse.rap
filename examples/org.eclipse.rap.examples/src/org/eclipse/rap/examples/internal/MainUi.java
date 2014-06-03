0a25ac7f6e8d56b36d38e863c0900834d2e054dc
Ivan Furnadjiev <ivan@eclipsesource.com>
2014-06-03 15:07:03 +0300

    Remove splash DOM element in Example Demo after application is loaded
    Change-Id: I53848405d056828a187f953dd6065065c99fc3b4
    Signed-off-by: Ivan Furnadjiev <ivan@eclipsesource.com>

--- examples/org.eclipse.rap.examples/src/org/eclipse/rap/examples/internal/MainUi.java
+++ examples/org.eclipse.rap.examples/src/org/eclipse/rap/examples/internal/MainUi.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 EclipseSource and others.
+ * Copyright (c) 2011, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -66,6 +66,7 @@ public class MainUi extends AbstractEntryPoint {
     scrolledArea.setContent( content );
     attachHistoryListener();
     selectInitialContribution();
+    removeSplash();
   }
 
   private void attachHistoryListener() {
@@ -87,6 +88,13 @@ public class MainUi extends AbstractEntryPoint {
     IExampleContribution contribution = Examples.getInstance().findInitialContribution();
     if( contribution != null ) {
       selectContribution( contribution );
+    }
+  }
+
+  private void removeSplash() {
+    JavaScriptExecutor executor = RWT.getClient().getService( JavaScriptExecutor.class );
+    if( executor != null ) {
+      executor.execute( "document.body.removeChild( document.getElementById( \"splash\" ) );" );
     }
   }
 
