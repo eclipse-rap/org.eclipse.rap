f1379243e616c39faf5928c0cdfe702e325b0c69
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-08-14 20:55:01 +0200

    Use ReadWriteLock in SharedInstanceBuffer
    Since instances are created only once but requested often,
    a read write lock that allows for concurrent reads and only
    blocks on write access should improve throughput.
    
    Since SharedInstanceBuffer is used by SingletonUtil, this should
    speed up access to singletons.
    
    Change-Id: I62691ebb27434807b35f8ed61333c8ded2c96939

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/util/SharedInstanceBuffer.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/util/SharedInstanceBuffer.java
@@ -13,32 +13,62 @@ package org.eclipse.rap.rwt.internal.util;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
 public class SharedInstanceBuffer<K, I> implements Serializable {
 
-  private final SerializableLock lock;
+  private final Lock readLock;
+  private final Lock writeLock;
   private final Map<K, I> store;
 
   public SharedInstanceBuffer() {
-    lock = new SerializableLock();
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    readLock = readWriteLock.readLock();
+    writeLock = readWriteLock.writeLock();
     store = new HashMap<>();
   }
 
   public I get( K key, InstanceCreator<K, I> instanceCreator ) {
-    synchronized( lock ) {
+    I result = getInstance( key );
+    if( result == null ) {
+      result = createInstance( key, instanceCreator );
+    }
+    return result;
+  }
+
+  public I remove( K key ) {
+    writeLock.lock();
+    try {
+      return store.remove( key );
+    } finally {
+      writeLock.unlock();
+    }
+  }
+
+  private I getInstance( K key ) {
+    readLock.lock();
+    try {
+      return store.get( key );
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  private I createInstance( K key, InstanceCreator<K, I> instanceCreator ) {
+    writeLock.lock();
+    try {
+      // Re-check because another thread might have acquired write lock and created an instance
+      // before we did. See doc on ReentrantReadWriteLock.
       I result = store.get( key );
       if( result == null ) {
         result = instanceCreator.createInstance( key );
         store.put( key, result );
       }
       return result;
-    }
-  }
-
-  public I remove( K key ) {
-    synchronized( lock ) {
-      return store.remove( key );
+    } finally {
+      writeLock.unlock();
     }
   }
 
