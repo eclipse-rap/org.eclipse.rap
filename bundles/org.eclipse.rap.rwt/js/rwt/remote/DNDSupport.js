d0b62de8d45231c1fc0a2e325c105fbbca16c6bb
Ivan Furnadjiev <ivan@eclipsesource.com>
2015-09-10 11:21:23 +0300

    Fix NFE with DND in Internet Explorer when zoomed
    Round DragSource/DropTarget x/y position to avoid fractional numbers in
    Internet Explorer when zoomed.
    
    476535: java.lang.NumberFormatException in
    DropTargetOperationHandler.createDropData on Internet Explorer when
    zoomed
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=476535
    
    Change-Id: I798b33abf5a36b4f3683f1d6d8671bdd8ddaabe6

--- bundles/org.eclipse.rap.rwt/js/rwt/remote/DNDSupport.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/remote/DNDSupport.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2014 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2009, 2015 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -119,8 +119,8 @@ rwt.remote.DNDSupport.prototype = {
     event[ "widget" ] = this._getDragSource( widget );
     event[ "eventName" ] = type;
     event[ "param" ] = {
-      "x" : x,
-      "y" : y,
+      "x" : Math.round( x ),
+      "y" : Math.round( y ),
       "time" : rwt.remote.EventUtil.eventTimestamp()
     };
     this._eventQueue[ type ] = event;
@@ -229,8 +229,8 @@ rwt.remote.DNDSupport.prototype = {
     event[ "widget" ] = this._getDropTarget( widget );
     event[ "eventName" ] = type;
     event[ "param" ] = {
-      "x" : x,
-      "y" : y,
+      "x" : Math.round( x ),
+      "y" : Math.round( y ),
       "item" : itemId,
       "operation" : operation,
       "feedback" : this._dropFeedbackFlags,
