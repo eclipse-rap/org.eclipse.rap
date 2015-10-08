38514578f8851bbb21f554a8a5598b5cbae91e9a
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-10-08 17:29:37 +0200

    Prevent NPE in SessionLocaleProvider
    When Equinox is started in -registryMultiLanguage mode, the current
    locale is requested from SessionLocaleProvider. There is a chance that
    ContextProvider.getUISession returns null even though a service context
    is available.
    
    Add a null check to handle this case.
    
    Change-Id: Ib8f65d5817f89479e7c487142427f6fe55341a1e

--- bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/rap/ui/internal/SessionLocaleProvider.java
+++ bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/rap/ui/internal/SessionLocaleProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 EclipseSource and others.
+ * Copyright (c) 2010, 2015 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,20 +13,21 @@ package org.eclipse.rap.ui.internal;
 import java.util.Locale;
 
 import org.eclipse.osgi.service.localization.LocaleProvider;
-import org.eclipse.rap.rwt.RWT;
 import org.eclipse.rap.rwt.internal.service.ContextProvider;
+import org.eclipse.rap.rwt.service.UISession;
 
 
 public final class SessionLocaleProvider implements LocaleProvider {
 
+  @Override
   public Locale getLocale() {
-    Locale result;
     if( ContextProvider.hasContext() ) {
-      result = RWT.getLocale();
-    } else {
-      result = Locale.getDefault();
+      UISession uiSession = ContextProvider.getUISession();
+      if( uiSession != null ) {
+        return uiSession.getLocale();
+      }
     }
-    return result;
+    return Locale.getDefault();
   }
 
 }
