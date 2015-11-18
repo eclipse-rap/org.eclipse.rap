9d2fb9a106bbca34d31e1ae33a31021899a6ab39
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-11-18 10:56:06 +0100

    Remove dead code for Firefox <4
    These Firefox versions aren't supported anymore and very unlikely to
    be still around anywhere.
    
    Change-Id: I8a272adac7d3a314934071f26b080cbad12a719b

--- bundles/org.eclipse.rap.rwt/js/rwt/remote/Request.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/remote/Request.js
@@ -32,119 +32,108 @@ rwt.remote.Request.createXHR = function() {
 
 rwt.remote.Request.prototype = {
 
-    dispose : function() {
-      if( this._request != null ) {
-        this._request.onreadystatechange = null;
-        this._request.abort();
-        this._success = null;
-        this._error = null;
-        this._request = null;
-      }
-    },
+  dispose : function() {
+    if( this._request != null ) {
+      this._request.onreadystatechange = null;
+      this._request.abort();
+      this._success = null;
+      this._error = null;
+      this._request = null;
+    }
+  },
 
-    send : function() {
-      var urlpar = null;
-      var post = this._method === "POST";
-      if( !post && this._data ) {
-        urlpar = this._data;
+  send : function() {
+    var urlpar = null;
+    var post = this._method === "POST";
+    if( !post && this._data ) {
+      urlpar = this._data;
+    }
+    var url = this._url;
+    if( urlpar ) {
+      url += ( url.indexOf( "?" ) >= 0 ? "&" : "?" ) + urlpar;
+    }
+    this._request.open( this._method, url, this._async );
+    this._configRequest();
+    this._request.send( post ? this._data : undefined );
+    if( !this._async ) {
+      this.dispose();
+    }
+  },
+
+  setAsynchronous : function( value ) {
+    this._async = value;
+  },
+
+  getAsynchronous : function() {
+    return this._async;
+  },
+
+  setSuccessHandler : function( handler, context ) {
+    this._success = function(){ handler.apply( context, arguments ); };
+  },
+
+  setErrorHandler : function( handler, context ) {
+    this._error = function(){ handler.apply( context, arguments ); };
+  },
+
+  setData : function( value ) {
+    this._data = value;
+  },
+
+  getData : function() {
+    return this._data;
+  },
+
+  _configRequest : function() {
+    if( !( Client.isWebkit() || Client.isBlink() ) ) {
+      this._request.setRequestHeader( "Referer", window.location.href );
+    }
+    var contentType = "application/json; charset=UTF-8";
+    this._request.setRequestHeader( "Content-Type", contentType );
+    this._request.onreadystatechange = rwt.util.Functions.bind( this._onReadyStateChange, this );
+  },
+
+  _onReadyStateChange : function() {
+    if( this._request.readyState === 4 ) {
+      var text;
+      // [if] typeof(..) == "unknown" is IE specific. Used to prevent error:
+      // "The data necessary to complete this operation is not yet available"
+      if( typeof this._request.responseText !== "unknown" ) {
+        text = this._request.responseText;
       }
-      var url = this._url;
-      if( urlpar ) {
-        url += ( url.indexOf( "?" ) >= 0 ? "&" : "?" ) + urlpar;
+      var event = {
+        "responseText" : text,
+        "status" : this._request.status,
+        "responseHeaders" : this._getHeaders(),
+        "target" : this
+      };
+      if( this._request.status === 200 ) {
+        if( this._success ) {
+          this._success( event );
+        }
+      } else {
+        if( this._error ) {
+          this._error( event );
+        }
       }
-      this._request.open( this._method, url, this._async );
-      this._configRequest();
-      this._request.send( post ? this._data : undefined );
-      if( !this._shouldUseStateListener() ) {
-        this._onReadyStateChange();
-      }
-      if( !this._async ) {
+      if( this._async ) {
         this.dispose();
       }
-    },
-
-    setAsynchronous : function( value ) {
-      this._async = value;
-    },
-
-    getAsynchronous : function() {
-      return this._async;
-    },
-
-    setSuccessHandler : function( handler, context ) {
-      this._success = function(){ handler.apply( context, arguments ); };
-    },
-
-    setErrorHandler : function( handler, context ) {
-      this._error = function(){ handler.apply( context, arguments ); };
-    },
-
-    setData : function( value ) {
-      this._data = value;
-    },
-
-    getData : function() {
-      return this._data;
-    },
-
-    _configRequest : function() {
-      var contentType = "application/json; charset=UTF-8";
-      this._request.setRequestHeader( "Content-Type", contentType );
-      if( this._shouldUseStateListener() ) {
-        this._request.onreadystatechange = rwt.util.Functions.bind( this._onReadyStateChange, this );
-      }
-    },
-
-    _shouldUseStateListener : function() {
-      var result = true;
-      if( !this._async && Client.isGecko() && Client.getMajor() < 4 ) {
-        // see Bug 398951 - RAP does not start in Firefox 3.x
-        result = false;
-      }
-      return result;
-    },
-
-    _onReadyStateChange : function() {
-      if( this._request.readyState === 4 ) {
-        var text;
-        // [if] typeof(..) == "unknown" is IE specific. Used to prevent error:
-        // "The data necessary to complete this operation is not yet available"
-        if( typeof this._request.responseText !== "unknown" ) {
-          text = this._request.responseText;
-        }
-        var event = {
-          "responseText" : text,
-          "status" : this._request.status,
-          "responseHeaders" : this._getHeaders(),
-          "target" : this
-        };
-        if( this._request.status === 200 ) {
-          if( this._success ) {
-            this._success( event );
-          }
-        } else {
-          if( this._error ) {
-            this._error( event );
-          }
-        }
-        if( this._async ) {
-          this.dispose();
-        }
-      }
-    },
-
-    _getHeaders : function() {
-      var text = this._request.getAllResponseHeaders();
-      var values = text.split( /[\r\n]+/g );
-      var result = {};
-      for( var i=0; i < values.length; i++ ) {
-        var pair = values[ i ].match( /^([^:]+)\s*:\s*(.+)$/i );
-        if( pair ) {
-          result[ pair[ 1 ] ] = pair[ 2 ];
-        }
-      }
-      return result;
     }
+  },
+
+  _getHeaders : function() {
+    var text = this._request.getAllResponseHeaders();
+    var values = text.split( /[\r\n]+/g );
+    var result = {};
+    for( var i = 0; i < values.length; i++ ) {
+      var pair = values[ i ].match( /^([^:]+)\s*:\s*(.+)$/i );
+      if( pair ) {
+        result[ pair[ 1 ] ] = pair[ 2 ];
+      }
+    }
+    return result;
+  }
 
 };
 
