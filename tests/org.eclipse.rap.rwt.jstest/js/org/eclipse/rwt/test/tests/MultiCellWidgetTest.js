a0cb5757b385490e61606c0676b2128ebccdbf42
Ivan Furnadjiev <ivan@eclipsesource.com>
2016-01-12 13:04:23 +0200

    Fix text-decoration not respected by MultiCellWidget based widgets
    text-decoration CSS property is not inherited by default. In order to
    respect the text-decoration set on the MultiCellWidget widget element,
    text cell node must have explicitly set text-decoration to inherit.
    
    Change-Id: Ie96cb07b1666a0e52a951e08a9a86b198b22ab0d

--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/MultiCellWidgetTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/MultiCellWidgetTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 EclipseSource and others.
+ * Copyright (c) 2009, 2016 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -498,6 +498,15 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.MultiCellWidgetTest", {
       assertEquals( "center", widget.getStyleProperty( "textAlign" ) );
       widget.setHorizontalChildrenAlign( "right" );
       assertEquals( "right", widget.getStyleProperty( "textAlign" ) );
+      widget.destroy();
+    },
+
+    testTextDecoration_initialValue : function() {
+      var widget = this.createDefaultWidget();
+      this.initWidget( widget, true );
+
+      var style = widget._getTargetNode().lastChild.style;
+      assertEquals( "inherit", style.textDecoration );
       widget.destroy();
     },
 
