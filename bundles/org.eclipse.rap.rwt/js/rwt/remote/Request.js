e1ee03e0026c1cc3f57825ae03b7fe57560d0a6a
Ivan Furnadjiev <ivan@eclipsesource.com>
2016-01-28 11:53:31 +0200

    Remove "Referer" http header from all requests (again)
    Commit 2a2ffcf6a387694209698ec5b6b9ec39bbaa572b has been reverted by
    mistake with commit 9d2fb9a106bbca34d31e1ae33a31021899a6ab39.
    Re-apply it again.
    
    Change-Id: Ib2c500ab741db6d6572104c1264e5472bdaa897e

--- bundles/org.eclipse.rap.rwt/js/rwt/remote/Request.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/remote/Request.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 EclipseSource and others.
+ * Copyright (c) 2012, 2016 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,6 @@
 namespace( "rwt.remote" );
 
 (function(){
-
-var Client = rwt.client.Client;
 
 rwt.remote.Request = function( url, method, responseType ) {
   this._url = url;
@@ -85,9 +83,6 @@ rwt.remote.Request.prototype = {
   },
 
   _configRequest : function() {
-    if( !( Client.isWebkit() || Client.isBlink() ) ) {
-      this._request.setRequestHeader( "Referer", window.location.href );
-    }
     var contentType = "application/json; charset=UTF-8";
     this._request.setRequestHeader( "Content-Type", contentType );
     this._request.onreadystatechange = rwt.util.Functions.bind( this._onReadyStateChange, this );
