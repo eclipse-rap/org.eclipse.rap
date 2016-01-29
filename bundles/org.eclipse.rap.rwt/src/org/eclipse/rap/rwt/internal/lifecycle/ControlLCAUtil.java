e5714f9319361dee5f51ad740b9d419ead750487
Ivan Furnadjiev <ivan@eclipsesource.com>
2016-01-29 14:28:37 +0200

    Don't render tabIndex property for not focusable controls
    This regression has been introduced with commit
    2ae722782aaa8ca3494ff7a2732e8d02517b0db7.
    tabIndex property (even -1) should not be rendered for not focusable
    controls. Having tabIndex set to -1 on ScrolledComposite content leads
    to unwanted scroll position reset.
    
    486756: Focus Out leads to jump to top in ScrolledComposite (3.0.1 only)
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=486756
    
    Change-Id: I1d8accb92734175768e31834ea06db7a469eb740

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/lifecycle/ControlLCAUtil.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/lifecycle/ControlLCAUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2015 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2002, 2016 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -190,10 +190,12 @@ public class ControlLCAUtil {
       computeTabIndices( ( Shell )control, 1 );
     }
     ControlRemoteAdapter remoteAdapter = getRemoteAdapter( control );
-    int actual = getTabIndex( control );
-    int preserved = remoteAdapter.getPreservedTabIndex();
-    if( !remoteAdapter.isInitialized() || actual != preserved ) {
-      getRemoteObject( control ).set( PROP_TAB_INDEX, actual );
+    if( takesFocus( control ) ) {
+      int actual = getTabIndex( control );
+      int preserved = remoteAdapter.getPreservedTabIndex();
+      if( !remoteAdapter.isInitialized() || actual != preserved ) {
+        getRemoteObject( control ).set( PROP_TAB_INDEX, actual );
+      }
     }
   }
 

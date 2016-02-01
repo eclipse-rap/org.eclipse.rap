eclipse-rap/org.eclipse.rap

commit 2ae722782aaa8ca3494ff7a2732e8d02517b0db7

Fix tab sequence when controls are excluded from the tab list
Client property tabIndex (Widget.js#tabIndex) has no reasonable default
for all widgets. We must render this property in all cases (even for
disabled tabbing, tabIndex = -1).
This regression has been introduced with commit
3e61ce166ad8b911282426ab94f17778339f32e1.

471795: Tab Sequencing ignored since RAP 3.0
https://bugs.eclipse.org/bugs/show_bug.cgi?id=471795

Change-Id: Ic1a541f3e0487081f2566f8fbf9f687af37a8ab6

bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/lifecycle/ControlLCAUtil.java
@@ -190,9 +190,10 @@ public class ControlLCAUtil {
       // tabIndex must be a positive value
       computeTabIndices( ( Shell )control, 1 );
     }
+    ControlRemoteAdapter remoteAdapter = getRemoteAdapter( control );
     int actual = getTabIndex( control );
-    int preserved = getRemoteAdapter( control ).getPreservedTabIndex();
-    if( changed( control, actual, preserved, -1 ) ) {
+    int preserved = remoteAdapter.getPreservedTabIndex();
+    if( !remoteAdapter.isInitialized() || actual != preserved ) {
       getRemoteObject( control ).set( PROP_TAB_INDEX, actual );
     }
   }

tests/org.eclipse.rap.rwt.test/src/org/eclipse/rap/rwt/internal/lifecycle/ControlLCAUtil_Test.java
@@ -225,6 +225,16 @@ public class ControlLCAUtil_Test {
   }
 
   @Test
+  public void testRenderTabIndex_disabled() {
+    shell.setTabList( new Control[ 0 ] );
+
+    ControlLCAUtil.renderChanges( shell );
+    ControlLCAUtil.renderChanges( control );
+
+    assertEquals( -1, getProtocolMessage().findSetProperty( control, "tabIndex" ).asInt() );
+  }
+
+  @Test
   public void testRenderTabIndex_changed() {
     Fixture.markInitialized( control );
 

commit e5714f9319361dee5f51ad740b9d419ead750487

Don't render tabIndex property for not focusable controls
This regression has been introduced with commit
2ae722782aaa8ca3494ff7a2732e8d02517b0db7.
tabIndex property (even -1) should not be rendered for not focusable
controls. Having tabIndex set to -1 on ScrolledComposite content leads
to unwanted scroll position reset.

486756: Focus Out leads to jump to top in ScrolledComposite (3.0.1 only)
https://bugs.eclipse.org/bugs/show_bug.cgi?id=486756

Change-Id: I1d8accb92734175768e31834ea06db7a469eb740

bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/lifecycle/ControlLCAUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2015 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2002, 2016 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -190,10 +190,12 @@ public class ControlLCAUtil {
       computeTabIndices( ( Shell )control, 1 );
     }
     ControlRemoteAdapter remoteAdapter = getRemoteAdapter( control );
-    int actual = getTabIndex( control );
-    int preserved = remoteAdapter.getPreservedTabIndex();
-    if( !remoteAdapter.isInitialized() || actual != preserved ) {
-      getRemoteObject( control ).set( PROP_TAB_INDEX, actual );
+    if( takesFocus( control ) ) {
+      int actual = getTabIndex( control );
+      int preserved = remoteAdapter.getPreservedTabIndex();
+      if( !remoteAdapter.isInitialized() || actual != preserved ) {
+        getRemoteObject( control ).set( PROP_TAB_INDEX, actual );
+      }
     }
   }
 

tests/org.eclipse.rap.rwt.test/src/org/eclipse/rap/rwt/internal/lifecycle/ControlLCAUtil_Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2015 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2002, 2016 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -52,6 +52,7 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.internal.graphics.ImageFactory;
 import org.eclipse.swt.internal.widgets.shellkit.ShellOperationHandler;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
@@ -200,6 +201,16 @@ public class ControlLCAUtil_Test {
     ControlLCAUtil.renderChanges( control );
 
     assertEquals( -1, getProtocolMessage().findSetProperty( control, "tabIndex" ).asInt() );
+  }
+
+  @Test
+  public void testRenderTabIndex_nonFocusableControl() {
+    control = new Composite( shell, SWT.NO_FOCUS );
+
+    ControlLCAUtil.renderChanges( shell );
+    ControlLCAUtil.renderChanges( control );
+
+    assertNull( getProtocolMessage().findSetOperation( control, "tabIndex" ) );
   }
 
   @Test