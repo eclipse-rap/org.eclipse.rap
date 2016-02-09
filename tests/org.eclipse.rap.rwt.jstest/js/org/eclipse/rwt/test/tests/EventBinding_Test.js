7219d68868ee2da1d4190713cfe15c090491bb19
Ivan Furnadjiev <ivan@eclipsesource.com>
2016-02-09 15:43:43 +0200

    Fix failing JS tests in Firefox
    - RequestTest.js#testSetRequestHeader - the "Referer" header has been
    removed with commit e1ee03e0026c1cc3f57825ae03b7fe57560d0a6a
    - EventBinding_Test.js#testDoItFalseMouseWheel - in Firefox "detail"
    property is used to define wheelDelta (see
    MouseEvent.js#_computeWheelDelta).
    
    Change-Id: Iab494ed4c76159de2999e7f344eb34eec2d9721c

--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/EventBinding_Test.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/EventBinding_Test.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 EclipseSource and others.
+ * Copyright (c) 2012, 2016 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -110,10 +110,13 @@ rwt.qx.Class.define( "org.eclipse.rap.clientscripting.EventBinding_Test", {
 
       EventBinding.addListener( text, "MouseWheel", listener );
       var domEvent = TestUtil.fakeMouseEventDOM( text.getElement(), "mousewheel", 1, 0, 0 );
-      domEvent.wheelDelta = 1;
+      if( rwt.client.Client.isGecko() ) {
+        domEvent.detail = 1;
+      } else {
+        domEvent.wheelDelta = 1;
+      }
       TestUtil.fireFakeDomEvent( domEvent );
 
-      // SWT doesnt support preventing native selection behavior (e.g. Text widget)
       assertTrue( EventHandlerUtil.wasStopped( domEvent ) );
     },
 
--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/RequestTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/RequestTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 EclipseSource and others.
+ * Copyright (c) 2012, 2016 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,6 @@
 (function(){
 
 var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
-var Client = rwt.client.Client;
 
 var request;
 
@@ -51,9 +50,6 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.RequestTest", {
       request.send();
 
       var contentType = "application/json; charset=UTF-8";
-      if( !( Client.isWebkit() || Client.isBlink() ) ) {
-        assertNotNull( findNativeCall( "setRequestHeader", [ "Referer", window.location.href ] ) );
-      }
       assertNotNull( findNativeCall( "setRequestHeader", [ "Content-Type", contentType ] ) );
       assertNull( findNativeCall( "setRequestHeader", [ "Pragma", "no-cache" ] ) );
       assertNull( findNativeCall( "setRequestHeader", [ "Cache-Control", "no-cache" ] ) );
