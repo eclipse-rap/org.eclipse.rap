65476668a63bdf1e9586adeb9c6f0bf4e1e57dd8
Ivan Furnadjiev <ivan@eclipsesource.com>
2016-04-13 16:45:42 +0300

    Fix client crash in case of missing operations array
    In some cases, as outlined in bug 390711 comment #4, the protocol JSON
    message could only have a head object. In RAP 2.3 such messages are
    processed correctly, but in RAP 3.1 they lead to client crash.
    
    Make operations array in JSON protocol message optional.
    
    Change-Id: Ie1464fe206c4cf4d391a1ab8c69eed2bc7e74823

--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/MessageProcessorTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/MessageProcessorTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2015 EclipseSource and others.
+ * Copyright (c) 2011, 2016 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -606,6 +606,17 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.MessageProcessorTest", {
           "cid": "foo"
         },
         "operations" : []
+      };
+      MessageProcessor.processMessage( message );
+      var connection = rwt.remote.Connection.getInstance();
+      assertEquals( "foo", connection.getConnectionId() );
+    },
+
+    testProcessMessage_withoutOperationsArray : function() {
+      var message = {
+        "head": {
+          "cid": "foo"
+        }
       };
       MessageProcessor.processMessage( message );
       var connection = rwt.remote.Connection.getInstance();
