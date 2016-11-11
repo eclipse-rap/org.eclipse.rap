eclipse-rap/org.eclipse.rap

commit 7d072db9b02b3b04e3c635440ae65dd8758f07cb

Fix save/restore graphics context when arc is drawn
Restoring graphics context state on the client in GC.js#_ellipse, before
fill/stroke operation may lead to a wrong shape - the transformation set
by translate/scale is restored.

499417: [GC] Draw arc is not correct in some cases
https://bugs.eclipse.org/bugs/show_bug.cgi?id=499417

Change-Id: I1459aa68411d4b33240e5860c4f3a669a3567c38

bundles/org.eclipse.rap.rwt/widgetkits/org/eclipse/swt/internal/widgets/canvaskit/GCOperationWriter.java
@@ -223,6 +223,7 @@ final class GCOperationWriter {
     float cy = operation.y + ry + offset;
     float startAngle = round( operation.startAngle * factor * -1, 4 );
     float arcAngle = round( operation.arcAngle * factor * -1, 4 );
+    addClientOperation( "save" );
     addClientOperation( "beginPath" );
     operations.add( new JsonArray()
       .add( "ellipse" )
@@ -236,9 +237,10 @@ final class GCOperationWriter {
       .add( arcAngle < 0 )
     );
     if( operation.fill ) {
-      addClientOperation( "lineTo", cx, cy  );
+      addClientOperation( "closePath" );
     }
     addClientOperation( operation.fill ? "fill" : "stroke" );
+    addClientOperation( "restore" );
   }
 
   private void drawPolyline( DrawPolyline operation ) {

tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/internal/widgets/canvaskit/GCOperationWriter_Test.java
@@ -454,9 +454,11 @@ public class GCOperationWriter_Test {
     gc.drawArc( 10, 20, 100, 200, 50, 100 );
 
     JsonArray ops = getGCOperations( canvas );
-    assertEquals( "[\"beginPath\"]", getOperation( 1, ops ) );
-    assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,-2.618,true]", getOperation( 2, ops ) );
-    assertEquals( "[\"stroke\"]", getOperation( 3, ops ) );
+    assertEquals( "[\"save\"]", getOperation( 1, ops ) );
+    assertEquals( "[\"beginPath\"]", getOperation( 2, ops ) );
+    assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,-2.618,true]", getOperation( 3, ops ) );
+    assertEquals( "[\"stroke\"]", getOperation( 4, ops ) );
+    assertEquals( "[\"restore\"]", getOperation( 5, ops ) );
   }
 
   @Test
@@ -465,9 +467,11 @@ public class GCOperationWriter_Test {
     gc.drawArc( 10, 20, 100, 200, 50, 100 );
 
     JsonArray ops = getGCOperations( canvas );
-    assertEquals( "[\"beginPath\"]", getOperation( 1, ops ) );
-    assertEquals( "[\"ellipse\",60.5,120.5,50,100,0,-0.8727,-2.618,true]", getOperation( 2, ops ) );
-    assertEquals( "[\"stroke\"]", getOperation( 3, ops ) );
+    assertEquals( "[\"save\"]", getOperation( 1, ops ) );
+    assertEquals( "[\"beginPath\"]", getOperation( 2, ops ) );
+    assertEquals( "[\"ellipse\",60.5,120.5,50,100,0,-0.8727,-2.618,true]", getOperation( 3, ops ) );
+    assertEquals( "[\"stroke\"]", getOperation( 4, ops ) );
+    assertEquals( "[\"restore\"]", getOperation( 5, ops ) );
   }
 
   @Test
@@ -476,10 +480,12 @@ public class GCOperationWriter_Test {
     gc.fillArc( 10, 20, 100, 200, 50, 100 );
 
     JsonArray ops = getGCOperations( canvas );
-    assertEquals( "[\"beginPath\"]", getOperation( 1, ops ) );
-    assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,-2.618,true]", getOperation( 2, ops ) );
-    assertEquals( "[\"lineTo\",60,120]", getOperation( 3, ops ) );
-    assertEquals( "[\"fill\"]", getOperation( 4, ops ) );
+    assertEquals( "[\"save\"]", getOperation( 1, ops ) );
+    assertEquals( "[\"beginPath\"]", getOperation( 2, ops ) );
+    assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,-2.618,true]", getOperation( 3, ops ) );
+    assertEquals( "[\"closePath\"]", getOperation( 4, ops ) );
+    assertEquals( "[\"fill\"]", getOperation( 5, ops ) );
+    assertEquals( "[\"restore\"]", getOperation( 6, ops ) );
   }
 
   @Test
@@ -488,10 +494,12 @@ public class GCOperationWriter_Test {
     gc.fillArc( 10, 20, 100, 200, 50, -100 );
 
     JsonArray ops = getGCOperations( canvas );
-    assertEquals( "[\"beginPath\"]", getOperation( 1, ops ) );
-    assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,0.8726001,false]", getOperation( 2, ops ) );
-    assertEquals( "[\"lineTo\",60,120]", getOperation( 3, ops ) );
-    assertEquals( "[\"fill\"]", getOperation( 4, ops ) );
+    assertEquals( "[\"save\"]", getOperation( 1, ops ) );
+    assertEquals( "[\"beginPath\"]", getOperation( 2, ops ) );
+    assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,0.8726001,false]", getOperation( 3, ops ) );
+    assertEquals( "[\"closePath\"]", getOperation( 4, ops ) );
+    assertEquals( "[\"fill\"]", getOperation( 5, ops ) );
+    assertEquals( "[\"restore\"]", getOperation( 6, ops ) );
   }
 
   @Test

bundles/org.eclipse.rap.rwt/js/rwt/widgets/GC.js
@@ -171,12 +171,10 @@ rwt.qx.Class.define( "rwt.widgets.GC", {
       var endAngle = operation[ 7 ];
       var dir = operation[ 8 ];
       if( rx > 0 && ry > 0 ) {
-        this._context.save();
         this._context.translate( cx, cy );
         // TODO [tb] : using scale here changes the stroke-width also, looks wrong
         this._context.scale( 1, ry / rx );
         this._context.arc( 0, 0, rx, startAngle, endAngle, dir );
-        this._context.restore();
       }
     },
 

commit 8caebc22c337a2c4766ccbf1fb742275334f7c94

Fix drawing of filled arc
Fill arc should produce a piece of pie instead of slice of a circle.

This regression has been introduced with change
7d072db9b02b3b04e3c635440ae65dd8758f07cb.

507392: Drawing a filled arc does not produce a pie
https://bugs.eclipse.org/bugs/show_bug.cgi?id=507392

Change-Id: Ia6f222fd3657af3c7e1f308d72c972ab5912d04c

bundles/org.eclipse.rap.rwt/widgetkits/org/eclipse/swt/internal/widgets/canvaskit/GCOperationWriter.java
@@ -237,6 +237,7 @@ final class GCOperationWriter {
       .add( arcAngle < 0 )
     );
     if( operation.fill ) {
+      addClientOperation( "lineTo", 0, 0 );
       addClientOperation( "closePath" );
     }
     addClientOperation( operation.fill ? "fill" : "stroke" );

examples/org.eclipse.rap.demo.controls/src/org/eclipse/rap/demo/controls/CanvasTab.java
@@ -163,8 +163,8 @@ public final class CanvasTab extends ExampleTab {
     gc.setBackground( display.getSystemColor( SWT.COLOR_WHITE ) );
     gc.setForeground( display.getSystemColor( SWT.COLOR_GREEN ) );
     gc.drawArc( 90, 220, 50, 25, 45, 180 );
-    gc.setBackground( display.getSystemColor( SWT.COLOR_YELLOW ) );
-    gc.fillArc( 150, 220, 50, 25, 45, 180 );
+    gc.setBackground( display.getSystemColor( SWT.COLOR_MAGENTA ) );
+    gc.fillArc( 150, 220, 50, 25, 45, 250 );
     gc.setBackground( display.getSystemColor( SWT.COLOR_BLUE ) );
     gc.fillOval( 210, 220, 50, 50 );
   }

tests/org.eclipse.rap.rwt.test/src/org/eclipse/swt/internal/widgets/canvaskit/GCOperationWriter_Test.java
@@ -483,9 +483,10 @@ public class GCOperationWriter_Test {
     assertEquals( "[\"save\"]", getOperation( 1, ops ) );
     assertEquals( "[\"beginPath\"]", getOperation( 2, ops ) );
     assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,-2.618,true]", getOperation( 3, ops ) );
-    assertEquals( "[\"closePath\"]", getOperation( 4, ops ) );
-    assertEquals( "[\"fill\"]", getOperation( 5, ops ) );
-    assertEquals( "[\"restore\"]", getOperation( 6, ops ) );
+    assertEquals( "[\"lineTo\",0,0]", getOperation( 4, ops ) );
+    assertEquals( "[\"closePath\"]", getOperation( 5, ops ) );
+    assertEquals( "[\"fill\"]", getOperation( 6, ops ) );
+    assertEquals( "[\"restore\"]", getOperation( 7, ops ) );
   }
 
   @Test
@@ -497,9 +498,10 @@ public class GCOperationWriter_Test {
     assertEquals( "[\"save\"]", getOperation( 1, ops ) );
     assertEquals( "[\"beginPath\"]", getOperation( 2, ops ) );
     assertEquals( "[\"ellipse\",60,120,50,100,0,-0.8727,0.8726001,false]", getOperation( 3, ops ) );
-    assertEquals( "[\"closePath\"]", getOperation( 4, ops ) );
-    assertEquals( "[\"fill\"]", getOperation( 5, ops ) );
-    assertEquals( "[\"restore\"]", getOperation( 6, ops ) );
+    assertEquals( "[\"lineTo\",0,0]", getOperation( 4, ops ) );
+    assertEquals( "[\"closePath\"]", getOperation( 5, ops ) );
+    assertEquals( "[\"fill\"]", getOperation( 6, ops ) );
+    assertEquals( "[\"restore\"]", getOperation( 7, ops ) );
   }
 
   @Test