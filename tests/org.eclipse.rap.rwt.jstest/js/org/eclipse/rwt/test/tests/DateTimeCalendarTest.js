f005b3425a4bd91005062d8521bf9d87b791dba8
Ivan Furnadjiev <ivan@eclipsesource.com>
2017-10-23 16:39:34 +0300

    Fix failing JS test due to recent changes in DateTime protocol
    Change-Id: If808091249452a6e3d33f421dd0c770887f82804

--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/DateTimeCalendarTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/DateTimeCalendarTest.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 EclipseSource and others.
+ * Copyright (c) 2010, 2017 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -67,23 +67,11 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.DateTimeCalendarTest", {
       widget.destroy();
     },
 
-    testSetYearByProtocol : function() {
+    testSetDateByProtocol : function() {
       var widget = this._createDefaultDateTimeByProtocol( "w3", "w2" );
-      TestUtil.protocolSet( "w3", { "year" : 2000 } );
+      TestUtil.protocolSet( "w3", { "date" : [ 2000, 6, 10 ] } );
       assertEquals( 2000, widget._calendar.getDate().getFullYear() );
-      widget.destroy();
-    },
-
-    testSetMonthByProtocol : function() {
-      var widget = this._createDefaultDateTimeByProtocol( "w3", "w2" );
-      TestUtil.protocolSet( "w3", { "month" : 6 } );
       assertEquals( 6, widget._calendar.getDate().getMonth() );
-      widget.destroy();
-    },
-
-    testSetDayByProtocol : function() {
-      var widget = this._createDefaultDateTimeByProtocol( "w3", "w2" );
-      TestUtil.protocolSet( "w3", { "day" : 10 } );
       assertEquals( 10, widget._calendar.getDate().getDate() );
       widget.destroy();
     },
