d03ee35be7e602751ec580996afe031da8ebbac8
Ivan Furnadjiev <ivan@eclipsesource.com>
2018-08-22 17:12:11 +0300

    Cleanup file input field value on FileUpload destruction
    Bug 510510: With RAP and IE, locks remain on file uploaded using
    FileDialog
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=510510
    
    Change-Id: If4573c40eb107ac36d646c9a448dd99eadcd09aa

--- bundles/org.eclipse.rap.rwt/js/rwt/widgets/FileUpload.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/widgets/FileUpload.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 EclipseSource and others.
+ * Copyright (c) 2011, 2018 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,6 +30,9 @@ rwt.qx.Class.define( "rwt.widgets.FileUpload", {
   },
 
   destruct : function() {
+    if( this._inputElement ) {
+      this._inputElement.value = "";
+    }
     this._formElement = null;
     this._inputElement = null;
   },
