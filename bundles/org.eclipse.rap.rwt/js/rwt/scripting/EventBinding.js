205aafdd9f61cb57eefb50dee415cb64353dba30
Ivan Furnadjiev <ivan@eclipsesource.com>
2018-10-02 15:40:13 +0300

    Prevent JS error when client listener is removed twice
    Bug 539069: [Widget] Removing client listener twice leads to client side
    error
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=539069
    
    Change-Id: I34fa13285bfacbca9345a8cba08a3e27a9fc2b16

--- bundles/org.eclipse.rap.rwt/js/rwt/scripting/EventBinding.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/scripting/EventBinding.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2017 EclipseSource and others.
+ * Copyright (c) 2012, 2018 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,7 +34,9 @@ rwt.scripting.EventBinding = {
     var nativeType = this._getNativeEventType( widget, eventType );
     var nativeSource = this._getNativeEventSource( widget, eventType );
     var wrappedListener = wrapperList.pop();
-    nativeSource.removeEventListener( nativeType, wrappedListener, window );
+    if( wrappedListener ) {
+      nativeSource.removeEventListener( nativeType, wrappedListener, window );
+    }
   },
 
   _wrapListener : function( widget, eventType, targetFunction ) {
