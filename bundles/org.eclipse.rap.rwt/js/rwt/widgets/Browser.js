420869781fe230fcfd0e31c069c351b454cc4c83
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2018-12-26 11:57:12 -0800

    Ignore the query string in case it contains "://"
    Bug 543055: BrowserFunction not injected when the url is relative but
    contains "://"
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=543055
    
    Change-Id: I64e0e7dadccdbe1b37a0cba1cb09f8e9fdf56f14
    Signed-off-by: Hiromu Hota <hiromu.hota@hal.hitachi.com>

--- bundles/org.eclipse.rap.rwt/js/rwt/widgets/Browser.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/widgets/Browser.js
@@ -177,6 +177,7 @@ rwt.qx.Class.define( "rwt.widgets.Browser", {
       var accessible = false;
       try {
         var src = this.getSource() || "";
+        src = src.split( "?" )[ 0 ];
         if( src.indexOf( "://" ) === -1 ) { // relative path?
           src = document.URL; // works since we only check that the domain matches
         }
