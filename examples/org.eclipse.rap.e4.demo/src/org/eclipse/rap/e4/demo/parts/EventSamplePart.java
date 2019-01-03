274f6dc5005de33fe60ec36f936f0f44d925d3c1
Ivan Furnadjiev <ivan@eclipsesource.com>
2019-01-03 11:42:41 +0200

    Update bundles version to 3.8.0
    Update copyright headers.
    Remove unneeded Override annotations.
    
    Change-Id: I607ac705e3157bdc78b9b9c8fc46a8add1f4bc2d

--- examples/org.eclipse.rap.e4.demo/src/org/eclipse/rap/e4/demo/parts/EventSamplePart.java
+++ examples/org.eclipse.rap.e4.demo/src/org/eclipse/rap/e4/demo/parts/EventSamplePart.java
@@ -20,72 +20,71 @@ import org.eclipse.swt.widgets.Group;
 
 @SuppressWarnings("restriction")
 public class EventSamplePart {
-	@Inject
-	IEventBroker broker;
-	
-	@Inject
-	UISynchronize uiSync;
-	
-	private ListViewer listViewer;
-	
-	@PostConstruct
-	void init(Composite parent) {
-		parent.setLayout(new GridLayout());
+  @Inject
+  IEventBroker broker;
 
-		listViewer = new ListViewer(parent);
-		listViewer.getControl().setLayoutData(
-				new GridData(GridData.FILL_BOTH));
-		
-		createSendGroup(parent, "EventTopic");
-		createSendGroup(parent, "UIEventTopic");
-	}
+  @Inject
+  UISynchronize uiSync;
 
-	private void createSendGroup(Composite parent, final String topicType) {
-		Group g = new Group(parent, SWT.NONE);
-		g.setText("@" + topicType);
-		g.setLayout(new GridLayout(2, true));
-		g.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		Button b = new Button(g, SWT.PUSH);
-		b.setText("Sync sending");
-		b.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				broker.send("rape4/" + topicType, "@"+topicType+" - Event at " + System.currentTimeMillis());
-			}
-		});
-		b.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		b = new Button(g, SWT.PUSH);
-		b.setText("Async sending");
-		b.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				broker.post("rape4/" + topicType, "@"+topicType+" - Event at " + System.currentTimeMillis());
-			}
-		});
-		b.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-	}
-	
-	@Inject
-	@Optional
-	void receiveEvent(@EventTopic("rape4/EventTopic") final String eventData) {
-		if( listViewer.getControl().getDisplay().getThread() == Thread.currentThread() ) {
-			listViewer.add(eventData);
-		} else {
-			uiSync.syncExec(new Runnable() {
-				
-				@Override
-				public void run() {
-					listViewer.add(eventData); 
-				}
-			});
-		}
-	}
-	
-	@Inject
-	@Optional
-	void receiveUIEvent(@UIEventTopic("rape4/UIEventTopic") final String eventData) {
-		listViewer.add(eventData);
-	}
+  private ListViewer listViewer;
+
+  @PostConstruct
+  void init(Composite parent) {
+    parent.setLayout(new GridLayout());
+
+    listViewer = new ListViewer(parent);
+    listViewer.getControl().setLayoutData(
+        new GridData(GridData.FILL_BOTH));
+
+    createSendGroup(parent, "EventTopic");
+    createSendGroup(parent, "UIEventTopic");
+  }
+
+  private void createSendGroup(Composite parent, final String topicType) {
+    Group g = new Group(parent, SWT.NONE);
+    g.setText("@" + topicType);
+    g.setLayout(new GridLayout(2, true));
+    g.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+    Button b = new Button(g, SWT.PUSH);
+    b.setText("Sync sending");
+    b.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        broker.send("rape4/" + topicType, "@"+topicType+" - Event at " + System.currentTimeMillis());
+      }
+    });
+    b.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+    b = new Button(g, SWT.PUSH);
+    b.setText("Async sending");
+    b.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        broker.post("rape4/" + topicType, "@"+topicType+" - Event at " + System.currentTimeMillis());
+      }
+    });
+    b.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+  }
+
+  @Inject
+  @Optional
+  void receiveEvent(@EventTopic("rape4/EventTopic") final String eventData) {
+    if( listViewer.getControl().getDisplay().getThread() == Thread.currentThread() ) {
+      listViewer.add(eventData);
+    } else {
+      uiSync.syncExec(new Runnable() {
+
+        public void run() {
+          listViewer.add(eventData);
+        }
+      });
+    }
+  }
+
+  @Inject
+  @Optional
+  void receiveUIEvent(@UIEventTopic("rape4/UIEventTopic") final String eventData) {
+    listViewer.add(eventData);
+  }
 }
