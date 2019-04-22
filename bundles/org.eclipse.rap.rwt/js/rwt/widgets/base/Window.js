315ee9446cf0375bb0ef0ac7b799307e2809eea3
Ivan Furnadjiev <ivan@eclipsesource.com>
2019-04-22 12:28:42 +0300

    Fix shell not restored after it's maximized and then minimized
    If a shell is minimized after it was maximised before, setting the mode
    to "maximised" do nothing as the shell already has "maximised" state
    set. In this case "_restoreFromMinimized" function must be called
    instead of "_maximize" in Windows.js#_applyMode.
    
    Bug 546558: Shell  is not coming to top after maximizing and minimizing
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=546558
    
    Change-Id: I0f536038db2a7d81f3225dcb68b2e86aca3e7576

--- bundles/org.eclipse.rap.rwt/js/rwt/widgets/base/Window.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/widgets/base/Window.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2016 1&1 Internet AG, Germany, http://www.1und1.de,
+ * Copyright (c) 2004, 2019 1&1 Internet AG, Germany, http://www.1und1.de,
  *                          EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -808,7 +808,11 @@ rwt.qx.Class.define("rwt.widgets.base.Window",
 
         case "maximized":
           this._disableResize = true;
-          this._maximize();
+          if( old === "minimized" ) {
+            this._restoreFromMinimized();
+          } else {
+            this._maximize();
+          }
           break;
 
         default:
