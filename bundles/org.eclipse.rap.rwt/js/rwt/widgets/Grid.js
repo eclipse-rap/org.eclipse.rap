5a4dce11c277f9cef53970f10916294f428a5003
Ivan Furnadjiev <ivan@eclipsesource.com>
2019-07-10 14:11:54 +0300

    Don't allow vertical scrollbar max value to exceed visible items count
    When alternative scrolling is enabled on the client-side Grid, the
    vertical scrollbar value is adjusted in Grid#_updateScrollHeight. If
    there is only one partly visible item, Grid#_getLastPageRowCount returns
    zero. As a result the scrollbar maximum value is set to a value bigger
    with 1 than visible items.
    
    Bug 545437: Enabling AutoHeight for GridVierwer leads to
    IllegalArgumentException
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=545437
    
    Change-Id: Id420afe45d49cbf5d4feb95306aab72ffaf55e0d

--- bundles/org.eclipse.rap.rwt/js/rwt/widgets/Grid.js
+++ bundles/org.eclipse.rap.rwt/js/rwt/widgets/Grid.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2018 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2010, 2019 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -983,7 +983,7 @@ rwt.qx.Class.define( "rwt.widgets.Grid", {
     _updateScrollHeight : function() {
       var max = this.getRootItem().getVisibleChildrenCount();
       if( this._altScrollingEnabled ) {
-        max = max - this._getLastPageRowCount() + 1;
+        max = Math.min( max,  max - this._getLastPageRowCount() + 1 );
         this._setVerticalScrollBarVisible( max > 1 );
       }
       if( !this._vertScrollBar.getDisposed() && ( this._vertScrollBar.getMaximum() !== max ) ) {
