6b6ca4ad593b97c299dd9305113f8a83b0370b66
Ivan Furnadjiev <ivan@eclipsesource.com>
2019-09-04 16:22:33 +0300

    Use generics in WorkbenchPart#getAdapter
    Sync the code with the platform.
    
    Bug 550745: getAdapter in WorkbenchPart should use generics as its RCP
    counterpart does
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=550745
    
    Change-Id: I94e17744d171ea380484873a39e61165ea45c3fa

--- bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/ui/part/WorkbenchPart.java
+++ bundles/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/ui/part/WorkbenchPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -141,7 +141,7 @@ public abstract class WorkbenchPart extends EventManager implements
      * should invoke the method on their superclass to ensure that the
      * Platform's adapter manager is consulted).
      */
-    public Object getAdapter(Class adapter) {
+    public <T> T getAdapter(Class<T> adapter) {
 
         /**
          * This implementation of the method declared by <code>IAdaptable</code>
