33ba4008618873fda22b1c0e69d5146b014d6a5f
Ivan Furnadjiev <ivan@eclipsesource.com>
2021-05-26 18:49:56 +0300

    Add missing methods to TestServletContext
    Also ensure wiring the tests to a compatible javax.servlet version by
    incrementing the lower version limit to 4.0.
    
    Change-Id: I31fffd48950e20a9733304afd5feca698e29489a

--- tests/org.eclipse.rap.rwt.testfixture/src/org/eclipse/rap/rwt/testfixture/internal/TestServletContext.java
+++ tests/org.eclipse.rap.rwt.testfixture/src/org/eclipse/rap/rwt/testfixture/internal/TestServletContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2014 EclipseSource and others.
+ * Copyright (c) 2009, 2021 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -67,115 +67,142 @@ public class TestServletContext implements ServletContext {
     this.logger = logger;
   }
 
+  @Override
   public ServletContext getContext( String arg0 ) {
     return null;
   }
 
+  @Override
   public int getMajorVersion() {
     return majorVersion;
   }
 
+  @Override
   public int getMinorVersion() {
     return minorVersion;
   }
 
+  @Override
   public String getMimeType( String arg0 ) {
     return null;
   }
 
+  @Override
   public Set<String> getResourcePaths( String arg0 ) {
     return null;
   }
 
+  @Override
   public URL getResource( String arg0 ) throws MalformedURLException {
     return null;
   }
 
+  @Override
   public InputStream getResourceAsStream( String arg0 ) {
     return null;
   }
 
+  @Override
   public RequestDispatcher getRequestDispatcher( String arg0 ) {
     return null;
   }
 
+  @Override
   public RequestDispatcher getNamedDispatcher( String arg0 ) {
     return null;
   }
 
+  @Override
   public Servlet getServlet( String arg0 ) throws ServletException {
     return null;
   }
 
+  @Override
   public Enumeration<Servlet> getServlets() {
     return null;
   }
 
+  @Override
   public Enumeration<String> getServletNames() {
     return null;
   }
 
+  @Override
   public void log( String arg0 ) {
     log( arg0, null );
   }
 
+  @Override
   public void log( Exception arg0, String arg1 ) {
     log( arg1, arg0 );
   }
 
+  @Override
   public void log( String arg0, Throwable arg1 ) {
     if( logger != null ) {
       logger.log( arg0, arg1 );
     }
   }
 
+  @Override
   public String getRealPath( String path ) {
     return Fixture.WEB_CONTEXT_DIR + path;
   }
 
+  @Override
   public String getServerInfo() {
     return null;
   }
 
+  @Override
   public String getInitParameter( String name ) {
     return ( String )initParameters.get( name );
   }
 
+  @Override
   public boolean setInitParameter( String name, String value ) {
     initParameters.put( name, value );
     return true;
   }
 
+  @Override
   public Enumeration<String> getInitParameterNames() {
     return null;
   }
 
+  @Override
   public Object getAttribute( String arg0 ) {
     return attributes.get( arg0 );
   }
 
+  @Override
   public Enumeration<String> getAttributeNames() {
     return new Enumeration<String>() {
       Iterator<String> iterator = attributes.keySet().iterator();
 
+      @Override
       public boolean hasMoreElements() {
         return iterator.hasNext();
       }
 
+      @Override
       public String nextElement() {
         return iterator.next();
       }
     };
   }
 
+  @Override
   public void setAttribute( String arg0, Object arg1 ) {
     attributes.put( arg0, arg1 );
   }
 
+  @Override
   public void removeAttribute( String arg0 ) {
     attributes.remove( arg0 );
   }
 
+  @Override
   public String getServletContextName() {
     return servletContextName;
   }
@@ -184,113 +211,172 @@ public class TestServletContext implements ServletContext {
     this.servletContextName = servletContextName;
   }
 
+  @Override
   public String getVirtualServerName() {
     return null;
   }
 
+  @Override
   public String getContextPath() {
     return null;
   }
 
+  @Override
   public int getEffectiveMajorVersion() {
     return 0;
   }
 
+  @Override
   public int getEffectiveMinorVersion() {
     return 0;
   }
 
+  @Override
   public Dynamic addServlet( String servletName, String className ) {
     return null;
   }
 
+  @Override
   public Dynamic addServlet( String servletName, Servlet servlet ) {
     TestServletRegistration result = new TestServletRegistration( servletName, servlet );
     servlets.put( servletName, result );
     return result;
   }
 
+  @Override
   public Dynamic addServlet( String servletName, Class<? extends Servlet> servletClass ) {
     return null;
   }
 
+  @Override
   public <T extends Servlet> T createServlet( Class<T> clazz ) throws ServletException {
     return null;
   }
 
+  @Override
   public ServletRegistration getServletRegistration( String servletName ) {
     return servlets.get( servletName );
   }
 
+  @Override
   public Map<String, ? extends ServletRegistration> getServletRegistrations() {
     return servlets;
   }
 
+  @Override
   public javax.servlet.FilterRegistration.Dynamic addFilter( String filterName, String className ) {
     return null;
   }
 
+  @Override
   public javax.servlet.FilterRegistration.Dynamic addFilter( String filterName, Filter filter ) {
     TestFilterRegistration result = new TestFilterRegistration( filterName, filter );
     filters.put( filterName, result );
     return result;
   }
 
+  @Override
   public javax.servlet.FilterRegistration.Dynamic addFilter( String filterName,
                                                              Class<? extends Filter> filterClass )
   {
     return null;
   }
 
+  @Override
   public <T extends Filter> T createFilter( Class<T> clazz ) throws ServletException {
     return null;
   }
 
+  @Override
   public FilterRegistration getFilterRegistration( String filterName ) {
     return filters.get( filterName );
   }
 
+  @Override
   public Map<String, ? extends FilterRegistration> getFilterRegistrations() {
     return filters;
   }
 
+  @Override
   public SessionCookieConfig getSessionCookieConfig() {
     return null;
   }
 
+  @Override
   public void setSessionTrackingModes( Set<SessionTrackingMode> sessionTrackingModes ) {
   }
 
+  @Override
   public Set<SessionTrackingMode> getDefaultSessionTrackingModes() {
     return null;
   }
 
+  @Override
   public Set<SessionTrackingMode> getEffectiveSessionTrackingModes() {
     return null;
   }
 
+  @Override
   public void addListener( String className ) {
   }
 
+  @Override
   public <T extends EventListener> void addListener( T t ) {
   }
 
+  @Override
   public void addListener( Class<? extends EventListener> listenerClass ) {
   }
 
+  @Override
   public <T extends EventListener> T createListener( Class<T> clazz ) throws ServletException {
     return null;
   }
 
+  @Override
   public JspConfigDescriptor getJspConfigDescriptor() {
     return null;
   }
 
+  @Override
   public ClassLoader getClassLoader() {
     return null;
   }
 
+  @Override
   public void declareRoles( String... roleNames ) {
+  }
+
+  @Override
+  public Dynamic addJspFile( String servletName, String jspFile ) {
+    return null;
+  }
+
+  @Override
+  public int getSessionTimeout() {
+    return 0;
+  }
+
+  @Override
+  public void setSessionTimeout( int sessionTimeout ) {
+  }
+
+  @Override
+  public String getRequestCharacterEncoding() {
+    return null;
+  }
+
+  @Override
+  public void setRequestCharacterEncoding( String encoding ) {
+  }
+
+  @Override
+  public String getResponseCharacterEncoding() {
+    return null;
+  }
+
+  @Override
+  public void setResponseCharacterEncoding( String encoding ) {
   }
 
 }
