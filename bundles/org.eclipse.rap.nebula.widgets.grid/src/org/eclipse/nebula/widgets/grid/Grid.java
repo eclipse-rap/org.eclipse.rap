88bfe7da6d781ed9715adc4fe9b789fde958bc64
Ivan Furnadjiev <ivan@eclipsesource.com>
2021-12-14 14:02:03 +0200

    Dispose row header column when Grid is disposed
    Bug 577702: Memory-Leak: Grid.rowHeadersColumn does not get disposed
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=577702
    
    Change-Id: If1e699a77d4402149d20dc373deb84662ad1d7c5

--- bundles/org.eclipse.rap.nebula.widgets.grid/src/org/eclipse/nebula/widgets/grid/Grid.java
+++ bundles/org.eclipse.rap.nebula.widgets.grid/src/org/eclipse/nebula/widgets/grid/Grid.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2020 EclipseSource and others.
+ * Copyright (c) 2012, 2021 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3408,6 +3408,9 @@ public class Grid extends Composite {
     }
     for( GridColumnGroup group : columnGroups ) {
       group.dispose();
+    }
+    if( rowHeadersColumn != null ) {
+      rowHeadersColumn.dispose();
     }
   }
 
