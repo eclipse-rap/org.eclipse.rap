5521e23aaea89706fbe14c435e1b8e5e851b7b93
Ivan Furnadjiev <ivan@eclipsesource.com>
2022-09-01 12:22:45 +0300

    Small code refactoring (#49)
    Don't use a separate static block for initialization.

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/lifecycle/UITestUtil.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/lifecycle/UITestUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2014 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2007, 2022 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,17 +11,14 @@
  ******************************************************************************/
 package org.eclipse.rap.rwt.internal.lifecycle;
 
+import static org.eclipse.rap.rwt.internal.RWTProperties.ENABLE_UI_TESTS;
+
 import org.eclipse.rap.rwt.internal.RWTProperties;
 
 
 public final class UITestUtil {
 
-  static boolean enabled;
-
-  static {
-    String property = System.getProperty( RWTProperties.ENABLE_UI_TESTS );
-    enabled = Boolean.valueOf( property ).booleanValue();
-  }
+  static boolean enabled = RWTProperties.getBooleanProperty( ENABLE_UI_TESTS, false );
 
   public static boolean isEnabled() {
     return enabled;
