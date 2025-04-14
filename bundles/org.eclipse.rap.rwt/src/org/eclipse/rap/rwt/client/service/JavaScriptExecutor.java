ab953823849a212aa3abe094e29de701f2502d19
kysmith-csg <kyle.smith@cloud.com>
2025-04-14 13:53:57 -0400

    Add a method to JavaScriptExecutor to return a value

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/client/service/JavaScriptExecutor.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/client/service/JavaScriptExecutor.java
@@ -7,8 +7,11 @@
  *
  * Contributors:
  *    EclipseSource - initial API and implementation
+ *    Kyle Smith - Add evalute method
  ******************************************************************************/
 package org.eclipse.rap.rwt.client.service;
+
+import java.util.concurrent.CompletableFuture;
 
 
 /**
@@ -20,13 +23,35 @@ package org.eclipse.rap.rwt.client.service;
 public interface JavaScriptExecutor extends ClientService {
 
   /**
-   * Evaluate the JavaScript code on the client
-   *
-   * If the code throws an error, it will crash the web client.
-   * Accessing internals of the web client is strongly discouraged.
+   * Executes the JavaScript code on the client.
+   * <p>
+   * If the code throws an error, it will crash the web client. Accessing internals of the web
+   * client is strongly discouraged.
    *
    * @param code the JavaScript code to evaluate
    */
   public void execute( String code );
 
+  /**
+   * Evaluates the JavaScript code on the client. Unlike {@link #execute(String)}, this method
+   * returns a value (asynchronously) if the specified JavaScript code returns a value. The returned
+   * {@link CompletableFuture} is completed when the client object sends the return value, if any,
+   * in the next remote call, which may never happen. If the JavaScript code does not return a
+   * value, the future is completed with {@code null}.
+   * <p>
+   * <b>Important:</b> Do not wait for the future to complete in the UI thread or it will cause a
+   * deadlock. The code to execute is only sent to the client once the UI thread is available, which
+   * will not happen if the future is blocking it. Instead, use another thread or
+   * {@link java.util.concurrent.ExecutorService ExecutorService}.
+   * <p>
+   * If the code throws an error, it will crash the web client. Accessing internals of the web
+   * client is strongly discouraged.
+   *
+   * @param code the JavaScript code to evaluate
+   * @return a future that will be completed with the result of the JavaScript code, or a future
+   *         that will be completed with {@code null} if the given JavaScript code does not return a
+   *         value (i.e., returns {@code undefined})
+   * @since 4.3
+   */
+  public CompletableFuture<String> evaluate( String code );
 }
